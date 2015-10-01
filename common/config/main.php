395ac63262dbbfdb2681c49a463a439c50d93567
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-10-01 15:34:14 +0700

    yii2-user

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,56 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'en',
+    'sourceLanguage' => 'en',
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableConfirmation' => true,
+            'confirmWithin' => 21600,
+            'cost' => 12,
+            'admins' => ['superadmin'],
+            'modelMap' => [
+                'User' => 'dektrium\user\models\User',
+                'Profile' => 'dektrium\user\models\Profile',
+                'Account' => 'dektrium\user\models\Account',
+            ],
+        ],
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'request' => [
+            'enableCookieValidation' => true,
+            'enableCsrfValidation' => true,
+            'cookieValidationKey' => 'xyctuyvibonp',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'session' => [
+            'class' => 'yii\web\DbSession',
+            // 'db' => 'mydb',
+            'sessionTable' => 'session',
+        ],
+        'user' => [
+            'identityClass' => 'dektrium\user\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => [
+                'site/login' => 'user/security/login',
+                'site/signup' => 'user/registration/register',
+//                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+//                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+//                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+
+            ]
         ],
     ],
 ];
