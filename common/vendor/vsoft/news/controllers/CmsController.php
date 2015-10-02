a6e3ed84b8fd2160020e9c999bde5d7b7a72f473
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-10-02 11:20:48 +0700

    overrite cms

--- common/vendor/vsoft/news/controllers/CmsController.php
+++ common/vendor/vsoft/news/controllers/CmsController.php
@@ -2,6 +2,7 @@
 
 namespace vsoft\news\controllers;
 
+use funson86\cms\controllers\backend\CmsShowController;
 use Yii;
 use vsoft\news\models\CmsShow;
 use vsoft\news\models\CmsShowSearch;
@@ -12,110 +13,7 @@ use yii\filters\VerbFilter;
 /**
  * CmsController implements the CRUD actions for CmsShow model.
  */
-class CmsController extends Controller
+class CmsController extends CmsShowController
 {
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ],
-        ];
-    }
 
-    /**
-     * Lists all CmsShow models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new CmsShowSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single CmsShow model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new CmsShow model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new CmsShow();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing CmsShow model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing CmsShow model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the CmsShow model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return CmsShow the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = CmsShow::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
 }
