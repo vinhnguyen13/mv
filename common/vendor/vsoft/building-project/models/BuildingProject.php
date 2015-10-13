3020cfaf3a603c311a1868b0f78e7a13bf4a36bb
Lệnh Quách <qtlenh@gmail.com>
2015-10-13 14:45:52 +0700

    building project form

--- common/vendor/vsoft/building-project/models/BuildingProject.php
+++ common/vendor/vsoft/building-project/models/BuildingProject.php
@@ -130,6 +130,24 @@ class BuildingProject extends CmsShow {
     	return array_merge(parent::attributes(), self::getCustomFields());
     }
     
+    public function behaviors()
+    {
+    	return [
+	    	'slug' => [
+	    		'class' => 'Zelenin\yii\behaviors\Slug',
+	    		'slugAttribute' => 'slug',
+	    		'attribute' => 'title',
+	    		// optional params
+	    		'ensureUnique' => true,
+	    		'replacement' => '-',
+	    		'lowercase' => true,
+	    		'immutable' => false,
+	    		// If intl extension is enabled, see http://userguide.icu-project.org/transforms/general.
+	    		'transliterateOptions' => 'Russian-Latin/BGN; Any-Latin; Latin-ASCII; NFD; [:Nonspacing Mark:] Remove; NFC;'
+			],
+    	];
+    }
+    
     public function beforeSave($insert)
     {
     	if(parent::beforeSave($insert))
