0a7896a3a0efc8db220287377ceb0b6b57255005
vinh.nguyen <quangvinh.nguyen@trungthuygroup.vn>
2015-10-19 10:29:26 +0700

    add animation in detail page

--- frontend/web/themes/metvuong1/views/site/index.php
+++ frontend/web/themes/metvuong1/views/site/index.php
@@ -15,11 +15,11 @@ $this->title = Yii::t('express','We offer exeptional amenities and renowned whit
 </style>
 <div class="container-fluid layoutindex">
     <div class="row main_content">
-        <img src="<?=Yii::$app->view->theme->baseUrl?>/resources/IMG/01.png">
+        <img src="<?=Yii::$app->view->theme->baseUrl?>/resources/img/01.png">
     </div>
     <div class="bannerblock">
         <ul>
-            <li><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/IMG/logo_01.png"></li>
+            <li><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/img/logo_01.png"></li>
             <li class="title noticaitalic">Lancaster Legacy offers you a sweeping panoramic view of the city skyline<span class="linebuttom"></span> </li>
             <li>Besides 109 ultra-luxury and graciously furnished apartments ranging from studios to penthouses, the building also features 6 floors of working space for setting up professional and supreme offices.</li>
         </ul>
