fde7171ecb7249fe9326dff9b6ea79ec2cc9d288
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-10-23 17:59:51 +0700

    temp comment

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -75,11 +75,11 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        $meta = Yii::$app->meta->getMeta(Yii::$app->request->absoluteUrl);
-        if(!empty($meta)){
-            $json_data = Json::decode($meta->metadata, true);
-            Yii::$app->meta->addMeta($json_data);
-        }
+//        $meta = Yii::$app->meta->getMeta(Yii::$app->request->absoluteUrl);
+//        if(!empty($meta)){
+//            $json_data = Json::decode($meta->metadata, true);
+//            Yii::$app->meta->addMeta($json_data);
+//        }
 
         return $this->render('index');
     }
