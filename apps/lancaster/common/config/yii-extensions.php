31d67ec821cc912149099fdc508728c3f8e8537c
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-10-23 15:40:05 +0700

    remove

---
+++ apps/lancaster/common/config/yii-extensions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinhnguyen
+ * Date: 10/23/2015
+ * Time: 3:08 PM
+ */
--- common/vendor/vsoft/news/messages/vi-VN/cms.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Nhut Tran
- * Date: 10/19/2015 3:24 PM
- */
