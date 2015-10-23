b66b92ef9959bb90ad2322d583fa12a34630ef19
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-10-23 16:56:43 +0700

    new layout

--- apps/metvuong/frontend/web/themes/metvuong1/views/site/index.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/site/index.php
@@ -5,9 +5,9 @@
 $this->title = Yii::t('express','We offer exeptional amenities and renowned white - glove services');
 ?>
 <div class="row">
-    <div class="col-xs-8 col-lg-9 pdR-5">
+    <div class="col-sm-8 col-lg-9 col-right-home">
         <div class="news-hot">
-            <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/bg-banner.jpg" alt=""></div>
+            <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/bg-banner.jpg);"></div>
             <div class="content-news-hot">
                 <h2>9 dự án đang và sẽ thay đổi diện mạo Quận 4</h2>
                 <p>ngày đầu tiên là tên một trong những cuốn sách trên bàn làm việc của Nguyễn Trung Tín, mới được bổ nhiệm vào vị trí  mới được bổ nhiệm vào vị trí  mới được bổ nhiệm vào vị trí  mới được bổ nhiệm vào vị trí  mới được bổ nhiệm vào vị trí  mới được bổ nhiệm vào vị trí Tổng Giám đốc Công ty Cổ phần Tập đoàn ổng Giám đốc Công ty Cổ phần Tập đoàn ổng Giám đốc Công ty Cổ phần Tập đoàn ổng Giám đốc Công ty Công ty Cổ phần Tập đoàn ổng Giám đốc Công ty Công ty Cổ phần Tập đoàn ổng Giám đốc Công ty  Cổ phần Tập đoàn ổng Giám đốc Công ty Cổ phần Tập đoàn Trung Thủy (TTG) hôm 15.1, đúng vào dịp sinh nhật lần thứ 52 của thân mẫu, bà Dương Thanh Thủy</p>
@@ -16,66 +16,74 @@ $this->title = Yii::t('express','We offer exeptional amenities and renowned whit
         <div class="news-bds">
             <div class="widget-title clearfix"><h2>bất động sản</h2></div>
             <div class="row">
-                <div class="col-xs-4">
-                    <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190.jpg" alt=""></div>
+                <div class="col-sm-4">
+                    <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190.jpg);"></div>
                     <p><a class="color-title-link" href="#">Nông nghiệp Việt Nam đón nhận làn sóng đầu tư chưa</a></p>
                     <p>Việt Nam có điều kiện tự nhiên thuận lợi để sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                 </div>
 
-                <div class="col-xs-4">
-                    <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190-1.jpg" alt=""></div>
+                <div class="col-sm-4">
+                    <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190-1.jpg);"></div>
                     <p><a class="color-title-link" href="#">Nông nghiệp Việt Nam đón nhận làn sóng đầu tư chưa</a></p>
                     <p>Việt Nam có điều kiện tự nhiên thuận lợi để sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                 </div>
-                <div class="col-xs-4">
-                    <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190-2.jpg" alt=""></div>
+                <div class="col-sm-4">
+                    <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img283x190-2.jpg);"></div>
                     <p><a class="color-title-link" href="#">Nông nghiệp Việt Nam đón nhận làn sóng đầu tư chưa</a></p>
                     <p>Việt Nam có điều kiện tự nhiên thuận lợi để sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                 </div>
             </div>
         </div>
         <div class="news-tc-dn row">
-            <div class="col-xs-6">
+            <div class="col-sm-6">
                 <div class="widget-title clearfix"><h2>tài chính - ngân hàng</h2></div>
-                <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img458x305.jpg" alt=""></div>
+                <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img458x305.jpg);"></div>
                 <p><a class="color-title-link" href="#">Nông nghiệp Việt Nam đón nhận làn sóng đầu tư chưa</a></p>
                 <p>Việt Nam có điều kiện tự nhiên thuận lợi để sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương </p>
             </div>
-            <div class="col-xs-6">
+            <div class="col-sm-6">
                 <div class="widget-title clearfix"><h2>doanh nghiệp</h2></div>
-                <div class="wrap-img"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img458x305.jpg" alt=""></div>
+                <div class="wrap-img bgcover" style="background-image: url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img458x305.jpg);"></div>
                 <p><a class="color-title-link" href="#">Nông nghiệp Việt Nam đón nhận làn sóng đầu tư chưa</a></p>
                 <p>Việt Nam có điều kiện tự nhiên thuận lợi để sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương </p>
             </div>
         </div>
     </div>
-    <div class="col-xs-4 col-lg-3 pdL-5">
+    <div class="col-sm-4 col-lg-3 col-left-home">
         <div class="siderbar widget-tinmoi clearfix siderbar-style">
             <div class="widget-title clearfix"><h2>Tin mới</h2></div>
             <ul>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
@@ -84,48 +92,60 @@ $this->title = Yii::t('express','We offer exeptional amenities and renowned whit
             </ul>
         </div>
         <div class="siderbar widget-ads clearfix">
-            <a class="wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img295x210.jpg" alt=""></a>
+            <a class="wrap-img" href="#"><img src="images/img295x210.jpg" alt=""></a>
         </div>
         <div class="siderbar widget-dqt clearfix siderbar-style">
             <div class="widget-title clearfix"><h2>đáng quan tâm</h2></div>
             <ul>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
                     </div>
                 </li>
                 <li>
-                    <a class="pull-left wrap-img" href="#"><img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg" alt=""></a>
+                    <div class="pull-left wrap-img bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img82x55.jpg);">
+                        <a href="#" title=""></a>
+                    </div>
                     <div>
                         <a class="color-title-link" href="#">Việt Nam có điều kiện tự nhiên ...</a>
                         <p>sản xuất nông nghiệp nhưng bao đời nay chỉ sản xuất nhỏ lẻ, manh mún nên không xây dựng được thương hiệu... </p>
@@ -151,13 +171,13 @@ $this->title = Yii::t('express','We offer exeptional amenities and renowned whit
         <div class="pull-left item-danb">
             <div class="row">
                 <div class="col-xs-4">
-                    <img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg" alt="">
+                    <div class="bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg);"></div>
                 </div>
                 <div class="col-xs-4">
-                    <img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg" alt="">
+                    <div class="bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg);"></div>
                 </div>
                 <div class="col-xs-4">
-                    <img src="<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg" alt="">
+                    <div class="bgcover" style="background-image:url(<?=Yii::$app->view->theme->baseUrl?>/resources/images/img263x176.jpg);"></div>
                 </div>
             </div>
         </div>
