cf545fe1d6c7e4856e1284f5ff0749a50556bac3
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-10-23 18:13:55 +0700

    fix realestate template

--- apps/metvuong/common/vendor/vsoft/news/widgets/views/realestate.php
+++ apps/metvuong/common/vendor/vsoft/news/widgets/views/realestate.php
@@ -10,21 +10,21 @@ use yii\helpers\Html;
 <div class="news-bds">
     <div class="widget-title clearfix"><h2>bất động sản</h2></div>
     <?php if (!empty($news)) { ?>
-    <div class="row">
-        <?php foreach ($news as $k => $n) { ?>
-        <div class="col-xs-4">
-            <div class="wrap-img">
-                <a class="pull-left wrap-img" href="<?=\yii\helpers\Url::toRoute(['view', 'id' => $n->id, 'slug' => $n->slug])?>">
-                    <img src="/store/news/show/<?= $n->banner ?>" alt="<?= $n->title ?>">
-                </a>
-            </div>
-            <p>
-                <?= Html::a(strlen($n->title) > 30 ? mb_substr($n->title, 0, 30) . '...' : $n->title, ['view', 'id' => $n->id, 'slug' => $n->slug], ['class' => 'color-title-link']) ?>
-            </p>
-            <p> <?= strlen($n->brief) > 300 ? mb_substr($n->brief, 0, 300) : $n->brief ?> </p>
+        <div class="row">
+            <?php foreach ($news as $k => $n) { ?>
+                <div class="col-sm-4">
+                    <div class="wrap-img bgcover"
+                         style="background-image: url('/store/news/show/<?= $n->banner ?>');"></div>
+                    <p>
+                        <a class="color-title-link" href="<?= \yii\helpers\Url::toRoute(['view', 'id' => $n->id, 'slug' => $n->slug]) ?>">
+                            <?= strlen($n->title) > 30 ? mb_substr($n->title, 0, 30) . '...' : $n->title ?>
+                        </a>
+                    </p>
+
+                    <p> <?= strlen($n->brief) > 150 ? mb_substr($n->brief, 0, 150) . '...' : $n->brief ?></p>
+                </div>
+            <?php } ?>
         </div>
-        <?php } ?>
-    </div>
     <?php } ?>
 </div>
 
