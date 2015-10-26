e3faddc025b982fe46e95cd528e7de4b6563db18
thanhtat <tatthanh.nguyen@trungthuygroup.vn>
2015-10-26 17:04:37 +0700

    html layout

--- apps/metvuong/frontend/web/themes/metvuong1/resources/js/common.js
+++ apps/metvuong/frontend/web/themes/metvuong1/resources/js/common.js
@@ -17,5 +17,33 @@ $(document).ready(function() {
 		pushRight.open();
 	});
 
+	var btnScrollTop = $('<div id="topcontrol" title="Lên đầu trang"><img src="images/scrolltop.png" style="width:40px; height:40px"></div>')
+	$('body').append(btnScrollTop);
+	$(window).scroll(function(){
+		var valScroll = $(this).scrollTop();
+
+		if( valScroll >= 100 ) {
+			$("#topcontrol").css("display", "block");
+			$('#topcontrol').stop().animate({
+				opacity: 1
+			}, 500);
+		}else {
+			valScroll = 0;
+			$("#topcontrol").css({
+				display: 'none'
+			});
+			$('#topcontrol').stop().animate({
+				opacity: 0
+			},0);
+		}
+    });
+
+    $("#topcontrol").on('click', function() {
+    	$('body,html').stop().animate({
+			scrollTop: 0
+		}, 800);
+    });
 	
 });
+
+function l(x){console.log(x);}
