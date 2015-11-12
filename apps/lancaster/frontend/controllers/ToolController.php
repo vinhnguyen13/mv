8d5a426c532060bfa307c183e60042f0e34ede4a
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-11-12 17:54:48 +0700

    add cashflow

--- apps/lancaster/frontend/controllers/ToolController.php
+++ apps/lancaster/frontend/controllers/ToolController.php
@@ -79,6 +79,7 @@ class ToolController extends Controller
     public function actionSaveStep()
     {
         $app = Yii::$app;
+
         if(empty($this->_session)){
             $this->_session = $app->getSession();
         }
@@ -88,29 +89,98 @@ class ToolController extends Controller
 //            $this->_session[$step] = $app->request->post();
             $this->_session = Yii::$app->session;
             $this->_session->set($step, filter_var($app->request->post('total_project_cost'), FILTER_SANITIZE_NUMBER_INT));
+
             $path = Yii::$app->view->theme->basePath.'\\resources\\chart\\';
-            $file_name = 'data.json';
+            $data_file = 'data.json';
 
             if($step == 'scenario_1'){
                 $net_cashflow = $app->request->post()["net_cashflow"];
                 $scenario_1 = ['scenario_1' => $net_cashflow];
                 $data_1 = Json::encode($scenario_1, JSON_NUMERIC_CHECK);
-                if($this->writeFile($path.$file_name, $data_1))
-                    return ['status'=>'Scenario 1 OK', 'file'=>$file_name];
+                if($this->writeFile($path.$data_file, $data_1))
+                    return ['status'=>'Scenario 1 OK', 'file'=>$data_file];
                 else
                     return ['status' => 'Failed'];
             }
             else if($step == 'scenario_2'){
                 $net_cashflow_2 = $app->request->post()["net_cashflow"];
-                $file = $this->readFile($path.$file_name);
-                if(!empty($file)) {
+
+                if(!file_exists($path.$data_file)) {
+                    $file = $this->readFile($path.$data_file);
                     $arr_file = Json::decode($file, true);
                     $arr_file['scenario_2'] = $net_cashflow_2;
                     $data_2 = Json::encode($arr_file, JSON_NUMERIC_CHECK);
-                    $this->writeFile($path . $file_name, $data_2);
-                    return ['status' => 'Scenario 2 OK', 'file' => $file_name];
+                    $this->writeFile($path . $data_file, $data_2);
+                    return ['status' => 'Scenario 2 OK', 'file' => $data_file];
                 } else
                     return ['status' => 'Failed'];
+            }
+            else if($step == 'add'){
+                $path = Yii::$app->view->theme->basePath.'/resources/chart/add/';
+                $add_file = "add.json";
+
+                $post = $app->request->post();
+
+                $month_name = $post["field_name"];
+                $sales = $post["sales_".$month_name];
+                $cashflow = $post["cashflow_".$month_name];
+                $payment = null;
+
+                if(array_key_exists("payment_".$month_name, $post))
+                {
+                    $payment = $post["payment_".$month_name];
+                    unset($post["payment_".$month_name]);
+                }
+
+                if(!file_exists($path.$add_file)) {
+                    $scenario_1 = ['scenario_1' => [
+                            "pay_1" => $post["pay_1"],
+                            "pay_2" => $post["pay_2"],
+                            "pay_3" => $post["pay_3"],
+                            "pay_4" => $post["pay_4"],
+                            "pay_5" => $post["pay_5"],
+                            "pay_6" => $post["pay_6"],
+                            "pay_7" => $post["pay_7"],
+                            "pay_8" => $post["pay_8"],
+                            "pay_9" => $post["pay_9"],
+                            "pay_10" => $post["pay_10"],
+                            "pay_11" => $post["pay_11"],
+                            "pay_12" => $post["pay_12"],
+                            "pay_13" => $post["pay_13"],
+                            "pay_14" => $post["pay_14"],
+                            "pay_15" => $post["pay_15"],
+                            "total_project_cost" => $post["total_project_cost"],
+                            "sales_price_w_vat" => $post["sales_price_w_vat"],
+                            "net_sellable_area" => $post["net_sellable_area"],
+                            $month_name => [
+                                "sales" => $sales,
+                                "cashflow" => $cashflow,
+                                "payment" => $payment,
+                            ]
+                        ]
+                    ];
+                    $data_1 = Json::encode($scenario_1, JSON_NUMERIC_CHECK);
+
+                    if($this->writeFile($path.$add_file, $data_1))
+                        return ['status'=>'Scenario 1 OK', 'file'=>$add_file];
+                    else
+                        return ['status' => 'Failed'];
+                }
+                else{
+                    $file = $this->readFile($path.$add_file);
+                    $arr_file = Json::decode($file, true);
+                    $arr_file['scenario_1'][$month_name] = [
+                            "sales" => $sales,
+                            "cashflow" => $cashflow,
+                            "payment" => $payment,
+                    ];
+                    $data_1 = Json::encode($arr_file, JSON_NUMERIC_CHECK);
+
+                    if($this->writeFile($path.$add_file, $data_1))
+                        return ['status'=>'Scenario 1 OK', 'file'=>$add_file];
+                    else
+                        return ['status' => 'Failed'];
+                }
             }
             else{
                 return ['total_project_cost'=>filter_var($app->request->post('total_project_cost'), FILTER_SANITIZE_NUMBER_INT)];
@@ -164,6 +234,7 @@ class ToolController extends Controller
     }
 
     private function readFile($filePath){
+
         $handle = fopen($filePath, 'r') or die('Cannot open file:  '.$filePath);
         if(filesize($filePath) > 0) {
             $data = fread($handle, filesize($filePath));
--- apps/lancaster/frontend/web/themes/lancaster2/views/tool/_partials/cashflow_1.php
+++ apps/lancaster/frontend/web/themes/lancaster2/views/tool/_partials/cashflow_1.php
@@ -8,59 +8,110 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 ?>
+<style>
+    .check-payment {
+        overflow-y: scroll;
+        height: 240px;
+    }
+    .payment_schedule { margin: 0px 0px 20px 0px; }
+    .payment_schedule .row{
+        padding: 5px 0 0 15px !important;
+        display: none;
+    }
+</style>
 <h1>Scenario 1</h1>
 <?php $form = ActiveForm::begin([
     'options' => ['enctype' => 'multipart/form-data', 'id'=>'p_scenario_1'],
     'action' => \yii\helpers\Url::toRoute(['tool/save-step', 'step'=>'scenario_1'])
 ]); ?>
 
-    <?=Html::hiddenInput('total_project_cost', 2246792608858,['class'=>'form-control form-group total_project_cost']);?>
+<div class="payment_schedule">
+    <h4><b>Payment Schedule (%)</b></h4>
+    <div class="row">
+        <div class="col-md-3"><label>Reservation Fee</label></div>
+        <div class="col-md-3"><input type="text" name="pay_1" value="5" class="form-control"/></div>
 
-    <fieldset id="1">
-        <legend>T1</legend>
-        <div class="col-lg-3">
-            <div class="form-group field-cmscatalog-parent_id">
-                <?=Html::label('Incoming Cashflow');?>
-                <?=Html::input('text','incoming_cashflow[T1]',0,['class'=>'form-control form-group incoming_cashflow']);?>
-                <?=Html::hiddenInput(null, 0,['class'=>'form-control form-group hid_incoming_cashflow']);?>
-                <div class="hint-block"></div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="form-group field-cmscatalog-parent_id">
-                <?=Html::label('Outgoing Cashflow (%)');?>
-                <?=Html::input('text','cashflow[T1]',null,['class'=>'form-control form-group cashflow']);?>
-                <?=Html::hiddenInput(null, 0,['class'=>'form-control form-group hid_outgoing_cashflow']);?>
-                <?=Html::hiddenInput(null, 0,['class'=>'form-control form-group hid_accum_incoming_cashflow']);?>
-                <div class="hint-block"></div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="form-group field-cmscatalog-parent_id">
-                <?=Html::label('Sales (%)');?>
-                <?=Html::input('text','sales[T1]',null,['class'=>'form-control form-group sales']);?>
-                <div class="hint-block"></div>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="form-group field-cmscatalog-parent_id">
-                <?=Html::label('Net Accumulative Cashflow');?>
-                <?=Html::label('', 0, ['class'=>'form-control form-group net_cashflow']);?>
-                <?=Html::hiddenInput('net_cashflow[T1]', 0,['class'=>'form-control form-group net_cashflow']);?>
-                <div class="hint-block"></div>
-            </div>
-        </div>
-    </fieldset>
+        <div class="col-md-3"><label>1st Payment (within 1 month)</label></div>
+        <div class="col-md-3"><input type="text" name="pay_2" value="5" class="form-control"/></div>
 
-    <div class="form-group">
-        <label class="col-lg-1 control-label"></label>
-        <div class="col-lg-1">
-            <?= Html::button('Add', ['class' => 'btn btn-primary add']) ?>
-        </div>
-        <div class="col-lg-10">
-        <?= Html::submitButton('Next', ['class' => 'btn btn-primary next']) ?>
-        </div>
+        <div class="col-md-3"><label>Upon Finish Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_3" value="10" class="form-control" /></div>
+
+        <div class="col-md-3"><label>2 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_4" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>4 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_5" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>6 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_6" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>8 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_7" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>10 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_8" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>12 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_9" value="5" class="form-control" /></div>
+
+        <div class="col-md-3"><label>14 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_10" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>16 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_11" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>18 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_12" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>20 months after Foundation</label></div>
+        <div class="col-md-3"><input type="text" name="pay_13" value="5" class="form-control"/></div>
+
+        <div class="col-md-3"><label>Building Handover Procedure</label></div>
+        <div class="col-md-3"><input type="text" name="pay_14" value="25" class="form-control"/></div>
+
+        <div class="col-md-3"><label>Apartment Ownership Handover</label></div>
+        <div class="col-md-3"><input type="text" name="pay_15" value="5" class="form-control"/></div>
     </div>
+</div>
+
+<?=Html::hiddenInput('total_project_cost', 2246792608858,['class'=>'form-control form-group total_project_cost']);?>
+<?=Html::hiddenInput('sales_price_w_vat', 36438367,['class'=>'form-control form-group sales_price_w_vat']);?>
+<?=Html::hiddenInput('net_sellable_area', 78000,['class'=>'form-control form-group net_sellable_area']);?>
+
+<fieldset id="1">
+    <legend>T1</legend>
+    <div class="col-lg-3 input-percent">
+        <?=Html::label('Sales (%)');?>
+        <?=Html::input('text','sales_T1',null,['class'=>'form-control form-group sales']);?>
+        <?=Html::label('Outgoing Cashflow (%)');?>
+        <?=Html::input('text','cashflow_T1',null,['class'=>'form-control form-group cashflow']);?>
+    </div>
+    <div class="col-lg-4 check-payment">
+        <?php
+        $list = [
+            'pay_1' => 'Reservation Fee', 'pay_2' => '1st Payment (within 1 month)', 'pay_3' => 'Upon Finish Foundation',
+            'pay_4' => '2 months after Foundation', 'pay_5' => '4 months after Foundation', 'pay_6' => '6 months after Foundation',
+            'pay_7' => '8 months after Foundation', 'pay_8' => '10 months after Foundation', 'pay_9' => '12 months after Foundation',
+            'pay_10' => '14 months after Foundation', 'pay_11' => '16 months after Foundation', 'pay_12' => '18 months after Foundation',
+            'pay_13' => '20 months after Foundation', 'pay_14' => 'Building Handover Procedure', 'pay_15' => 'Apartment Ownership Handover',
+        ];
+        echo Html::checkboxList('payment_T1', null, $list);
+        ?>
+    </div>
+    <?=Html::hiddenInput('field_name', 'T1',['class'=>'form-control form-group field_name']);?>
+</fieldset>
+
+<div class="form-group command">
+    <div class="col-lg-3">
+        <?= Html::button('Add', ['class' => 'btn btn-primary add']) ?>
+    </div><div class="col-lg-3">
+        <?= Html::button('Calculate', ['class' => 'btn btn-primary calculate']) ?>
+    </div>
+    <div class="col-lg-4">
+        <?= Html::submitButton('Next', ['class' => 'btn btn-primary next']) ?>
+    </div>
+</div>
 <?php ActiveForm::end(); ?>
 
 <div class="cash_result" style="clear: both; padding-top: 20px;">
@@ -70,6 +121,17 @@ use yii\helpers\Html;
 </div>
 
 <script>
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    $(".payment_schedule").click(function(){
+        $(".payment_schedule .row").toggle();
+    });
+
     $(document).on("click",'#scenario_1 .next',function() {
         chart.next('/tool/save-step?step=scenario_1', 'p_scenario_1', 'scenario_1/afterNext');
         return false;
@@ -80,54 +142,61 @@ use yii\helpers\Html;
         $(".mainConfigSetParams").find(".nav-tabs a[href='#scenario_2']").trigger("click");
     });
 
-    var outgoing_cashflow_accumulative = 0;
-    var accumulative_incoming_cashflow = 0;
-    var incoming_cashflow = 0;
-    $(document).on("change",'#scenario_1 .cashflow',function() {
-        var fieldset = $(this).closest('fieldset');
-        console.log($('.total_project_cost').val());
-        var total_project_cost = $('.total_project_cost').val();
-        var cashflow = (-1 * parseFloat(fieldset.find('.cashflow').val()) * total_project_cost)/100;
-        var incoming = parseFloat(fieldset.find('.incoming_cashflow').val());
-        var total = 0;
-        var hid_outgoing_cashflow = parseFloat(fieldset.find('.hid_outgoing_cashflow').val());
-        var hid_incoming_cashflow = parseFloat(fieldset.find('.hid_incoming_cashflow').val());
-        var hid_accum_incoming_cashflow = parseFloat(fieldset.find('.hid_accum_incoming_cashflow').val());
+//    var total_project_cost = $(".total_project_cost").val();
+//    var sales_price_w_vat = $(".sales_price_w_vat").val();
+//    var net_sellable_area = $(".net_sellable_area").val();
 
-        if(hid_outgoing_cashflow == 0 && hid_accum_incoming_cashflow == 0) {
+//    var outgoing_cashflow = 0,
+//        cumulative_revenue = 0,
+//        accumulative_incoming_cashflow = 0;
+//    $(document).on("change",'#scenario_1 .cashflow',function() {
+//        var fieldset = $(this).closest('fieldset');
+//        console.log($('.total_project_cost').val());
+//
+//        var out_cashflow = (-1 * parseFloat(fieldset.find('.cashflow').val()) * total_project_cost)/100;
+//        var revenue = parseFloat(fieldset.find('.sales').val()/100) * sales_price_w_vat * net_sellable_area;
+//
+//        var total = 0;
+//        var hid_cumulative_revenue = parseFloat(fieldset.find('.hid_cumulative_revenue').val());
+//        var hid_outgoing_cashflow = parseFloat(fieldset.find('.hid_outgoing_cashflow').val());
+//
+//        fieldset.find('.hid_cumulative_revenue').val(cumulative_revenue);
+//        fieldset.find('.hid_outgoing_cashflow').val(outgoing_cashflow);
+//
+//        outgoing_cashflow = outgoing_cashflow + out_cashflow;
+//        cumulative_revenue = cumulative_revenue + revenue;
+//
+//        var incoming_cashflow = 0;
+//        fieldset.find('input[type=checkbox]').each(function() {
+//            if ($(this).is(":checked")) {
+//                var name = 'input[name=pay_'+ $(this).attr('value')+']';
+//                var value = revenue * parseFloat($(name).val()/100);
+//                incoming_cashflow = incoming_cashflow + value;
+//            }
+//        });
+//
+//        accumulative_incoming_cashflow = accumulative_incoming_cashflow + incoming_cashflow;
+//        total = outgoing_cashflow + accumulative_incoming_cashflow;
+//
+//        total = Math.round(total);
+//        fieldset.find('input.hid_net_accumulative_cashflow').val(total);
+//        fieldset.find('input.hid_net_accumulative_cashflow').autoNumeric('init', {aPad: false});
+//    });
 
-            fieldset.find('.hid_outgoing_cashflow').val(outgoing_cashflow_accumulative);
-            outgoing_cashflow_accumulative = outgoing_cashflow_accumulative + cashflow;
 
-            fieldset.find('.hid_incoming_cashflow').val(hid_incoming_cashflow);
-            incoming_cashflow = incoming_cashflow + incoming;
-
-            fieldset.find('.hid_accum_incoming_cashflow').val(accumulative_incoming_cashflow);
-            accumulative_incoming_cashflow = accumulative_incoming_cashflow + incoming_cashflow;
-
-            total = outgoing_cashflow_accumulative + accumulative_incoming_cashflow;
-        }
-        else {
-            fieldset.find('.hid_outgoing_cashflow').val(hid_outgoing_cashflow);
-            hid_outgoing_cashflow = hid_outgoing_cashflow + cashflow;
-
-            fieldset.find('hid_incoming_cashflow').val(hid_incoming_cashflow);
-            hid_incoming_cashflow = hid_incoming_cashflow + incoming;
-
-            fieldset.find('.hid_accum_incoming_cashflow').val(hid_accum_incoming_cashflow);
-            hid_accum_incoming_cashflow = hid_accum_incoming_cashflow + hid_incoming_cashflow;
-
-            total = hid_outgoing_cashflow + hid_accum_incoming_cashflow;
-        }
-
-        total = Math.round(total);
-        fieldset.find('label.net_cashflow').html(total).autoNumeric('init', {aPad: false});
-        fieldset.find('input.net_cashflow').val(total);
-    });
 
     var counter = 1;
     $("#scenario_1 .add").click(function(){
         var fieldset = $('form[id^="p_scenario_1"] fieldset:last');
+        $.ajax({
+            type: "post",
+            dataType: 'json',
+            url: '/tool/save-step?step=add',
+            data: ($('#p_scenario_1').serializeArray()),
+            success: function(data) {
+//                $("#"+formId).trigger(event, [{data: data}, 'something'] );
+            }
+        });
         counter += 1;
         var f = fieldset.clone().attr("id", counter);
         f.find("legend").text("T"+counter);
@@ -136,13 +205,11 @@ use yii\helpers\Html;
         f.find('.hid_outgoing_cashflow').attr("value", 0);
         f.find('.hid_accum_incoming_cashflow').attr("value", 0);
 
-        f.find("input.incoming_cashflow").attr("name", "incoming_cashflow[T"+counter+"]");
-        f.find("input.cashflow").attr("name", "cashflow[T"+counter+"]");
-        f.find("input.sales").attr("name", "sales[T"+counter+"]");
-        f.find("input.net_cashflow").attr("name", "net_cashflow[T"+counter+"]");
-
-        f.find("input.cashflow").attr("value", null);
-        f.find("input.net_cashflow").attr("value", 0);
+//        f.find("input.incoming_cashflow").attr("name", "incoming_cashflow_T"+counter);
+        f.find("input.cashflow").attr("name", "cashflow_T"+counter);
+        f.find("input.sales").attr("name", "sales_T"+counter);
+        f.find("input.field_name").val("T"+counter);
+        f.find("input[type=checkbox]").attr("name", "payment_T"+counter+"[]");
 
         f.insertAfter(fieldset);
     });
