76b4c3f30c4343bdf54a08ed884a93c1d2bd6d44
Lệnh Quách <qtlenh@gmail.com>
2015-11-24 14:52:34 +0700

    cms ad

--- apps/metvuong/console/migrations/m151120_083737_alter_ads_table.php
+++ apps/metvuong/console/migrations/m151120_083737_alter_ads_table.php
@@ -28,9 +28,6 @@ class m151120_083737_alter_ads_table extends Migration
 		$this->execute("ALTER TABLE `metvuong`.`ad_building_project`   
   CHANGE `lng` `lng` FLOAT(10,6) NULL,
   CHANGE `lat` `lat` FLOAT(10,6) NULL;");
-		
-		$this->execute("ALTER TABLE `metvuong`.`ad_investor`   
-  ADD COLUMN `status` TINYINT(1) DEFAULT 1  NOT NULL AFTER `email`;");
     }
 
     public function down()
@@ -57,9 +54,6 @@ class m151120_083737_alter_ads_table extends Migration
 		$this->execute("ALTER TABLE `metvuong`.`ad_building_project`   
   CHANGE `lng` `lng` FLOAT NULL,
   CHANGE `lat` `lat` FLOAT NULL;");
-		
-		$this->execute("ALTER TABLE `metvuong`.`ad_investor`   
-  DROP COLUMN `status`;");
     }
 
     /*
