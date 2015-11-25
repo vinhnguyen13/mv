dca4bb5349208232c3ba16e18de0343425f7b22a
Lệnh Quách <qtlenh@gmail.com>
2015-11-25 17:16:24 +0700

    cms ad

--- apps/metvuong/common/vendor/vsoft/ad/controllers/InvestorController.php
+++ apps/metvuong/common/vendor/vsoft/ad/controllers/InvestorController.php
@@ -36,6 +36,8 @@ class InvestorController extends Controller
 
     public function actionDelete($id)
     {
+    	Yii::$app->db->createCommand()->delete('ad_investor_building_project', 'investor_id = :investor_id', [':investor_id' => $id])->execute();
+    	
     	AdInvestor::findOne($id)->delete();
     
     	return $this->redirect(['index']);
