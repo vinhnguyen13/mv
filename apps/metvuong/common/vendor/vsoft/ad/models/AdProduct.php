f12b3795bb163405cc114551ee7b7fc9baa8a37f
Lệnh Quách <qtlenh@gmail.com>
2015-12-03 13:27:14 +0700

    dang tin

--- apps/metvuong/common/vendor/vsoft/ad/models/AdProduct.php
+++ apps/metvuong/common/vendor/vsoft/ad/models/AdProduct.php
@@ -10,4 +10,37 @@ class AdProduct extends AdProductBase
 {
 	const TYPE_FOR_SELL = 1;
 	const TYPE_FOR_RENT = 2;
+	
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'category_id' => 'Category ID',
+			'project_building_id' => 'Project Building ID',
+			'user_id' => 'User ID',
+			'city_id' => 'City ID',
+			'district_id' => 'District ID',
+			'ward_id' => 'Ward ID',
+			'street_id' => 'Street ID',
+			'type' => 'Hình thức',
+			'title' => 'Title',
+			'content' => 'Content',
+			'area' => 'Area',
+			'price' => 'Price',
+			'price_type' => 'Price Type',
+			'lng' => 'Lng',
+			'lat' => 'Lat',
+			'start_date' => 'Start Date',
+			'end_date' => 'End Date',
+			'score' => 'Score',
+			'view' => 'View',
+			'verified' => 'Verified',
+			'created_at' => 'Created At',
+			'updated_at' => 'Updated At',
+			'status' => 'Status',
+		];
+	}
 }
--- apps/metvuong/frontend/web/themes/metvuong1/views/ads/post.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/ads/post.php
@@ -2,165 +2,195 @@
 use yii\web\View;
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
-$this->title = Yii::t('express','We offer exeptional amenities and renowned white - glove services');
+use common\vendor\vsoft\ad\models\AdProduct;
+$this->title = Yii::t ( 'express', 'We offer exeptional amenities and renowned white - glove services' );
 
-Yii::$app->getView()->registerJsFile(Yii::$app->view->theme->baseUrl.'/resources/js/dang-tin.js', ['position'=>View::POS_END]);
+Yii::$app->getView ()->registerJsFile ( Yii::$app->view->theme->baseUrl . '/resources/js/dang-tin.js', ['position' => View::POS_END]);
+
+$type = [ 
+	AdProduct::TYPE_FOR_SELL => Yii::t ( 'post', 'Nhà đất bán' ),
+	AdProduct::TYPE_FOR_RENT => Yii::t ( 'post', 'Nhà đất cho thuê' ) 
+];
 ?>
- <div class="wrap-dangtin">
-                <div class="title-frm" clearfix>Đăng tin</div>
-                <div class="wrap-step-post clearfix">
-                    <form action="" id="frm-post-tin" class="form-horizontal">
-                        <ul id="progressbar" class="clearfix">
-                            <li class="active">
-                                <div>
-                                    <a href="#">1</a>
-                                    <span class="line-process"></span>
-                                    <div>Step 1</div>
-                                </div>
-                            </li>
-                            <li class="step-center">
-                                <div>
-                                    <a href="#">2</a>
-                                    <span class="line-process"></span>
-                                    <div>Step 2</div>
-                                </div>
-                            </li>
-                            <li class="text-left">
-                                <div>
-                                    <a href="#">3</a>
-                                    <span class="line-process"></span>
-                                    <div>Hoàn thành</div>
-                                </div>
-                            </li>
-                        </ul>
-                        <div class="fieldset clearfix" style="display:block;">
-                            <div class="form-group">
-                                <label for="" class="col-sm-2 control-label">Địa chỉ:</label>
-                                <div class="col-sm-10 group-item-frm">
-                                    <input type="email" class="form-control" id="" placeholder="Số nhà">
-                                    <select class="form-control">
-                                        <option selected>--Tên đường--</option>
-                                        <option>Nguyễn Chung Ngạn</option>
-                                        <option>Nguyễn Thị Minh Khai</option>
-                                        <option>Lê Thánh Tôn</option>
-                                    </select>
-                                    <select class="form-control mgB-0">
-                                        <option selected>--Phường/Xã--</option>
-                                        <option>Phường 1</option>
-                                        <option>Phường 2</option>
-                                        <option>Phường 3</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group text-inline">
-                                <label for="" class="col-sm-2 control-label">Diện tích:</label>
-                                <div class="col-sm-10">
-                                    <div class="inline-group col-xs-6">
-                                        <input type="email" class="form-control" id="">
-                                    </div>
-                                    <div class="inline-group col-xs-6 pdR-0">
-                                        <span>m<sup>2</sup></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="" class="col-sm-2 control-label">Giá:</label>
-                                <div class="col-sm-10 group-item-frm">
-                                    <div class="inline-group col-xs-6">
-                                        <input type="email" class="form-control" id="">
-                                    </div>
-                                    <div class="inline-group col-xs-6 pdR-0">
-                                        <select class="form-control">
-                                        <option selected>--Đơn vị--</option>
-                                        <option>Thỏa thuận</option>
-                                        <option>Trăm nghìn/tháng</option>
-                                        <option>Triệu/tháng</option>
-                                    </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <button type="button" class="btn btn-primary btn-common mgT-15 next action-button pull-right">Tiếp theo<em class="fa fa-chevron-right"></em></button>
-                        </div>
-                        <div class="fieldset clearfix" style="display:none;">
-                            <div class="form-group">
-                                <div class="title-sub-frm">Thông tin mở rộng</div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row row-group-inline">
-                                    <div class="col-xs-3">
-                                        <select class="form-control">
-                                            <option>Quận/Huyện(*)</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <select class="form-control">
-                                            <option>Phường/Xã(*)</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <select class="form-control">
-                                            <option>Đường/Phố(*)</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <select class="form-control">
-                                            <option>Dự án</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <textarea class="form-control" rows="3" placeholder="Nội thất..."></textarea>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group">
-                                <div class="title-sub-frm">Thông tin liện hệ</div>
-                            </div>
-                            <div class="form-group">
-                                <div class="row row-group-inline">
-                                    <div class="col-xs-3">
-                                        <input type="text" class="form-control" placeholder="Tên liên hệ">
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="text" class="form-control" placeholder="Điện thoại">
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="text" class="form-control" placeholder="Di động(*)">
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <input type="text" class="form-control" placeholder="Email">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Địa chỉ">
-                            </div>
-                            <div class="form-group">
-                                <div class="title-sub-frm">Bản đồ</div>
-                            </div>
-                            <div class="form-group">
-                                <p>Để tăng độ tin cậy và tin rao được nhiều người quan tâm hơn, hãy sửa vị trí tin rao của bạn trên bản đồ bằng cách kéo icon<em class="fa fa-map-marker"></em>tới đúng vị trí của tin rao.</p>
-                                <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0T2NLikpdTERKxE8d61aX_E&amp;key=AIzaSyDgukAnWQNq0fitebULUbottG5gvK64OCQ" allowfullscreen=""></iframe>
-                            </div>
-                            <button type="button" class="btn btn-primary btn-common mgT-15 next action-button pull-right">Tiếp theo<em class="fa fa-chevron-right"></em></button>
-                            <button type="button" class="btn btn-primary btn-common mgT-15 previous action-button pull-right"><em class="fa fa-chevron-left"></em>Quay lại</button>
-                        </div>
-                        <div class="fieldset clearfix finish-post" style="display:none;">
-                            text
-                            <button type="button" class="btn btn-primary btn-common mgT-15 next action-button pull-right">Đăng bài</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="wrap-dangtin">
+	<div class="title-frm" clearfix>Đăng tin</div>
+	<div class="wrap-step-post clearfix">
+		<?php $form = ActiveForm::begin ( [ 
+				'id' => 'frm-post-tin',
+				'options' => [ 
+					'spellcheck' => 'false',
+					'class' => 'form-horizontal'
+				]]); ?>
+			<ul id="progressbar" class="clearfix">
+				<li class="active">
+					<div>
+						<a href="#">1</a> <span class="line-process"></span>
+						<div>Step 1</div>
+					</div>
+				</li>
+				<li class="step-center">
+					<div>
+						<a href="#">2</a> <span class="line-process"></span>
+						<div>Step 2</div>
+					</div>
+				</li>
+				<li class="text-left">
+					<div>
+						<a href="#">3</a> <span class="line-process"></span>
+						<div>Hoàn thành</div>
+					</div>
+				</li>
+			</ul>
+			<div class="fieldset clearfix" style="display: block;">
+				<?= $form->field($model, 'type', [
+						'labelOptions' => ['class' => 'col-sm-2 control-label'],
+						'inputOptions' => ['class' => 'col-xs-6 form-control'],
+						'template' => '{label}<div class="col-sm-10 group-item-frm"><div class="inline-group pdR-0">{input}</div></div>{hint}{error}']
+					)->dropDownList($type) ?>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Địa chỉ:</label>
+					<div class="col-sm-10 group-item-frm">
+						<input type="email" class="form-control" id=""
+							placeholder="Số nhà"> <select class="form-control">
+							<option selected>--Tên đường--</option>
+							<option>Nguyễn Chung Ngạn</option>
+							<option>Nguyễn Thị Minh Khai</option>
+							<option>Lê Thánh Tôn</option>
+						</select> <select class="form-control mgB-0">
+							<option selected>--Phường/Xã--</option>
+							<option>Phường 1</option>
+							<option>Phường 2</option>
+							<option>Phường 3</option>
+						</select>
+					</div>
+				</div>
+				<div class="form-group text-inline">
+					<label for="" class="col-sm-2 control-label">Diện tích:</label>
+					<div class="col-sm-10">
+						<div class="inline-group col-xs-6">
+							<input type="email" class="form-control" id="">
+						</div>
+						<div class="inline-group col-xs-6 pdR-0">
+							<span>m<sup>2</sup></span>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="" class="col-sm-2 control-label">Giá:</label>
+					<div class="col-sm-10 group-item-frm">
+						<div class="inline-group col-xs-6">
+							<input type="email" class="form-control" id="">
+						</div>
+						<div class="inline-group col-xs-6 pdR-0">
+							<select class="form-control">
+								<option selected>--Đơn vị--</option>
+								<option>Thỏa thuận</option>
+								<option>Trăm nghìn/tháng</option>
+								<option>Triệu/tháng</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<button type="button"
+					class="btn btn-primary btn-common mgT-15 next action-button pull-right">
+					Tiếp theo<em class="fa fa-chevron-right"></em>
+				</button>
+			</div>
+			<div class="fieldset clearfix" style="display: none;">
+				<div class="form-group">
+					<div class="title-sub-frm">Thông tin mở rộng</div>
+				</div>
+				<div class="form-group">
+					<div class="row row-group-inline">
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Quận/Huyện(*)</option>
+							</select>
+						</div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Phường/Xã(*)</option>
+							</select>
+						</div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Đường/Phố(*)</option>
+							</select>
+						</div>
+						<div class="col-xs-3">
+							<select class="form-control">
+								<option>Dự án</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div class="form-group">
+					<textarea class="form-control" rows="3" placeholder="Nội thất..."></textarea>
+				</div>
+				<div class="clearfix"></div>
+				<div class="form-group">
+					<div class="title-sub-frm">Thông tin liện hệ</div>
+				</div>
+				<div class="form-group">
+					<div class="row row-group-inline">
+						<div class="col-xs-3">
+							<input type="text" class="form-control" placeholder="Tên liên hệ">
+						</div>
+						<div class="col-xs-3">
+							<input type="text" class="form-control" placeholder="Điện thoại">
+						</div>
+						<div class="col-xs-3">
+							<input type="text" class="form-control" placeholder="Di động(*)">
+						</div>
+						<div class="col-xs-3">
+							<input type="text" class="form-control" placeholder="Email">
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Địa chỉ">
+				</div>
+				<div class="form-group">
+					<div class="title-sub-frm">Bản đồ</div>
+				</div>
+				<div class="form-group">
+					<p>
+						Để tăng độ tin cậy và tin rao được nhiều người quan tâm hơn, hãy
+						sửa vị trí tin rao của bạn trên bản đồ bằng cách kéo icon<em
+							class="fa fa-map-marker"></em>tới đúng vị trí của tin rao.
+					</p>
+					<iframe width="600" height="450" frameborder="0" style="border: 0"
+						src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ0T2NLikpdTERKxE8d61aX_E&amp;key=AIzaSyDgukAnWQNq0fitebULUbottG5gvK64OCQ"
+						allowfullscreen=""></iframe>
+				</div>
+				<button type="button"
+					class="btn btn-primary btn-common mgT-15 next action-button pull-right">
+					Tiếp theo<em class="fa fa-chevron-right"></em>
+				</button>
+				<button type="button"
+					class="btn btn-primary btn-common mgT-15 previous action-button pull-right">
+					<em class="fa fa-chevron-left"></em>Quay lại
+				</button>
+			</div>
+			<div class="fieldset clearfix finish-post" style="display: none;">
+				text
+				<button type="button"
+					class="btn btn-primary btn-common mgT-15 next action-button pull-right">Đăng
+					bài</button>
+			</div>
+		<?php $form->end()?>
+	</div>
+</div>
+
 <div class="social-share">
-    <ul>
-        <li><a href="#"><em class="fa fa-facebook"></em></a></li>
-        <li><a href="#"><em class="fa fa-twitter"></em></a></li>
-        <li><a href="#"><em class="fa fa-instagram"></em></a></li>
-        <li><a href="#"><em class="fa fa-google-plus"></em></a></li>
-        <li><a href="#"><em class="fa fa-youtube-play"></em></a></li>
-        <li><a href="#"><em class="fa fa-pinterest"></em></a></li>
-        <li><a href="#"><em class="fa fa-linkedin"></em></a></li>
-    </ul>
+	<ul>
+		<li><a href="#"><em class="fa fa-facebook"></em></a></li>
+		<li><a href="#"><em class="fa fa-twitter"></em></a></li>
+		<li><a href="#"><em class="fa fa-instagram"></em></a></li>
+		<li><a href="#"><em class="fa fa-google-plus"></em></a></li>
+		<li><a href="#"><em class="fa fa-youtube-play"></em></a></li>
+		<li><a href="#"><em class="fa fa-pinterest"></em></a></li>
+		<li><a href="#"><em class="fa fa-linkedin"></em></a></li>
+	</ul>
 </div>
