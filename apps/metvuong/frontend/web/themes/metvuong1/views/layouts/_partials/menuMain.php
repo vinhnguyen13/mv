47d9ce7a2766eaf906ac99b2d8e91b420796fc34
dientruong <dien.truong@trungthuygroup.vn>
2015-12-07 09:51:03 +0700

    update style box search

--- apps/metvuong/frontend/web/themes/metvuong1/views/layouts/_partials/menuMain.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/layouts/_partials/menuMain.php
@@ -1,16 +1,18 @@
 <?php
 
 ?>
-<ul class="pull-right menu-home">
-    <?php if(Yii::$app->user->isGuest){?>
-        <li><a href="#" data-toggle="modal" data-target="#frmRegister"><em class="icon-user"></em>Đăng ký</a></li>
-        <li><a href="#" data-toggle="modal" data-target="#frmLogin"><em class="icon-key"></em>Đăng nhập</a></li>
-    <?php }else{?>
-        <li><a data-method="post" href="<?=\yii\helpers\Url::to(['/member/logout'])?>">
-                <em class="icon-user"></em>
-                <?=!empty(Yii::$app->user->identity->profile->name) ? Yii::$app->user->identity->profile->name : Yii::$app->user->identity->email;?>
-        </a></li>
-    <?php }?>
-    <li class="lang-icon icon-en"><a href="<?=\yii\helpers\Url::current(['language-change'=>'en-US'])?>"></a></li>
-    <li class="lang-icon icon-vi"><a href="<?=\yii\helpers\Url::current(['language-change'=>'vi-VN'])?>"></a></li>
-</ul>
+<div class="pull-right wrap-menu-option">
+    <ul class="pull-right menu-home">
+        <?php if(Yii::$app->user->isGuest){?>
+            <li><a href="#" data-toggle="modal" data-target="#frmRegister"><em class="icon-user"></em>Đăng ký</a></li>
+            <li><a href="#" data-toggle="modal" data-target="#frmLogin"><em class="icon-key"></em>Đăng nhập</a></li>
+        <?php }else{?>
+            <li><a data-method="post" href="<?=\yii\helpers\Url::to(['/member/logout'])?>">
+                    <em class="icon-user"></em>
+                    <?=!empty(Yii::$app->user->identity->profile->name) ? Yii::$app->user->identity->profile->name : Yii::$app->user->identity->email;?>
+            </a></li>
+        <?php }?>
+        <li class="lang-icon icon-en"><a href="<?=\yii\helpers\Url::current(['language-change'=>'en-US'])?>"></a></li>
+        <li class="lang-icon icon-vi"><a href="<?=\yii\helpers\Url::current(['language-change'=>'vi-VN'])?>"></a></li>
+    </ul>
+</di>
