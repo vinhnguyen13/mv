b0e085ab4a1c4dc59f169364af9efd950babbb0a
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-12-09 17:01:48 +0700

    test store proceduce

--- apps/metvuong/frontend/controllers/TestController.php
+++ apps/metvuong/frontend/controllers/TestController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 use dektrium\user\Mailer;
 use vsoft\user\models\User;
 use Yii;
+use yii\db\mssql\PDO;
 use yii\helpers\Url;
 use vsoft\news\models\CmsShow;
 use vsoft\ad\models\AdBuildingProject;
@@ -20,6 +21,25 @@ class TestController extends \yii\web\Controller
         print_r($chk);
         echo "</pre>";
         exit;
+    }
+
+    public function actionNews()
+    {
+        $sql =  'CALL getContent (1)' ;
+        $command = Yii::$app->db->createCommand($sql);
+        $news = $command->queryAll();
+
+        $news = CmsShow::findBySql('CALL getContent (1)')->all();
+        if(!empty($news)){
+            echo 'Begin ...<br />';
+            foreach($news as $idx => $new){
+                echo "-------- ID: {$new->id} Title: {$new->title} <br />";
+                flush();
+                ob_flush();
+                sleep(1);
+            }
+            echo 'End ...<br />';
+        }
     }
 
 }
