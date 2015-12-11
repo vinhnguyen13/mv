d243353221565ad57b3b7c2865fbf6e71c71726c
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-12-11 14:15:26 +0700

    user management

--- apps/metvuong/frontend/web/themes/metvuong1/views/user-management/chart/ads.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/user-management/chart/ads.php
@@ -18,11 +18,11 @@ Yii::$app->getView()->registerJsFile('http://code.highcharts.com/modules/exporti
     $(function () {
         $('#chartAds').highcharts({
             title: {
-                text: 'Monthly Average Temperature',
+                text: 'Monthly Average Real Estate',
                 x: -20 //center
             },
             subtitle: {
-                text: 'Source: WorldClimate.com',
+                text: 'MetVuong.com',
                 x: -20
             },
             xAxis: {
@@ -31,7 +31,7 @@ Yii::$app->getView()->registerJsFile('http://code.highcharts.com/modules/exporti
             },
             yAxis: {
                 title: {
-                    text: 'Temperature (°C)'
+                    text: 'Visitor'
                 },
                 plotLines: [{
                     value: 0,
@@ -40,7 +40,7 @@ Yii::$app->getView()->registerJsFile('http://code.highcharts.com/modules/exporti
                 }]
             },
             tooltip: {
-                valueSuffix: '°C'
+                valueSuffix: ' Users'
             },
             legend: {
                 layout: 'vertical',
@@ -49,16 +49,16 @@ Yii::$app->getView()->registerJsFile('http://code.highcharts.com/modules/exporti
                 borderWidth: 0
             },
             series: [{
-                name: 'Tokyo',
+                name: '21 Lê Thánh Tôn',
                 data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
             }, {
-                name: 'New York',
+                name: '57 Tôn Đản',
                 data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
             }, {
-                name: 'Berlin',
+                name: '23 Pastuer',
                 data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
             }, {
-                name: 'London',
+                name: '11 Nguyễn Văn Trỗi',
                 data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
             }]
         });
