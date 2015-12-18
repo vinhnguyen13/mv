27b6675c969ffb89efc99e9c26fa3d179c9eb3db
Nhut Tran <nhut.tran@trungthuygroup.vn>
2015-12-18 15:21:59 +0700

    add 404 page

---
+++ apps/metvuong/frontend/web/themes/metvuong1/views/news/404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nhut Tran
+ * Date: 12/18/2015 10:48 AM
+ */
+?>
+<div class="row">
+    <div class="col-sm-8 col-lg-9 col-right-home detail-news">
+        <div class="wrap-detail-article">
+            <input id="404" type="hidden" value="page not found">
+            <img src="http://www.devbright.net/assets/images/404.png" alt="Page not found. Please, try again!" class="right">
+        </div>
+    </div>
+</div>
