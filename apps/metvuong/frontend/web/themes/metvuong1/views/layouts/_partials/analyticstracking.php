95146ca378c97451056d848a012f7bcc39777c70
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-12-18 09:51:48 +0700

    GA

--- apps/metvuong/frontend/web/themes/metvuong1/views/layouts/_partials/analyticstracking.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/layouts/_partials/analyticstracking.php
@@ -1,3 +1,6 @@
+<?php
+if(!YII_DEBUG){
+?>
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -8,3 +11,6 @@
     ga('send', 'pageview');
 
 </script>
+<?php
+}
+?>
