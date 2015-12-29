da5a5e655cf7316761df307102320b79f699f0b4
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2015-12-29 15:16:12 +0700

    rating

--- apps/metvuong/frontend/web/themes/metvuong1/resources/js/search-map.js
+++ apps/metvuong/frontend/web/themes/metvuong1/resources/js/search-map.js
@@ -134,12 +134,12 @@ function start() {
 			_ratingContainer.find('ul > li').removeClass('active');
 			_ratingContainer.find('ul > li').each(function(i) {
 				if(i <= _index){
-					$(this).addClass('active');
+					$(this).addClass('hover-star');
 				}
 			});
-		}).on('mouseleave', '> li', function() {
+		}).on('mouseleave', '.rating li', function() {
 			var _ratingContainer = $(this).closest('.rating');
-			_ratingContainer.find('ul > li').removeClass('active');
+			_ratingContainer.find('ul > li').removeClass('hover-star');
 		});
 		
 		var hoverTimeout;
