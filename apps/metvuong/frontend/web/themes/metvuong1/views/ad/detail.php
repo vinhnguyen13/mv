cfca8192478d6f7b727ff422a58263d87846611c
dientruong <dien.truong@trungthuygroup.vn>
2015-12-30 16:35:52 +0700

    update style star

--- apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
@@ -237,13 +237,25 @@
                                 <div class="function-listing clearfix">
                                     <p>Đánh giá</p>
                                     <div class="rating">
-                                        <ul class="clearfix" data-id="<?=$product->id;?>" data-url="<?=Url::to(['/ad/rating', 'type'=>'']);?>">
+                                        <!-- <ul class="clearfix" data-id="<?=$product->id;?>" data-url="<?=Url::to(['/ad/rating', 'type'=>'']);?>">
                                             <li class="active"><a href="#"><em class="fa fa-star-o"></em></a></li>
                                             <li class="active"><a href="#"><em class="fa fa-star-o"></em></a></li>
                                             <li class="active"><a href="#"><em class="fa fa-star-o"></em></a></li>
                                             <li><a href="#"><em class="fa fa-star-o"></em></a></li>
                                             <li><a href="#"><em class="fa fa-star-o"></em></a></li>
-                                        </ul>
+                                        </ul> -->
+                                        <fieldset class="rate">
+                                            <input type="radio" id="rating10" name="rating" value="10" /><label for="rating10" title="5 stars"></label>
+                                            <input type="radio" id="rating9" name="rating" value="9" /><label class="half" for="rating9" title="4 1/2 stars"></label>
+                                            <input type="radio" id="rating8" name="rating" value="8" /><label for="rating8" title="4 stars"></label>
+                                            <input type="radio" id="rating7" name="rating" value="7" /><label class="half" for="rating7" title="3 1/2 stars"></label>
+                                            <input type="radio" id="rating6" name="rating" value="6" /><label for="rating6" title="3 stars"></label>
+                                            <input type="radio" id="rating5" name="rating" value="5" /><label class="half" for="rating5" title="2 1/2 stars"></label>
+                                            <input type="radio" id="rating4" name="rating" value="4" /><label for="rating4" title="2 stars"></label>
+                                            <input type="radio" id="rating3" name="rating" value="3" /><label class="half" for="rating3" title="1 1/2 stars"></label>
+                                            <input type="radio" id="rating2" name="rating" value="2" /><label for="rating2" title="1 star"></label>
+                                            <input type="radio" id="rating1" name="rating" value="1" /><label class="half" for="rating1" title="1/2 star"></label>
+                                        </fieldset>
                                     </div>
                                     <div class="dropdown report-listing">
                                         <button class="btn btn-default dropdown-toggle" type="button" id="btn-report" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
