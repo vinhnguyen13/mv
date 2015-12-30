72c42c16da697c87f9952efd22b6ed827b6cb80e
dientruong <dien.truong@trungthuygroup.vn>
2015-12-30 16:57:20 +0700

    update style star

--- apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
@@ -194,16 +194,16 @@
                                             <li><a href="#"><em class="fa fa-star-o"></em></a></li>
                                         </ul> -->
                                         <fieldset class="rate">
-                                            <input type="radio" id="rating10" name="rating" value="10" /><label for="rating10" title="5 stars"></label>
-                                            <input type="radio" id="rating9" name="rating" value="9" /><label class="half" for="rating9" title="4 1/2 stars"></label>
-                                            <input type="radio" id="rating8" name="rating" value="8" /><label for="rating8" title="4 stars"></label>
-                                            <input type="radio" id="rating7" name="rating" value="7" /><label class="half" for="rating7" title="3 1/2 stars"></label>
-                                            <input type="radio" id="rating6" name="rating" value="6" /><label for="rating6" title="3 stars"></label>
-                                            <input type="radio" id="rating5" name="rating" value="5" /><label class="half" for="rating5" title="2 1/2 stars"></label>
-                                            <input type="radio" id="rating4" name="rating" value="4" /><label for="rating4" title="2 stars"></label>
-                                            <input type="radio" id="rating3" name="rating" value="3" /><label class="half" for="rating3" title="1 1/2 stars"></label>
-                                            <input type="radio" id="rating2" name="rating" value="2" /><label for="rating2" title="1 star"></label>
-                                            <input type="radio" id="rating1" name="rating" value="1" /><label class="half" for="rating1" title="1/2 star"></label>
+                                            <input type="radio" id="rating-contact-10" name="rating-contact" value="10" /><label for="rating-contact-10" title="5 stars"></label>
+                                            <input type="radio" id="rating-contact-9" name="rating-contact" value="9" /><label class="half" for="rating-contact-9" title="4 1/2 stars"></label>
+                                            <input type="radio" id="rating-contact-8" name="rating-contact" value="8" /><label for="rating-contact-8" title="4 stars"></label>
+                                            <input type="radio" id="rating-contact-7" name="rating-contact" value="7" /><label class="half" for="rating-contact-7" title="3 1/2 stars"></label>
+                                            <input type="radio" id="rating-contact-6" name="rating-contact" value="6" /><label for="rating-contact-6" title="3 stars"></label>
+                                            <input type="radio" id="rating-contact-5" name="rating-contact" value="5" /><label class="half" for="rating-contact-5" title="2 1/2 stars"></label>
+                                            <input type="radio" id="rating-contact-4" name="rating-contact" value="4" /><label for="rating-contact-4" title="2 stars"></label>
+                                            <input type="radio" id="rating-contact-3" name="rating-contact" value="3" /><label class="half" for="rating-contact-3" title="1 1/2 stars"></label>
+                                            <input type="radio" id="rating-contact-2" name="rating-contact" value="2" /><label for="rating-contact-2" title="1 star"></label>
+                                            <input type="radio" id="rating-contact-1" name="rating-contact" value="1" /><label class="half" for="rating-contact-1" title="1/2 star"></label>
                                         </fieldset>
                                     </div>
                                     <p class="title-contact">Liên hệ</p>
