149539fd6fea04da61358d00ebe124a707ee893c
dientruong <dien.truong@trungthuygroup.vn>
2016-01-05 14:01:39 +0700

    update listing page

--- apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/ad/detail.php
@@ -47,6 +47,7 @@
 	}
 ?>
 <div id="detail-listing">
+
         <div class="detail-slide">
             <div class="modal-content">
                 <div class="modal-header">
@@ -112,9 +113,49 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="modal fade" id="box-share" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="wrap-modal clearfix">
+                                            <h3>Chia sẻ</h3>
+                                            <form>
+                                                <div class="form-group">
+                                                    <label for="recipient-email">Email người nhận</label>
+                                                    <input type="text" class="form-control" id="recipient-email" placeholder="Email người nhận...">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="your-email">Email của bạn</label>
+                                                    <input type="text" class="form-control" id="your-email" placeholder="Email của bạn...">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="">Nội dụng</label>
+                                                    <textarea class="form-control" name="" id="" cols="30" rows="5" placeholder="Nội dụng chia sẻ..."></textarea>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button class="btn btn-common">Gửi email</button>
+                                                </div>
+                                                <ul class="share-social clearfix">
+                                                    <li>Chia sẻ kết nối</li>
+                                                    <li><a href="#" class="logo-social fb-icon"></a></li>
+                                                    <li><a href="#" class="logo-social twe-icon"></a></li>
+                                                    <li><a href="#" class="logo-social g-icon"></a></li>
+                                                </ul>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
-                <div class="modal-body">
+                <div id="modal-detail-listting" class="modal-body">
                     <div class="wrap-modal clearfix">
                     	<?php
                     		if(!empty($images)) :
