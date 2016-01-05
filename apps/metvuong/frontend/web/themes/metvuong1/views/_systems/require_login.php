222d021eee2ee3251deceee8c0d9d05d8bad24ea
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-01-05 14:25:31 +0700

    require login view

--- apps/metvuong/frontend/web/themes/metvuong1/views/_systems/require_login.php
+++ apps/metvuong/frontend/web/themes/metvuong1/views/_systems/require_login.php
@@ -1,1 +1,7 @@
-Bạn cần phải <a href="#" data-toggle="modal" data-target="#frmLogin">Đăng nhập</a> để đăng tin
+<div class="row">
+    <div class="col-xs-12">
+        <div class="alert alert-danger">
+            <p class="text-center">Bạn cần phải <a href="#" data-toggle="modal" data-target="#frmLogin">đăng nhập</a> để đăng tin</p>
+        </div>
+    </div>
+</div>
