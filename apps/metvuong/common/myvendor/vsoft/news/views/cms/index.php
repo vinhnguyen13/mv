d8e1ee43beb59adcb41e303c556f833d1bd93243
Nhut Tran <nhut.tran@trungthuygroup.vn>
2016-01-11 13:36:52 +0700

    add link more detail

--- apps/metvuong/common/myvendor/vsoft/news/views/cms/index.php
+++ apps/metvuong/common/myvendor/vsoft/news/views/cms/index.php
@@ -60,7 +60,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         return $model->getCatalog()->one()->title;
                     return '';
                 },
-                'filter' => Html::activeDropDownList($searchModel, 'catalog_id', ArrayHelper::map(\vsoft\news\models\CmsCatalog::find()->where(['not in', 'id', [Yii::$app->params['buildingCatID'],Yii::$app->params['newsCatID'] ]])
+                'filter' => Html::activeDropDownList($searchModel, 'catalog_id', ArrayHelper::map(\vsoft\news\models\CmsCatalog::find()->where(['not in', 'id', [!empty(Yii::$app->params['buildingCatID']) ? Yii::$app->params['buildingCatID'] : 1, !empty(Yii::$app->params['newsCatID']) ? Yii::$app->params['newsCatID'] : 2]])
                     ->andWhere('status = :status', [':status' => 1])->asArray()->all(), 'id', 'title'),['class'=>'form-control','prompt' => 'All']),
             ],
 
