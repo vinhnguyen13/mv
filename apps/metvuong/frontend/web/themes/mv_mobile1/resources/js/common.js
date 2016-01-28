ae1c67b5e3b494237eb7864ef3d8ea3b34219dcc
dientruong <dien.truong@trungthuygroup.vn>
2016-01-28 11:40:33 +0700

    update layout html mobile

--- apps/metvuong/frontend/web/themes/mv_mobile1/resources/js/common.js
+++ apps/metvuong/frontend/web/themes/mv_mobile1/resources/js/common.js
@@ -8,3 +8,21 @@ $(document).ready(function() {
 	$(document).on('click','.link-login', function() {$('a[href=#tab-signin]').trigger('click');});
 	$(document).on('click','.link-regis', function() {$('a[href=#tab-signup]').trigger('click');});
 });
+
+$.fn.radio = function (options) {
+
+    return this.each(function() {
+        var defaults = {
+            
+        },
+        sc = {},
+        el = $(this);
+
+        if ( el.length == 0 ) return el;
+
+        sc.settings = $.extend({}, defaults, options);
+
+        
+
+    });
+}
