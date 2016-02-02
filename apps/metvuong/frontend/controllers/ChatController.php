01fc0753b8ff8ac86b7fa8cdf75e354f0306a508
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-02-02 13:16:54 +0700

    chat mobile

--- apps/metvuong/frontend/controllers/ChatController.php
+++ apps/metvuong/frontend/controllers/ChatController.php
@@ -1,26 +1,20 @@
 <?php
 
 namespace frontend\controllers;
-use common\components\Util;
-use dektrium\user\Mailer;
-use Elasticsearch\ClientBuilder;
 use frontend\components\Controller;
-use frontend\models\Elastic;
-use frontend\models\Tracking;
-use GuzzleHttp\Ring\Client\CurlHandler;
-use vsoft\tracking\models\base\AdProductVisitor;
-use vsoft\user\models\User;
 use Yii;
-use yii\db\mssql\PDO;
 use yii\helpers\Url;
-use vsoft\news\models\CmsShow;
-use vsoft\ad\models\AdBuildingProject;
+use yii\web\NotFoundHttpException;
+use yii\web\UnauthorizedHttpException;
 
 class ChatController extends Controller
 {
     public $layout = '@app/views/layouts/layout';
 
     public function actionIndex(){
+        if(Yii::$app->user->isGuest) {
+            return $this->render('/_systems/require_login');
+        }
         return $this->render('index');
     }
 }
