dedd1438f92527c671d891b8e20a50499170c1b5
Lệnh Quách <qtlenh@gmail.com>
2016-02-03 13:37:42 +0700

    dang tin

--- apps/metvuong/frontend/web/themes/mv_mobile1/views/ad/_partials/search-form.php
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/ad/_partials/search-form.php
@@ -1,112 +1,141 @@
 <form>
 	<input type="hidden" name="sort" id="sort" class="value_selected" value="price" />
 	<div class="search-subpage clearfix">
-		<div class="search-fill">
-			<input class="style-click" type="text" placeholder="Tìm kiếm nhanh...">
-			<button type="submit" id="btn-search" class="style-click">
-				<span class="icon"></span>
-			</button>
-			<a href="#" class="advande-search-btn style-click"><span
-				class="bd-left"></span><span class="bd-right"></span></a>
-		</div>
-		<div class="advande-search">
-			<div class="each-advande choice_price_dt">
-				<div class="value-selected price-search style-click">
-					Giá
-					<div>
-						<span class="tu">từ</span> <span class="wrap-min">1 tỷ</span> <span
-							class="trolen">trở lên</span> <span class="den">đến</span> <span
-							class="wrap-max">4 tỷ</span> <span class="troxuong">trở xuống</span>
+					<div class="search-fill">
+						<div class="fillter-fast">
+							<div class="val-selected style-click"><span class="selected selected_val">Tìm kiếm nhanh...</span></div>
+							<div class="item-dropdown hide-dropdown">
+								<strong>Bạn hãy chọn quận</strong>
+								<ul class="clearfix">
+									<li><a href="#">Quận 1</a></li>
+									<li><a href="#">Quận 2</a></li>
+									<li><a href="#">Quận 3</a></li>
+									<li><a href="#">Quận 4</a></li>
+									<li><a href="#">Quận 5</a></li>
+									<li><a href="#">Quận 6</a></li>
+									<li><a href="#">Quận 7</a></li>
+								</ul>
+								<input type="hidden" id="quan-huyen" class="">
+							</div>
+						</div>
+						<a href="#" class="advande-search-btn style-click"><span class="bd-left"></span><span class="bd-right"></span></a>
+					</div>
+					<div class="advande-search">
+						<div class="each-advande choice_price_dt" data-item-minmax="prices">
+							<div class="value-selected price-search style-click">
+								Giá 
+								<div>
+									<span class="tu">từ</span>
+									<span class="wrap-min">1 tỷ</span>
+									<span class="trolen">trở lên</span>
+									<span class="den">đến</span>
+									<span class="wrap-max">4 tỷ</span>
+									<span class="troxuong">trở xuống</span>
+								</div>
+							</div>
+
+							<div class="item-advande">
+								<div class="box-input clearfix">
+									<input readonly="readonly" class="min-max active min-val" name="price-min" id="" type="text" placeholder="Thấp nhất">
+									<span class="text-center">Đến</span>
+									<input readonly="readonly" class="min-max max-val" name="price-max" id="" type="text" placeholder="Cao nhất">
+									<input type="hidden" id="priceMin" />
+									<input type="hidden" id="priceMax" />
+								</div>
+								<div class="filter-minmax clearfix">
+									<ul data-wrap-minmax="min-val" class="wrap-minmax"></ul>
+									<ul data-wrap-minmax="max-val" class="wrap-minmax"></ul>
+								</div>
+							</div>
+						</div>
+						<div class="each-advande choice_price_dt" data-item-minmax="area">
+							<div class="value-selected dt-search style-click">
+								Diện tích
+								<div>
+									<span class="tu">từ</span>
+									<span class="wrap-min">1 tỷ</span>
+									<span class="trolen">trở lên</span>
+									<span class="den">đến</span>
+									<span class="wrap-max">4 tỷ</span>
+									<span class="troxuong">trở xuống</span>
+								</div>
+							</div>
+							<div class="item-advande">
+								<div class="box-input clearfix">
+									<input readonly="readonly" class="min-max active min-val" name="price-min" id="" type="text" placeholder="Thấp nhất">
+									<span class="text-center">Đến</span>
+									<input readonly="readonly" class="min-max max-val" name="price-max" id="" type="text" placeholder="Cao nhất">
+									<input type="hidden" id="dtMin" />
+									<input type="hidden" id="dtMax" />
+								</div>
+								<div class="filter-minmax clearfix">
+									<ul data-wrap-minmax="min-val" class="wrap-minmax"></ul>
+									<ul data-wrap-minmax="max-val" class="wrap-minmax"></ul>
+								</div>
+							</div>
+						</div>
+						<div class="each-advande clearfix">
+							<div class="col-xs-6 num-phongngu">
+								<div class="value-selected style-click val-selected" data-text-add="Phòng ngủ trở lên"><span class="selected">Phòng ngủ</span></div>
+
+								<div class="item-advande item-dropdown item-bed-bath">
+									<ul class="clearfix">
+										<li><a href="#">1</a></li>
+										<li><a href="#">2</a></li>
+										<li><a href="#">3</a></li>
+										<li><a href="#">4</a></li>
+										<li><a href="#">5</a></li>
+									</ul>
+									<input type="hidden" id="val-bed" class="value_selected" />
+								</div>
+							</div>
+							<div class="col-xs-6 num-phongtam">
+								<div class="value-selected style-click val-selected" data-text-add="Phòng tắm trở lên"><span class="selected">Phòng tắm</span></div>
+								<div class="item-advande item-dropdown item-bed-bath">
+									<ul class="clearfix">
+										<li><a href="#">1</a></li>
+										<li><a href="#">2</a></li>
+										<li><a href="#">3</a></li>
+										<li><a href="#">4</a></li>
+										<li><a href="#">5</a></li>
+									</ul>
+									<input type="hidden" id="val-bath" class="value_selected" />
+								</div>
+							</div>
+						</div>
+						<div class="each-advande row">
+							<div class="col-xs-12 other-fill">
+								<div class="value-selected style-click">Thêm tuỳ chọn</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="item-advande row">
-					<div class="col-xs-5">
-						<input type="number"
-							class="form-control style-click input-min min-max" id=""
-							placeholder="Thấp nhất">
-					</div>
-					<div class="col-xs-2 from-to-value">đến</div>
-					<div class="col-xs-5">
-						<input type="number"
-							class="form-control style-click input-max min-max" id=""
-							placeholder="Cao nhất">
-					</div>
-				</div>
-			</div>
-			<div class="each-advande choice_price_dt">
-				<div class="value-selected dt-search style-click">
-					Diện tích
-					<div>
-						<span class="tu">từ</span> <span class="wrap-min">1 tỷ</span> <span
-							class="trolen">trở lên</span> <span class="den">đến</span> <span
-							class="wrap-max">4 tỷ</span> <span class="troxuong">trở xuống</span>
-					</div>
-				</div>
-				<div class="item-advande row">
-					<div class="col-xs-5">
-						<input type="number"
-							class="form-control style-click min-max input-min" id=""
-							placeholder="Thấp nhất">
-					</div>
-					<div class="col-xs-2 from-to-value">đến</div>
-					<div class="col-xs-5">
-						<input type="number"
-							class="form-control style-click min-max input-max" id=""
-							placeholder="Cao nhất">
-					</div>
-				</div>
-			</div>
-			<div class="each-advande clearfix">
-				<div class="col-xs-6 num-phongngu">
-					<div class="value-selected style-click val-selected"
-						data-text-add="Phòng ngủ trở lên">
-						<span class="selected">Phòng ngủ</span>
-					</div>
-	
-					<div class="item-advande item-dropdown item-bed-bath">
-						<ul class="clearfix">
-							<li><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">4</a></li>
-							<li><a href="#">5</a></li>
-						</ul>
-						<input type="hidden" id="val-bed" class="value_selected" />
-					</div>
-				</div>
-				<div class="col-xs-6 num-phongtam">
-					<div class="value-selected style-click val-selected"
-						data-text-add="Phòng tắm trở lên">
-						<span class="selected">Phòng tắm</span>
-					</div>
-					<div class="item-advande item-dropdown item-bed-bath">
-						<ul class="clearfix">
-							<li><a href="#">1</a></li>
-							<li><a href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">4</a></li>
-							<li><a href="#">5</a></li>
-						</ul>
-						<input type="hidden" id="val-bath" class="value_selected" />
-					</div>
-				</div>
-			</div>
-			<div class="each-advande row">
-				<div class="col-xs-12 other-fill">
-					<div class="value-selected style-click">Thêm tuỳ chọn</div>
-				</div>
-			</div>
-		</div>
-	</div>
 </form>
 <script>
 $(document).ready(function () {
-	$('.dropdown-select').dropdown();
-	$('.num-phongngu, .num-phongtam').dropdown({
-		txtAdd: true,
+	$('.dropdown-select').dropdown({
+		hiddenFillValue: '#sort'
+	});
+
+	$('.fillter-fast').dropdown({
+		hiddenFillValue: '#quan-huyen',
 		styleShow: 0
 	});
+	
+	$('.num-phongngu').dropdown({
+		txtAdd: true,
+		styleShow: 0,
+		hiddenFillValue: '#val-bed'
+	});
+	
+	$('.num-phongtam').dropdown({
+		txtAdd: true,
+		styleShow: 0,
+		hiddenFillValue: '#val-bath'
+	});
+
 	$('.search-subpage').toggleShowMobi();
+	
 	$('.choice_price_dt').price_dt();
 });
 </script>
