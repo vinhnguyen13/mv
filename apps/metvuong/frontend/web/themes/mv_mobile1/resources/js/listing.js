bd9f8271203ad89887a77b1100fed3b0338a1bad
Lệnh Quách <qtlenh@gmail.com>
2016-02-15 15:00:44 +0700

    dang tin

--- apps/metvuong/frontend/web/themes/mv_mobile1/resources/js/listing.js
+++ apps/metvuong/frontend/web/themes/mv_mobile1/resources/js/listing.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var searchForm = $('#search-form');
 	var listingList = $('#listing-list');
-	var page = $('#page');
+	var page = 1;
 	
 	$(window).scroll(function() {
 		var self = $(this);
@@ -9,10 +9,12 @@ $(document).ready(function(){
 			if(!self.data('loading')) {
 				self.data('loading', true);
 
-				page.val(Number(page.val()) + 1);
+				var inputPage = $('<input type="hidden" name="page" value="' + (++page) + '" />');
+				searchForm.append(inputPage);
 				
 				$.get(searchForm.attr('action'), searchForm.serialize(), function(r){
 					var items = $(r).find('.item-listing');
+					inputPage.remove();
 					
 					if(items.length > 0) {
 						listingList.append(items);
