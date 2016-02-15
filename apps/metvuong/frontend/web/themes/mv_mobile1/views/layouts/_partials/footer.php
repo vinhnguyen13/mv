e4a79376db188b23c88b320b989c20cda09268f7
dientruong <dien.truong@trungthuygroup.vn>
2016-02-15 13:23:55 +0700

    update profile layout html mobile

--- apps/metvuong/frontend/web/themes/mv_mobile1/views/layouts/_partials/footer.php
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/layouts/_partials/footer.php
@@ -1,3 +1,26 @@
 <footer>
-	test
+	<div class="clearfix">
+		<div class="pull-left col-xs-6">
+			<ul>
+				<li><a href="#">Về Chúng Tôi</a></li>
+				<li><a href="#">Thông Tin Thị Trường</a></li>
+			</ul>
+		</div>
+		<div class="pull-right col-xs-6">
+			<ul>
+				<li><a href="#">Đội Ngũ Metvuong</a></li>
+				<li><a href="#">Điều Khoản & Điều Kiện</a></li>
+			</ul>
+		</div>
+		<div class="pull-left col-xs-6">
+			<em class="icon-call-out"></em> <strong>(08) 123 456</strong> hoặc <strong>0908 123 456</strong>
+		</div>
+		<div class="pull-right col-xs-6">
+			<em class="fa fa-envelope-o"></em><strong>info@metvuong.com</strong>
+		</div>
+	</div>
+	<div class="infor-address">
+		<p>21 Nguyễn Trung Ngạn, P.Bến Nghé, Q1, Tp.Ho Chi Minh</p>
+		<p>&copy; Copyright 2016 <a href="#">metvuong.com</a>. All rights reserved.</p>
+	</div>
 </footer>
