1d34cf702fd77dad51835fe1c79e45cad7086788
dientruong <dien.truong@trungthuygroup.vn>
2016-02-16 17:35:36 +0700

    update profile layout html mobile

--- apps/metvuong/frontend/web/themes/mv_mobile1/views/chat/with.php
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/chat/with.php
@@ -1,3 +1,65 @@
 <div class="chat-real">
-	<div class="title-top">Chat history</div>
+	<div class="title-top">Gao's Messages</div>
+	<div class="wrap-chat-item">
+		<div class="container-chat">
+			<div class="item box-me">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/default-avatar.png' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+			<div class="item box-you">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/MV-Agent Photo.jpg' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+			<div class="item box-me">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/default-avatar.png' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+			<div class="item box-you">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/MV-Agent Photo.jpg' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+			<div class="item box-me">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/default-avatar.png' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+			<div class="item box-you">
+				<a href="#" class="avatar wrap-img"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/MV-Agent Photo.jpg' ?>" alt=""></a>
+				<div class="chat-txt">
+					<div class="txt-detail">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
+					</div>
+					<div class="time-detail">today, 14:35</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="type-input-chat">
+		<input type="text" id="typingMsg" placeholder="Tin nhắn của bạn...">
+        <button class="sm-chat"><em class="fa fa-location-arrow"></em></button>
+    </div>
 </div>
