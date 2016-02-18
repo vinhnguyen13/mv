202d8455f44a7515d55606dd563f07f149ef2717
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-02-18 08:46:32 +0700

    chat with onather user

--- apps/metvuong/frontend/web/js/lib/chat.js
+++ apps/metvuong/frontend/web/js/lib/chat.js
@@ -141,7 +141,7 @@ var Chat = {
         // returning false would remove it after it finishes.
         return true;
     },
-    sttSaveMsg: false,
+    sttSaveMsg: true,
     sendMessage : function(messgeTo,message,type,params){
         if(message.length <= 0){
             return false;
