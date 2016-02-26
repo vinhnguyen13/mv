6843bd848ee4f76f0d8eb7be08019a1f788c781c
dientruong <dien.truong@trungthuygroup.vn>
2016-02-26 10:26:26 +0700

    chat

--- apps/metvuong/console/migrations/m160225_020657_alter_building_project_dbCraw.php
+++ apps/metvuong/console/migrations/m160225_020657_alter_building_project_dbCraw.php
@@ -7,44 +7,44 @@ class m160225_020657_alter_building_project_dbCraw extends Migration
 {
     public function up()
     {
-        if(empty(Yii::$app->get('dbCraw'))){
-            return false;
+        if(!empty(Yii::$app->dbCraw)){            
+          $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project` ADD COLUMN `description` TEXT NULL COMMENT '' AFTER `location`;");
+          $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
+                              CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL COMMENT '' ,
+                              CHANGE COLUMN `logo` `logo` VARCHAR(255) NULL COMMENT '' ,
+                              CHANGE COLUMN `slug` `slug` VARCHAR(255) NOT NULL COMMENT '' ;");
+
+          $this->execute("CREATE TABLE `db_mv_tool`.`ad_contractor` (
+                            `id` int(11) NOT NULL AUTO_INCREMENT,
+                            `name` varchar(255) NOT NULL,
+                            `address` varchar(255) DEFAULT NULL,
+                            `phone` varchar(32) DEFAULT NULL,
+                            `fax` varchar(32) DEFAULT NULL,
+                            `website` varchar(255) DEFAULT NULL,
+                            `email` varchar(255) DEFAULT NULL,
+                            `description` varchar(1022) DEFAULT NULL,
+                            `created_at` int(11) NOT NULL,
+                            `updated_at` int(11) DEFAULT NULL,
+                            `status` tinyint(1) NOT NULL DEFAULT '1',
+                            PRIMARY KEY (`id`)
+                          ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+          $this->execute("CREATE TABLE `db_mv_tool`.`ad_contractor_building_project` (
+                            `building_project_id` int(11) DEFAULT NULL,
+                            `contractor_id` int(11) DEFAULT NULL,
+                          CONSTRAINT `fk_building_project_id` FOREIGN KEY (`building_project_id`) REFERENCES `ad_building_project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                          CONSTRAINT `fk_contractor_id` FOREIGN KEY (`contractor_id`) REFERENCES `ad_contractor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+                          UNIQUE INDEX `building_project_id&contractor_id` (`building_project_id` ASC, `contractor_id` ASC)  COMMENT '',
+                          INDEX `fk_building_project_id_idx` (`building_project_id` ASC)  COMMENT '',
+                          INDEX `fk_contractor_id_idx` (`contractor_id` ASC)  COMMENT ''
+                          ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
+          $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
+                              ADD COLUMN `start_date` INT NULL COMMENT '' AFTER `seo_description`,
+                              ADD COLUMN `facade_width` FLOAT NULL COMMENT '' AFTER `facilities_detail`,
+                              ADD COLUMN `lift` SMALLINT NULL COMMENT '' AFTER `facade_width`;");
+
         }
-        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project` ADD COLUMN `description` TEXT NULL COMMENT '' AFTER `location`;");
-        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
-                            CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL COMMENT '' ,
-                            CHANGE COLUMN `logo` `logo` VARCHAR(255) NULL COMMENT '' ,
-                            CHANGE COLUMN `slug` `slug` VARCHAR(255) NOT NULL COMMENT '' ;");
-
-        $this->execute("CREATE TABLE `db_mv_tool`.`ad_contractor` (
-                          `id` int(11) NOT NULL AUTO_INCREMENT,
-                          `name` varchar(255) NOT NULL,
-                          `address` varchar(255) DEFAULT NULL,
-                          `phone` varchar(32) DEFAULT NULL,
-                          `fax` varchar(32) DEFAULT NULL,
-                          `website` varchar(255) DEFAULT NULL,
-                          `email` varchar(255) DEFAULT NULL,
-                          `description` varchar(1022) DEFAULT NULL,
-                          `created_at` int(11) NOT NULL,
-                          `updated_at` int(11) DEFAULT NULL,
-                          `status` tinyint(1) NOT NULL DEFAULT '1',
-                          PRIMARY KEY (`id`)
-                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-
-        $this->execute("CREATE TABLE `db_mv_tool`.`ad_contractor_building_project` (
-                          `building_project_id` int(11) DEFAULT NULL,
-                          `contractor_id` int(11) DEFAULT NULL,
-                        CONSTRAINT `fk_building_project_id` FOREIGN KEY (`building_project_id`) REFERENCES `ad_building_project` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-                        CONSTRAINT `fk_contractor_id` FOREIGN KEY (`contractor_id`) REFERENCES `ad_contractor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-                        UNIQUE INDEX `building_project_id&contractor_id` (`building_project_id` ASC, `contractor_id` ASC)  COMMENT '',
-                        INDEX `fk_building_project_id_idx` (`building_project_id` ASC)  COMMENT '',
-                        INDEX `fk_contractor_id_idx` (`contractor_id` ASC)  COMMENT ''
-                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-
-        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
-                            ADD COLUMN `start_date` INT NULL COMMENT '' AFTER `seo_description`,
-                            ADD COLUMN `facade_width` FLOAT NULL COMMENT '' AFTER `facilities_detail`,
-                            ADD COLUMN `lift` SMALLINT NULL COMMENT '' AFTER `facade_width`;");
     }
 
     public function down()
