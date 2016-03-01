0041b155388fbfc5fdb1c951b184191e266c814e
dientruong <dien.truong@trungthuygroup.vn>
2016-03-01 14:21:08 +0700

    html page news

---
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/news/index.php
@@ -0,0 +1,49 @@
+<div class="title-fixed-wrap">
+	<div class="page-news">
+		<div class="title-top">TIN TỨC</div>
+		<div class="wrap-news">
+			<ul class="clearfix">
+				<li>
+					<a href="#" class="rippler rippler-default">
+						<div class="img-show"><div><img src="<?=Yii::$app->view->theme->baseUrl."/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg"?>"></div></div>
+					</a>
+					<p class="name-news"><a href="#">Lorem ipsum dolorit aset modo phasel vel mi quam dolor</a></p>
+					<p class="date-post">12/02/2016, 14:30</p>
+					<p class="short-txt">
+						Vestibulum consequat orci ex, non hendrerit tortor accumsan eu. Fusce lorem ipsum, tempor vitae risus eu, consequat rutrum nisi. Curabitur mollis quam eget justo mollis cursus. Vestibulum non volutpat diam....
+					</p>
+				</li>
+				<li>
+					<a href="#" class="rippler rippler-default">
+						<div class="img-show"><div><img src="<?=Yii::$app->view->theme->baseUrl."/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg"?>"></div></div>
+					</a>
+					<p class="name-news"><a href="#">Lorem ipsum dolorit aset modo phasel vel mi quam dolor</a></p>
+					<p class="date-post">12/02/2016, 14:30</p>
+					<p class="short-txt">
+						Vestibulum consequat orci ex, non hendrerit tortor accumsan eu. Fusce lorem ipsum, tempor vitae risus eu, consequat rutrum nisi. Curabitur mollis quam eget justo mollis cursus. Vestibulum non volutpat diam....
+					</p>
+				</li>
+				<li>
+					<a href="#" class="rippler rippler-default">
+						<div class="img-show"><div><img src="<?=Yii::$app->view->theme->baseUrl."/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg"?>"></div></div>
+					</a>
+					<p class="name-news"><a href="#">Lorem ipsum dolorit aset modo phasel vel mi quam dolor</a></p>
+					<p class="date-post">12/02/2016, 14:30</p>
+					<p class="short-txt">
+						Vestibulum consequat orci ex, non hendrerit tortor accumsan eu. Fusce lorem ipsum, tempor vitae risus eu, consequat rutrum nisi. Curabitur mollis quam eget justo mollis cursus. Vestibulum non volutpat diam....
+					</p>
+				</li>
+				<li>
+					<a href="#" class="rippler rippler-default">
+						<div class="img-show"><div><img src="<?=Yii::$app->view->theme->baseUrl."/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg"?>"></div></div>
+					</a>
+					<p class="name-news"><a href="#">Lorem ipsum dolorit aset modo phasel vel mi quam dolor</a></p>
+					<p class="date-post">12/02/2016, 14:30</p>
+					<p class="short-txt">
+						Vestibulum consequat orci ex, non hendrerit tortor accumsan eu. Fusce lorem ipsum, tempor vitae risus eu, consequat rutrum nisi. Curabitur mollis quam eget justo mollis cursus. Vestibulum non volutpat diam....
+					</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
