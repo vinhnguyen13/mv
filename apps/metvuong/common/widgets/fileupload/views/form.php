91f5d605d053bc1e1371b8192a4b36ee46bc98f9
Lệnh Quách <qtlenh@gmail.com>
2016-03-02 11:10:59 +0700

    dang tin

--- apps/metvuong/common/widgets/fileupload/views/form.php
+++ apps/metvuong/common/widgets/fileupload/views/form.php
@@ -6,25 +6,27 @@ $context = $this->context;
 ?>
 <?= Html::beginTag('div', $context->options); ?>
 	<ul class="files"></ul>
-    <span class="btn btn-success fileinput-button">
-		<i class="glyphicon glyphicon-plus"></i>
-		<span><?= Yii::t('fileupload', 'Add files') ?>...</span>
-		<?php
-			$options = $context->fieldOptions;
-		
-			if(($context->model instanceof \yii\base\Model && $context->attribute !== null)) {
-				$name = array_key_exists('name', $options) ? $options['name'] : Html::getInputName($context->model, $context->attribute);
-			} else {
-				$name = $context->name;
-			}
+	<div class="fileinput-button-wrap">
+	    <span class="btn btn-success fileinput-button">
+			<i class="glyphicon glyphicon-plus"></i>
+			<span><?= Yii::t('fileupload', 'Add files') ?>...</span>
+			<?php
+				$options = $context->fieldOptions;
 			
-			if(!array_key_exists('id', $options)) {
-				$options['id'] = ($context->model instanceof \yii\base\Model && $context->attribute !== null) ? Html::getInputId($context->model, $context->attribute) : $this->context->getId();
-			}
+				if(($context->model instanceof \yii\base\Model && $context->attribute !== null)) {
+					$name = array_key_exists('name', $options) ? $options['name'] : Html::getInputName($context->model, $context->attribute);
+				} else {
+					$name = $context->name;
+				}
+				
+				if(!array_key_exists('id', $options)) {
+					$options['id'] = ($context->model instanceof \yii\base\Model && $context->attribute !== null) ? Html::getInputId($context->model, $context->attribute) : $this->context->getId();
+				}
+				
+				$options['ref'] = $name;
+			?>
 			
-			$options['ref'] = $name;
-		?>
-		
-		<?= Html::input('file', $this->context->inputFileName, null, $options) ?>
-	</span>
+			<?= Html::input('file', $this->context->inputFileName, null, $options) ?>
+		</span>
+	</div>
 <?= Html::endTag('div');?>
