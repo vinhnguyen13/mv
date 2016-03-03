775bbff47a7f715181872983f1a15baca86897c0
Nhut Tran <nhut.tran@trungthuygroup.vn>
2016-03-03 18:00:22 +0700

    update profile

--- apps/metvuong/frontend/web/themes/mv_mobile1/views/member/user/profile.php
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/member/user/profile.php
@@ -72,16 +72,11 @@ $user = $model->getUser();
             <div class="title-text">Danh sách tin đã đăng</div>
             <ul class="clearfix list-post">
                 <?php foreach ($products as $product) {
-                    if(!empty($product->source)){
-                        $image = $product->representImage;
-                    } else {
-                        $image = "/store/ad/". $product->representImage;
-                    }
                     ?>
                 <li>
                     <a href="<?= $product->urlDetail() ?>" class="rippler rippler-default">
-                        <div class="img-show"><div><img src="<?=$image ?>">
-                                <input type="hidden" value="<?=$image?>">
+                        <div class="img-show"><div><img src="<?= $product->representImage ?>">
+                                <input type="hidden" value="<?= $product->representImage ?>">
                             </div></div>
                         <div class="title-item"><?= ucfirst($categories[$product->category_id]['name']) ?> <?= $types[$product->type] ?></div>
                     </a>
