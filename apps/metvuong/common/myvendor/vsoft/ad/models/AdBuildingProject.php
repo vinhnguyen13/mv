216038b86148dd9227a0e10c1b912d2a5157bb99
Nhut Tran <nhut.tran@trungthuygroup.vn>
2016-03-03 09:10:53 +0700

    add uppercase first method

--- apps/metvuong/common/myvendor/vsoft/ad/models/AdBuildingProject.php
+++ apps/metvuong/common/myvendor/vsoft/ad/models/AdBuildingProject.php
@@ -214,4 +214,12 @@ class AdBuildingProject extends ABP
 			}
 		}
 	}
+
+    public static function mb_ucfirst($string, $encoding)
+    {
+        $strlen = mb_strlen($string, $encoding);
+        $firstChar = mb_substr($string, 0, 1, $encoding);
+        $then = mb_substr($string, 1, $strlen - 1, $encoding);
+        return mb_strtoupper($firstChar, $encoding) . $then;
+    }
 }
