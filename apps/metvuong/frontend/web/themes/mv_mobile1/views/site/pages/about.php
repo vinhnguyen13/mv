1ed8a9f692a40b0f056eead1d89d74f5a486f685
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-03-10 09:18:40 +0700

    desktop

--- apps/metvuong/frontend/web/themes/mv_mobile1/views/site/pages/about.php
+++ apps/metvuong/frontend/web/themes/mv_mobile1/views/site/pages/about.php
@@ -1,9 +1,12 @@
+<?php
+$themeBaseUrl = Yii::$app->view->theme->baseUrl;
+?>
 <div class="title-fixed-wrap">
 	<div class="page-about-us">
 		<div class="title-top">VỀ CHÚNG TÔI</div>
 		<div class="inner-page">
 			<div class="text-center logo-search-box wrap-img">
-                <img src="/frontend/web/themes/mv_mobile1/resources/images/logo.png" alt="">
+                <img src="<?=$themeBaseUrl?>/resources/images/logo.png" alt="">
             </div>
             <p>
             	<a href="/"><strong>Metvuong.com</strong></a> là cổng giao dịch trực tuyến về mua bán - cho thuê bất động sản. Chúng tôi cung cấp lên đến hàng chục ngàn các căn hộ cao cấp, nhà riêng và biệt thự cho thuê hoặc đang rao bán tại hầu hết các tỉnh thành trên cả nước. Với tiêu chí đề cao uy tín và chất lượng của sản phẩm dịch vụ, Metvuong.com luôn chú trọng việc kiểm duyệt nội dung các tin đăng nhà đất, đảm bảo mọi tin rao đều có thông tin đầy đủ và hình ảnh chân thật. Chúng tôi cam kết giúp khách hàng tìm được sản phẩm phù hợp nhất, một cách nhanh chóng và hiệu quả.
