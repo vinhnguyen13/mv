a7940af4a903f599eb7ab3fefba1fa2d4cfa6af4
dientruong <dien.truong@trungthuygroup.vn>
2016-03-11 15:55:46 +0700

    update layout desktop

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/site/index.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/site/index.php
@@ -40,76 +40,82 @@ use frontend\models\Ad;
     $products = Ad::find()->homePageRandom();
     ?>
     <section class="box-item box-feature-item">
-        <div class="title-sub">Featured properties</div>
-        <div class="wrap-item wrap-lazy">
-            <ul class="clearfix">
-                <?php foreach ($products as $product): ?>
-                <li>
-                    <div class="item">
-                        <a href="<?= Url::to(['/ad/detail', 'id' => $product->id, 'slug' => \common\components\Slug::me()->slugify($product->getAddress())]) ?>" class="pic-intro rippler rippler-default">
-                            <div class="img-show">
-                                <div><img src="" data-original="<?= $product->representImage ?>"></div>
-                            </div>
-                            <div class="title-item"><?= ucfirst($categories[$product->category_id]['name']) ?> <?= $types[$product->type] ?></div>
-                        </a>
-                        <div class="info-item">
-                            <div class="address-feat">
-        <!--                        <strong>Lancaster x</strong>-->
-                                <span class="icon address-icon"></span><?= $product->getAddress(true) ?>
-                                <p class="id-duan">ID tin đăng:<span><?=$product->id;?></span></p>
-                                <ul class="clearfix list-attr-td">
-                                    <li>
-                                        <span class="icon icon-dt icon-dt-small"></span>80m2
-                                    </li>
-                                    <li>
-                                        <span class="icon icon-bed icon-bed-small"></span> 02
-                                    </li>
-                                    <li>
-                                        <span class="icon icon-pt icon-pt-small"></span> 02
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="bottom-feat-box clearfix">
-                                <a href="<?= Url::to(['/ad/detail', 'id' => $product->id, 'slug' => \common\components\Slug::me()->slugify($product->getAddress())]) ?>" class="pull-right">Chi tiết</a>
-                                <p>Giá <strong>4 tỷ đồng</strong></p>
+        <div class="container">
+            <div class="title-sub">Featured properties</div>
+            <div class="wrap-item wrap-lazy">
+                <ul class="clearfix">
+                    <?php foreach ($products as $product): ?>
+                    <li>
+                        <div class="item">
+                            <a href="<?= Url::to(['/ad/detail', 'id' => $product->id, 'slug' => \common\components\Slug::me()->slugify($product->getAddress())]) ?>" class="pic-intro rippler rippler-default">
+                                <div class="img-show">
+                                    <div><img src="" data-original="<?= $product->representImage ?>"></div>
+                                </div>
+                                <div class="title-item"><?= ucfirst($categories[$product->category_id]['name']) ?> <?= $types[$product->type] ?></div>
+                            </a>
+                            <div class="info-item">
+                                <div class="address-feat">
+            <!--                        <strong>Lancaster x</strong>-->
+                                    <span class="icon address-icon"></span><?= $product->getAddress(true) ?>
+                                    <p class="id-duan">ID tin đăng:<span><?=$product->id;?></span></p>
+                                    <ul class="clearfix list-attr-td">
+                                        <li>
+                                            <span class="icon icon-dt icon-dt-small"></span>80m2
+                                        </li>
+                                        <li>
+                                            <span class="icon icon-bed icon-bed-small"></span> 02
+                                        </li>
+                                        <li>
+                                            <span class="icon icon-pt icon-pt-small"></span> 02
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="bottom-feat-box clearfix">
+                                    <a href="<?= Url::to(['/ad/detail', 'id' => $product->id, 'slug' => \common\components\Slug::me()->slugify($product->getAddress())]) ?>" class="pull-right">Chi tiết</a>
+                                    <p>Giá <strong>4 tỷ đồng</strong></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-                <?php
-                endforeach;
-                ?>
-            </ul>
+                    </li>
+                    <?php
+                    endforeach;
+                    ?>
+                </ul>
+            </div>
         </div>
     </section>
 
     <?php if(count($news) > 0) {?>
     <section class="box-item news-item">
-        <div class="title-sub">news</div>
-        <div class="wrap-item">
-            <?php
-                foreach($news as $n){
-            ?>
-            <div class="item clearfix">
-                <a class="rippler rippler-default" href="<?= \yii\helpers\Url::to(['news/view', 'id' => $n['id'], 'slug' => $n['slug']], true) ?>">
-                    <div class="wrap-img bgcover" style="background-image:url(<?=Url::to('/store/news/show/' . $n['banner']) ?>);"></div>
-                    <span class="txt-short-news">
-                        <span class="title-news color-30a868"><?=$n['title']?></span>
-                        <span class="date-news"><?=date('d/m/Y, H:i', $n['created_at'])?></span>
-                        <?=StringHelper::truncate($n['brief'], 130)?>
-                    </span>
-                </a>
+        <div class="container">
+            <div class="title-sub">news</div>
+            <div class="wrap-item">
+                <?php
+                    foreach($news as $n){
+                ?>
+                <div class="item clearfix">
+                    <a class="rippler rippler-default" href="<?= \yii\helpers\Url::to(['news/view', 'id' => $n['id'], 'slug' => $n['slug']], true) ?>">
+                        <div class="wrap-img bgcover" style="background-image:url(<?=Url::to('/store/news/show/' . $n['banner']) ?>);"></div>
+                        <span class="txt-short-news">
+                            <span class="title-news color-30a868"><?=$n['title']?></span>
+                            <span class="date-news"><?=date('d/m/Y, H:i', $n['created_at'])?></span>
+                            <?=StringHelper::truncate($n['brief'], 130)?>
+                        </span>
+                    </a>
+                </div>
+                <?php } ?>
             </div>
-            <?php } ?>
         </div>
     </section>
     <?php } ?>
 
     <section class="search-home">
-        <div class="txt-intro">
-            <h2>Chúng tôi sẽ giúp bạn tìm được ngôi nhà mà mình mong muốn trong chốc lát</h2>
-            <p>Mét Vuông cung cấp lên đến hàng chục ngàn các căn hộ cao cấp, nhà riêng và biệt thự cho thuê hoặc đang rao bán tại hầu hết các tỉnh thành trên cả nước. Thông qua những mô tả chi tiết và hình ảnh chân thật, chúng tôi cam kết giúp khách hàng tìm được sản phẩm phù hợp nhất, một cách nhanh chóng và hiệu quả.</p>
-            <div class="text-center pdT-25"><a href="#" class="btn-common rippler rippler-default">Tìm hiểu thêm</a></div>
+        <div class="container">
+            <div class="txt-intro">
+                <h2>Chúng tôi sẽ giúp bạn tìm được ngôi nhà mà mình mong muốn trong chốc lát</h2>
+                <p>Mét Vuông cung cấp lên đến hàng chục ngàn các căn hộ cao cấp, nhà riêng và biệt thự cho thuê hoặc đang rao bán tại hầu hết các tỉnh thành trên cả nước. Thông qua những mô tả chi tiết và hình ảnh chân thật, chúng tôi cam kết giúp khách hàng tìm được sản phẩm phù hợp nhất, một cách nhanh chóng và hiệu quả.</p>
+                <div class="text-center pdT-25"><a href="#" class="btn-common rippler rippler-default">Tìm hiểu thêm</a></div>
+            </div>
         </div>
     </section>
 </div>
