8aea1837de2195702adbf5e17fa5649b22a94c3f
Lệnh Quách <qtlenh@gmail.com>
2016-03-16 09:45:15 +0700

    dang tin

--- apps/metvuong/frontend/web/themes/mv_desktop1/resources/js/listing.js
+++ apps/metvuong/frontend/web/themes/mv_desktop1/resources/js/listing.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	
 	var map = {
+		gmap: null,
 		initMap: function() {
 			m2Map.initial(function(gmap) {
 				var options = {center: {lat: 10.783068, lng: 106.704825}, zoom: 15};
-				var gmap = new google.maps.Map(document.getElementById('map'), options);
+				map.gmap = new google.maps.Map(document.getElementById('map'), options);
 			});
 		}
 	};
