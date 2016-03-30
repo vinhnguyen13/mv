5b07a547cf1843bd77f385e0a9ad168eb2784674
Lệnh Quách <qtlenh@gmail.com>
2016-03-30 11:00:41 +0700

    listing map

--- apps/metvuong/console/migrations/m160330_034231_alter_ads_table.php
+++ apps/metvuong/console/migrations/m160330_034231_alter_ads_table.php
@@ -7,14 +7,14 @@ class m160330_034231_alter_ads_table extends Migration
 {
     public function up()
     {
-		$this->execute("ALTER TABLE `metvuong`.`ad_street`   
+		$this->execute("ALTER TABLE `ad_street`   
   ADD COLUMN `geometry` TEXT NULL AFTER `pre`,
   ADD COLUMN `center` VARCHAR(32) NULL AFTER `geometry`;");
     }
 
     public function down()
     {
-        $this->execute("ALTER TABLE `metvuong`.`ad_street`   
+        $this->execute("ALTER TABLE `ad_street`   
   DROP COLUMN `geometry`, 
   DROP COLUMN `center`;");
     }
