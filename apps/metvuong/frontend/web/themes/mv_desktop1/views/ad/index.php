cf1f1ad43c45c970bda7397265cb496bb1a58772
Lệnh Quách <qtlenh@gmail.com>
2016-04-06 09:34:44 +0700

    listing map

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/index.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/index.php
@@ -97,8 +97,7 @@ $this->registerJs($script, View::POS_BEGIN);
 													<?php 
 														$cities = AdCity::find()->all();
 														$citiesDropdown = ArrayHelper::map($cities, 'id', 'name');
-														// $citiesOptions = ArrayHelper::map($cities, 'id', function($city){ return ['disabled' => ($city->id != AdProduct::DEFAULT_CITY)]; });
-														$citiesOptions = [];
+														$citiesOptions = ArrayHelper::map($cities, 'id', function($city){ return ['disabled' => ($city->id != AdProduct::DEFAULT_CITY)]; });
 													?>
 													<?= Html::activeDropDownList($searchModel, 'city_id', $citiesDropdown, ['class' => 'form-control select2 style-common area-select', 'options' => $citiesOptions]) ?>
 												</div>
