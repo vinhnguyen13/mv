ba37effa4d6650bc83dc3292519016b25806c53d
Nhut Tran <nhuttranm@gmail.com>
2016-04-13 17:25:52 +0700

    update project

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/shareSocial.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/shareSocial.php
@@ -19,10 +19,11 @@ use yii\helpers\Url;
                         <a href="#" class="btn-close close" data-dismiss="modal" aria-label="Close"><span class="icon icon-close"></span></a>
                         <div class="wrap-body-popup">
                             <span>Share on Social Network</span>
+                            <?php if(!empty($product_id)) {?>
                             <ul class="clearfix">
                                 <li>
-                                    <a href="#"
-                                       data-url="<?=Url::to(['/ad/tracking-share', 'product_id' => $product_id, 'type' => \vsoft\tracking\models\base\AdProductShare::SHARE_FACEBOOK], true)?>" class="share-facebook">
+                                    <a href="#" class="share-facebook"
+                                       data-url="<?=Url::to(['/ad/tracking-share', 'product_id' => $product_id, 'type' => \vsoft\tracking\models\base\AdProductShare::SHARE_FACEBOOK], true)?>">
                                         <div class="circle"><div><span class="icon icon-face"></span></div></div>
                                     </a>
                                 </li>
@@ -34,6 +35,20 @@ use yii\helpers\Url;
                                     </a>
                                 </li>
                             </ul>
+                            <?php } else {?>
+                            <ul class="clearfix">
+                                <li>
+                                    <a href="#" class="share-facebook">
+                                        <div class="circle"><div><span class="icon icon-face"></span></div></div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" data-toggle="modal" data-target="#popup-email" class="email-btn">
+                                        <div class="circle"><div><span class="icon icon-email-1"></span></div></div>
+                                    </a>
+                                </li>
+                            </ul>
+                            <?php } ?>
                         </div>
                     </div>
                 </div>
@@ -50,9 +65,15 @@ use yii\helpers\Url;
     }
 
     $('#popup-share-social ul li a').click(function (){
+        $('body').loading();
+        if($(this).attr('class') == 'share-facebook') {
+            $('body').loading({done:true});
+            fbShare('<?=$url ?>', '<?=$title ?>', '<?=$description ?>', '<?= $image ?>', 520, 350);
+            return true;
+        }
+
         var url = $(this).data("url");
-        if(url.length > 0) {
-            $('body').loading();
+        if(url != undefined && url.length > 0) {
             $.ajax({
                 type: "get",
                 dataType: 'json',
@@ -62,11 +83,9 @@ use yii\helpers\Url;
                 }
             });
             $('body').loading({done:true});
-            if($(this).attr('class') == 'share-facebook')
-                fbShare('<?=$url ?>', '<?=$title ?>', '<?=$description ?>', '<?= $image ?>', 520, 350);
-
             return true;
         }
+
         return false;
     });
 
