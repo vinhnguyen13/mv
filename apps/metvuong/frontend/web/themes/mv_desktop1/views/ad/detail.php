57be98efc18f663b823ce15dfb90a268655b87c3
Nhut Tran <nhuttranm@gmail.com>
2016-04-13 13:36:39 +0700

    update detail

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
@@ -160,8 +160,9 @@ if(isset(Yii::$app->params['tracking']['all']) && (Yii::$app->params['tracking']
 				</div>
 
 				<?php
-                $description = \yii\helpers\StringHelper::truncate($product->content, 50, $suffix = '...', $encoding = 'UTF-8');
-                $description = str_ireplace("\n", " ", $description);
+                $description = \yii\helpers\StringHelper::truncate($product->content, 200, $suffix = '...', $encoding = 'UTF-8');
+                $description = str_replace("\r", "", $description);
+                $description = str_replace("\n", "", $description);
 
                 echo $this->render('/ad/_partials/shareSocial',[
                     'product_id' => $product->id,
