f4e261ff5687c9c1fe0bee7dfe3b376c186cc5ca
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-04-21 16:52:50 +0700

    commingsoon

---
+++ apps/metvuong/frontend/views/site/commingsoon.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <title>Coming Soon</title>
+    <link type="image/jpeg" href="http://dev.metvuong.com/frontend/web/themes/mv_desktop1/resources/favicon/favicon.png" rel="shortcut icon">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,300,700' rel='stylesheet' type='text/css'>
+    <link href="/commingsoon/comingsoon.css" rel="stylesheet" type="text/css" />
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-71233207-1', 'auto');
+        ga('send', 'pageview');
+
+        ga('create', 'UA-71563942-1', 'auto', {'name':'metvuong'});
+        ga('metvuong.send', 'pageview');
+
+    </script>
+</head>
+<body>
+<div class="img-show">
+    <div>
+        <div class="inner-box">
+            <div class="wrap-logo"><img src="/commingsoon/logo.png" alt=""></div>
+            <h1>COMING SOON!</h1>
+            <p>We are currently under construction.</p>
+            <p>We will be here soon with our awesome website.</p>
+            <div class="line"></div>
+            <p><strong>Go to <a href="/tin-tuc">News</a></strong> or</p>
+            <p><strong>PLEASE CHECK OUT OUR SOCIAL NETWORK</strong></p>
+            <div class="text-center">
+                <a href="https://www.facebook.com/metvuongbatdongsan" title="facebook" class="logo-face"><img src="/commingsoon/logo-face.png" alt=""></a>
+                <a href="javascript:alert('Coming Soon !')" title="google+" class="logo-go"><img src="/commingsoon/logo-go.png" alt=""></a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
