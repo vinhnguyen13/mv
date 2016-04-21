7ecfc9e819ffd7302b03a934f99ad14c17e4f60a
Nhut Tran <nhuttranm@gmail.com>
2016-04-21 17:59:39 +0700

    update share tranlate

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/news/detail.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/news/detail.php
@@ -96,7 +96,7 @@ $banner = Yii::$app->urlManager->createAbsoluteUrl('/store/news/show/'. $news["b
                             <div id="social<?=$news["id"]?>" class="share-social mgT-10 wrap-img">
                                 <div class="fb-like" data-href="<?= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>" data-layout="button_count" style="margin-right: 10px;"></div>
                                 <div class="fb-send" data-href="<?= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>" data-show-faces="false" style="margin-right: 10px;"></div>
-                                <a class="fb-share" data-href="<?= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>" data-layout="button_count">share facebook</a><br>
+                                <a class="fb-share" data-href="<?= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>" data-layout="button_count"><?=Yii::t('news','Share facebook')?></a><br>
                                 <div class="fb-comments" data-href="<?= Yii::$app->urlManager->createAbsoluteUrl(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]])?>" data-width="100%" data-numposts="3"></div>
                             </div>
                         </div>
