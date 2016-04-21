92fd27a86c81df47f67246fec70fe928a1282c6c
Nhut Tran <nhuttranm@gmail.com>
2016-04-21 15:24:31 +0700

    update share facebook news detail

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/news/detail.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/news/detail.php
@@ -169,29 +169,29 @@ if($checkBanner == false)
             prevButton: '.swiper-button-prev'
         });
 
-        $(document).on('click', '.detail-content .fb-share', function(){
-            var href = $(this).data('href');
-            FB.ui({
-                method: 'share',
-                href: href
-            }, function(response){});
-            return false;
-        });
-
-
-//        function fbShare(url, title, descr, image, winWidth, winHeight) {
-//            var winTop = (screen.height / 2) - (winHeight / 2);
-//            var winLeft = (screen.width / 2) - (winWidth / 2);
-//            window.open('http://www.facebook.com/sharer.php?s=100&p[url]=' + url + '&p[title]=' + title + '&p[summary]=' + descr + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
-//        }
-//
-//        $(document).on('click', '.detail-content .share-facebook', function(){
-//            var url = '<?//= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>//';
-//            var image = '<?//=$banner?>//';
-//            var name = '<?//=$news["title"]?>//';
-//            var descr = '<?//=$news["brief"]?>//';
-//            fbShare(url, name, descr, image, 800, 500);
+//        $(document).on('click', '.detail-content .fb-share', function(){
+//            var href = $(this).data('href');
+//            FB.ui({
+//                method: 'share',
+//                href: href
+//            }, function(response){});
+//            return false;
 //        });
+
+
+        function fbShare(url, title, descr, image, winWidth, winHeight) {
+            var winTop = (screen.height / 2) - (winHeight / 2);
+            var winLeft = (screen.width / 2) - (winWidth / 2);
+            window.open('http://www.facebook.com/sharer.php?s=100&p[url]=' + url + '&p[title]=' + title + '&p[summary]=' + descr + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+        }
+
+        $(document).on('click', '.detail-content .fb-share', function(){
+            var url = '<?= \yii\helpers\Url::to(['news/view', 'id' => $news["id"], 'slug' => $news["slug"]], true) ?>';
+            var image = '<?=$banner?>';
+            var name = '<?=$news["title"]?>';
+            var descr = '<?=$news["brief"]?>';
+            fbShare(url, name, descr, image, 800, 500);
+        });
 
 //        var timer;
 //        $(window).scroll(function () {
