a04f784308bbcf93ab8ba0d5cd17454519af5f18
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-04-21 17:27:44 +0700

    commingsoon

--- apps/metvuong/frontend/components/Controller.php
+++ apps/metvuong/frontend/components/Controller.php
@@ -35,7 +35,11 @@ class Controller extends \yii\web\Controller
     public function beforeAction($action)
     {
         if(Yii::$app->controller->id != 'news' && Yii::$app->controller->action->id != 'commingsoon'){
-            $this->redirect('/site/commingsoon');
+            if(Yii::$app->controller->id == 'site' && Yii::$app->controller->action->id == 'index'){
+                $this->redirect(Url::to(['news/index']));
+            }else{
+                $this->redirect('/site/commingsoon');
+            }
         }
         if(Yii::$app->user->isGuest && !in_array($action->id, ['login', 'register', 'error', 'map-image']) && !Yii::$app->request->isAjax){
             Yii::$app->getUser()->setReturnUrl(Url::current());
