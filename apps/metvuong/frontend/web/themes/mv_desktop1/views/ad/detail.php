18d317bbdc35c158ad68b32da83a71384615ffd5
Nhut Tran <nhuttranm@gmail.com>
2016-04-25 09:34:28 +0700

    update project & product details share facebook

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
@@ -342,10 +342,8 @@ $userId = Yii::$app->user->identity ? Yii::$app->user->identity->id : null;
                                             var winHeight = 350;
                                             var winTop = (screen.height / 2) - (winHeight / 2);
                                             var winLeft = (screen.width / 2) - (winWidth / 2);
-                                            var title = '<?=$address?>';
-                                            var descr = '<?=$description?>';
-                                            var image = '<?=Yii::$app->request->hostInfo.$product->representImage?>';
-                                            window.open('http://www.facebook.com/sharer.php?s=100&p[url]=' + link + '&p[title]=' + title + '&p[summary]=' + descr + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+                                            window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(link), 'facebook-share-dialog', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+//                                            window.open('http://www.facebook.com/sharer.php?s=100&p[url]=' + link + '&p[title]=' + title + '&p[summary]=' + descr + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
                                         }
                                         $('body').loading({done:true});
                                         return true;
