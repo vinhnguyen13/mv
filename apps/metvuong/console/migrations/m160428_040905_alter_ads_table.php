0d06a34299f9586b36241bd7315902a022cc1654
Lệnh Quách <qtlenh@gmail.com>
2016-04-28 11:17:39 +0700

    group category

---
+++ apps/metvuong/console/migrations/m160428_040905_alter_ads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m160428_040905_alter_ads_table extends Migration
+{
+    public function up()
+    {
+		$this->execute("CREATE TABLE `ad_category_group`(
+		  `id` INT NOT NULL,
+		  `name` VARCHAR(32) NOT NULL,
+		  `categories_id` VARCHAR(32),
+		  `order` INT NOT NULL DEFAULT 0,
+		  `apply_to_type` TINYINT(1) NOT NULL DEFAULT 3,
+		  `status` TINYINT(1) NOT NULL DEFAULT 1,
+		  PRIMARY KEY (`id`)
+		);");
+    }
+
+    public function down()
+    {
+		$this->dropTable("ad_category_group");
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
