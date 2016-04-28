390e1bcf82e1856260bb63afd7553657cd06d7e9
Nhut Tran <nhuttranm@gmail.com>
2016-04-28 16:07:53 +0700

    update to crawl migrations

--- apps/metvuong/console/migrations/m160401_070302_alter_add_click_building_project.php
+++ apps/metvuong/console/migrations/m160401_070302_alter_add_click_building_project.php
@@ -7,8 +7,8 @@ class m160401_070302_alter_add_click_building_project extends Migration
     public function up()
     {
         $this->execute("ALTER TABLE `ad_building_project`
-ADD COLUMN `hot_project` TINYINT(1) NULL DEFAULT 0 COMMENT '1 is hot project' AFTER `data_html`,
-ADD COLUMN `click` INT NULL DEFAULT 0 COMMENT '' AFTER `hot_project`;");
+                            ADD COLUMN `hot_project` TINYINT(1) NULL DEFAULT 0 COMMENT '1 is hot project' AFTER `status`,
+                            ADD COLUMN `click` INT NULL DEFAULT 0 COMMENT '' AFTER `hot_project`;");
     }
 
     public function down()
