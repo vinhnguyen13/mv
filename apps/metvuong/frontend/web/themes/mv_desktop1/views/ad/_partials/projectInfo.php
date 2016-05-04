485847a0d83c1d0627b1a16bfad67c6e33e4db16
Lệnh Quách <qtlenh@gmail.com>
2016-05-04 13:40:18 +0700

    update project info link

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/projectInfo.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/projectInfo.php
@@ -2,7 +2,7 @@
 use yii\helpers\Url;
 ?>
 <div class="infor-duan-suggest clearfix">
-	<a target="_blank" href="<?= Url::to(["building/$project->slug"]); ?>" class="">
+	<a target="_blank" href="<?= Url::to(['building-project/view', 'slug' => $project->slug]); ?>" class="">
 		<span class="wrap-img pull-left">
 			<div class="img-intro"><div><img src="<?= $project->logoUrl ?>"></div></div>
 		</span>
