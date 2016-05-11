1552f36b9208e59d8194eab0e4e21c8a628ecaf5
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-05-11 18:21:01 +0700

    update loading homepage remove random

--- apps/metvuong/frontend/models/Ad.php
+++ apps/metvuong/frontend/models/Ad.php
@@ -175,7 +175,7 @@ class Ad extends Component
 //        $where = ['ad_product.status' => 1];
 //        $query->leftJoin('ad_product_addition_info', '`ad_product_addition_info`.`product_id` = `ad_product`.`id`');
 //        $query->with('adProductAdditionInfo');
-        $query->innerJoin(AdImages::tableName(), "ad_product.id = ad_images.product_id");
+//        $query->innerJoin(AdImages::tableName(), "ad_product.id = ad_images.product_id");
         $products = $query->limit(6)->orderBy(['id' => SORT_DESC])->all();
 //        $products = $query->limit(6)->orderBy("RAND()")->all();
         return $products;
