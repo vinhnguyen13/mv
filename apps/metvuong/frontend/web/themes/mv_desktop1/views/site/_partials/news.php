8c0739ad286b491659a97babd278be363f44bf2a
dientruong <dien.truong@trungthuygroup.vn>
2016-05-16 10:58:52 +0700

    udpate style

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/site/_partials/news.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/site/_partials/news.php
@@ -11,20 +11,24 @@ use yii\helpers\StringHelper;
 $news = CmsShow::getLatestNews(4);
 ?>
 <?php if(!empty($news)) {?>
-    <?php
-    foreach($news as $n){
-        ?>
-        <div class="item clearfix">
-            <a class="rippler rippler-default" href="<?= Url::to(['news/view', 'id' => $n['id'], 'slug' => $n['slug']], true) ?>" title="<?=$n['title']?>">
-                <div class="img-show">
-                    <div><img src="<?=Url::to('/store/news/show/' . $n['banner']) ?>" alt="<?=$n['title']?>"></div>
+    <ul class="clearfix">
+        <?php
+        foreach($news as $n){
+            ?>
+            <li class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="item clearfix">
+                    <a class="rippler rippler-default" href="<?= Url::to(['news/view', 'id' => $n['id'], 'slug' => $n['slug']], true) ?>" title="<?=$n['title']?>">
+                        <div class="img-show">
+                            <div><img src="<?=Url::to('/store/news/show/' . $n['banner']) ?>" alt="<?=$n['title']?>"></div>
+                        </div>
+                        <span class="txt-short-news">
+                            <span class="title-news color-30a868" title="<?=$n['title']?>"><?=StringHelper::truncate($n['title'], 60)?></span>
+                            <span class="date-news"><?=date('d/m/Y, H:i', $n['created_at'])?></span>
+                            <span><?=StringHelper::truncate($n['brief'], 120)?></span>
+                        </span>
+                    </a>
                 </div>
-                <span class="txt-short-news">
-                    <span class="title-news color-30a868" title="<?=$n['title']?>"><?=StringHelper::truncate($n['title'], 60)?></span>
-                    <span class="date-news"><?=date('d/m/Y, H:i', $n['created_at'])?></span>
-                    <span><?=StringHelper::truncate($n['brief'], 120)?></span>
-                </span>
-            </a>
-        </div>
-    <?php } ?>
+            </li>
+        <?php } ?>
+    </ul>
 <?php } ?>
