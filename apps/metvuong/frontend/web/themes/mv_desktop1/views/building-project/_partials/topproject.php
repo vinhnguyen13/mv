d16105176e1ba94d92434921b503c2a2cb92443e
Nhut Tran <nhuttranm@gmail.com>
2016-05-19 13:57:45 +0700

    update translate project

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/building-project/_partials/topproject.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/building-project/_partials/topproject.php
@@ -6,6 +6,15 @@
  */
 use yii\helpers\Url;
 
+Yii::t('project','Cao ốc văn phòng');
+Yii::t('project','Dự án khác');
+Yii::t('project','Khu căn hộ');
+Yii::t('project','Khu công nghiệp');
+Yii::t('project','Khu dân cư');
+Yii::t('project','Khu du lịch- nghỉ dưỡng');
+Yii::t('project','Khu phức hợp');
+Yii::t('project','Khu thương mại dịch vụ');
+Yii::t('project','Khu đô thị mới');
 ?>
 <div class="item-sidebar">
     <div class="title-sidebar"><?=Yii::t('project', 'TOP VIEW PROJECT')?></div>
@@ -16,7 +25,7 @@ use yii\helpers\Url;
                     <a href="<?= Url::to(["building-project/view", 'slug'=>$project->slug]); ?>" class="pic-intro rippler rippler-default">
                         <img src="<?=$project->logoUrl?>" alt="">
                     </a>
-                    <p class="infor-by-up"><?=$project->investment_type?></p>
+                    <p class="infor-by-up"><?=Yii::t('project', $project->investment_type)?></p>
                     <p class="name-post"><a href="<?= Url::to(["building-project/view", 'slug'=>$project->slug]); ?>"><?=$project->name?> </a></p>
                     <p class="fs-13 font-400"><?=$project->location?></p>
                 </div>
