175f50bfeb5cdb5ee7363a0229b59b3519ceff59
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-05-21 13:38:53 +0700

    translate terms

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/member/_partials/signup.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/member/_partials/signup.php
@@ -17,7 +17,7 @@ $model = Yii::createObject(RegistrationForm::className());
     <div class="form-group forgot-pass">
         <?= $form->field($model, 'password')->passwordInput(['class' => 'form-control', 'placeholder' => Yii::t('user', 'Password')])->label(false) ?>
     </div>
-    <div class="checkbox"><label> <input type="checkbox"> Chấp nhận <a href="#">điều khoản</a> sử dụng của <a href="#">metvuong.com</a></label>
+    <div class="checkbox"><label> <input type="checkbox"> <?=Yii::t('general', 'Accept the {terms} of use of metvuong.com', ['terms'=>\yii\helpers\Html::a(Yii::t('general', 'Terms and Conditions'), Url::to(['site/page', 'view'=>'terms']))])?></label>
     </div>
     <div class="footer-modal clearfix">
         <button type="button" class="btn-primary btn-common btn-login" id="btn-register"><?=Yii::t('user', 'Sign Up')?></button>
