87e7ce53158b6798660ca82b20e3544654aaffa0
Nhut Tran <nhuttranm@gmail.com>
2016-05-27 14:33:05 +0700

    translate service packages

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/payment/package/index.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/payment/package/index.php
@@ -11,28 +11,28 @@
 		<div class="title-top"><?=Yii::t('general', 'SERVICE PACKAGES')?></div>
 		<div class="item-packege col-xs-12 col-sm-6 col-md-4">
 			<div class="content-pac">
-				<h2><?=Yii::t('general', 'STARTER')?></h2>
+				<h2><?=Yii::t('general', 'STARTER 1')?></h2>
 				<p class="title-pac"><?=Yii::t('general', 'FREE')?></p>
-				<p><?=Yii::t('general', 'Limited to 3 Listings')?></p>
-				<p><?=Yii::t('general', 'Up to 30 Using Days')?></p>
+				<p><?=Yii::t('general', 'Limited Listings for Starter 1')?></p>
+				<p><?=Yii::t('general', 'Up to 30 Using Days for Starter 1')?></p>
 				<a href="#" class="btn-common"><?=Yii::t('general', 'Your Current Plan')?></a>
 			</div>
 		</div>
 		<div class="item-packege col-xs-12 col-sm-6 col-md-4">
 			<div class="content-pac">
-                <h2><?=Yii::t('general', 'STARTER')?></h2>
-				<p class="title-pac">300.000VND<span>/ <?=Yii::t('general', 'month')?></span></p>
-                <p><?=Yii::t('general', 'Limited to 3 Listings')?></p>
-                <p><?=Yii::t('general', 'Up to 30 Using Days')?></p>
-				<a href="#" class="btn-common">Your Current Plan</a>
+                <h2><?=Yii::t('general', 'STARTER 2')?></h2>
+				<p class="title-pac"><?=Yii::t('general','300.000')?>VND<span>/<?=Yii::t('general', 'month')?></span></p>
+                <p><?=Yii::t('general', 'Limited Listings for Starter 2')?></p>
+                <p><?=Yii::t('general', 'Up to 30 Using Days for Starter 2')?></p>
+				<a href="#" class="btn-common"><?=Yii::t('general', 'Your Current Plan')?></a>
 			</div>
 		</div>
 		<div class="item-packege col-xs-12 col-sm-6 col-md-4">
 			<div class="content-pac">
-                <h2><?=Yii::t('general', 'STARTER')?></h2>
-				<p class="title-pac">400.000VND<span>/ <?=Yii::t('general', 'month')?></span></p>
-                <p><?=Yii::t('general', 'Limited to 3 Listings')?></p>
-                <p><?=Yii::t('general', 'Up to 30 Using Days')?></p>
+                <h2><?=Yii::t('general', 'STARTER 3')?></h2>
+				<p class="title-pac"><?=Yii::t('general','400.000')?>VND<span>/<?=Yii::t('general', 'month')?></span></p>
+                <p><?=Yii::t('general', 'Limited Listings for Starter 3')?></p>
+                <p><?=Yii::t('general', 'Up to 30 Using Days for Starter 3')?></p>
 				<a href="#" class="btn-common"><?=Yii::t('general', 'Your Current Plan')?></a>
 			</div>
 		</div>
