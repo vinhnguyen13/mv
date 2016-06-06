4b7a589b06638478c8ebbbf78f1a133543a0b64a
Lệnh Quách <qtlenh@gmail.com>
2016-06-06 13:41:31 +0700

    map

--- apps/metvuong/api/modules/v1/controllers/MapController.php
+++ apps/metvuong/api/modules/v1/controllers/MapController.php
@@ -15,22 +15,129 @@ class MapController extends Controller {
 		
     	$v = Elastic::transform($v);
     	
-    	$response = [];
+    	$sentence = explode(' ', $v);
+    	$firstWord = $sentence[0];
     	
-    	$sort = ($t == AdProduct::TYPE_FOR_SELL) ? [AdProduct::TYPE_FOR_SELL_TOTAL => 'desc'] : [AdProduct::TYPE_FOR_RENT_TOTAL => 'desc'];
-    	
-    	$params = [
-			'query' => [
-				'match_phrase_prefix' => [
-					'search_field' => [
-						'query' => $v,
- 						'max_expansions' => 100
+    	$boost = [
+			[
+				"match" => [
+					"city_id" => [
+						"query" => 1,
+						"boost" => 1
 					]
 				]
 			],
-			'sort' => $sort,
-			'_source' => ['full_name', AdProduct::TYPE_FOR_SELL_TOTAL, AdProduct::TYPE_FOR_RENT_TOTAL]
-		];
+			[
+				"span_first" => [
+					"match" => [
+						"span_term" => [
+							"search_name" => $firstWord
+						]
+					],
+					"end" => 1,
+					"boost" => 3
+				]
+			]
+    	];
+
+    	$additionSearch = preg_replace("/(q|quan|p|phuong|cmt)([0-9])/", "$1 $2", $v);
+    	$additionSearch = str_replace("thang", "", $additionSearch);
+    	
+    	if(strpos($additionSearch, "du an ") !== false) {
+    		$additionSearch = str_replace("du an ", "", $v);
+    		
+    		$boost[] = [
+				"match" => [
+					"_type" => [
+						"query" => "project_building",
+						"boost" => 2	
+					]	
+				]
+			];
+    	} else {
+    		$boost[] = [
+				"match" => [
+					"_type" => [
+						"query" => "city",
+						"boost" => 1	
+					]	
+				]
+			];
+    	}
+    	
+    	$search = [
+			[
+				"match" => [
+					"search_name" => [
+						"query" => $v,
+						"operator" => "and"	
+					]
+				]
+			],
+			[
+				"match_phrase_prefix" => [
+					"search_name" => [
+						"query" => $v,
+						"operator" => "and",
+						"slop" => 5
+					]
+				]
+			],
+			[
+				"match_phrase_prefix" => [
+					"search_field" => [
+						"query" => $v,
+						"operator" => "and",
+						"slop" => 8
+					]
+				]
+			]
+    	];
+    	
+    	if($additionSearch != $v) {
+    		$search[] = [
+				"match" => [
+					"search_name" => [
+						"query" => $additionSearch,
+						"operator" => "and"	
+					]
+				]
+			];
+    		$search[] = [
+				"match_phrase_prefix" => [
+					"search_name" => [
+						"query" => $additionSearch,
+						"operator" => "and",
+						"slop" => 5
+					]
+				]
+			];
+    		$search[] = [
+				"match_phrase_prefix" => [
+					"search_field" => [
+						"query" => $additionSearch,
+						"operator" => "and",
+						"slop" => 8
+					]
+				]
+			];
+    	}
+    	
+    	$params = [
+    		"query" => [
+    			"bool" => [
+    				"must" => [
+    					"bool" => [
+    						"should" => $search
+    					]
+    				],
+    				"should" => $boost
+    			]	
+    		],
+    		'_source' => ['full_name', AdProduct::TYPE_FOR_SELL_TOTAL, AdProduct::TYPE_FOR_RENT_TOTAL]
+    	];
+    	
+    	$response = [];
     	
     	$ch = curl_init(\Yii::$app->params['elastic']['config']['hosts'][0] . '/term/_search');
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
