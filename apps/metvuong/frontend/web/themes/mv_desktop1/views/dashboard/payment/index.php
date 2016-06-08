3ee00eb105c9e502fe8acde6ab5e468081d30238
Nhut Tran <nhuttranm@gmail.com>
2016-06-08 17:21:31 +0700

    payment

---
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/dashboard/payment/index.php
@@ -0,0 +1,59 @@
+<?php
+use yii\web\View;
+use yii\helpers\Url;
+use yii\helpers\Json;
+use vsoft\ad\models\AdProduct;
+use frontend\models\UserActivity;
+use yii\helpers\Html;
+?>
+<div class="title-fixed-wrap container">
+    <div class="giao-dich">
+        <div class="title-top">Giao Dịch</div>
+        <div class="wrap-giao-dich">
+            <div class="title-gd mgB-15">Thông tin tài khoản</div>
+            <div class="mgB-30">
+                Số Keys Còn Lại: <span class="d-ib mgL-20 font-700"><span class="icon-mv mgR-5 color-gold fs-20"><span class="icon-coin-dollar"></span></span>10 Keys</span>
+                <a href="#" class="d-ib btn mgL-20 pdT-5 pdB-5 font-600 fs-13">Nạp tiền</a>
+            </div>
+            <div class="title-gd mgB-5">Giao dịch gần đây</div>
+            <table class="fixed_headers" cellspacing="0" cellpadding="0" border="0">
+                <thead>
+                    <tr>
+                        <th class="w-10"><span>Mã GD</span></th>
+                        <th class="w-20"><span>Ngày/Giờ</span></th>
+                        <th class="w-20"><span>Tiêu đề</span></th>
+                        <th class="w-20"><span>Loại giao dịch</span></th>
+                        <th class="w-10"><span>Tình trạng</span></th>
+                        <th class="w-20"><span>Số tiền</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1234</td>
+                        <td>08/06/2016 10:15:00 AM</td>
+                        <td><a href="#" class="color-cd">The Lancaster</a></td>
+                        <td>Post</td>
+                        <td class="color-cd">Thành công</td>
+                        <td>5 Keys</td>
+                    </tr>
+                    <tr>
+                        <td>1234</td>
+                        <td>08/06/2016 10:15:00 AM</td>
+                        <td><a href="#" class="color-cd">The Lancaster</a></td>
+                        <td>Boost</td>
+                        <td class="color-red">Thất bại</td>
+                        <td>2 Keys</td>
+                    </tr>
+                    <tr>
+                        <td>1234</td>
+                        <td>08/06/2016 10:15:00 AM</td>
+                        <td><a href="#" class="color-cd">The Lancaster</a></td>
+                        <td>Dashboard</td>
+                        <td class="color-cd">Thành công</td>
+                        <td>3 Keys</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
