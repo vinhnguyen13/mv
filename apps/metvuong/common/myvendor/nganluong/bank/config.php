ddb2383b7112a39f8a78640f32f107afe6e45757
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-06-16 11:46:02 +0700

    nl

---
+++ apps/metvuong/common/myvendor/nganluong/bank/config.php
@@ -0,0 +1,6 @@
+<?php
+define('URL_API','https://www.nganluong.vn/checkout.api.nganluong.post.php'); // Đường dẫn gọi api
+define('RECEIVER','quangvinhit2007@gmail.com'); // Email tài khoản ngân lượng
+define('MERCHANT_ID', '46833'); // Mã merchant kết nối
+define('MERCHANT_PASS', 'e2c1bd53868763cb53437f98a53a5849'); // Mật khẩu kết nôi
+?>
