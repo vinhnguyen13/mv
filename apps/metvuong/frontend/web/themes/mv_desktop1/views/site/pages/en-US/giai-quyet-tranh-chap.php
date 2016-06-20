7e1d13a20d7a014837a6c6ed57f93ef9f30a886a
dientruong <dien.truong@trungthuygroup.vn>
2016-06-20 13:39:44 +0700

    footer

---
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/site/pages/en-US/giai-quyet-tranh-chap.php
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="box-quy-che">
+        <p style="text-align: center;"><span style="font-size: 26px; line-height: 41.6px;text-transform:uppercase;">Các biện pháp và cơ chế giải quyết tranh chấp</span></p>
+        <p>Khi phát sinh tranh chấp, Công ty đề cao giải pháp thương lượng, hòa giải giữa các bên nhằm duy trì sự tin cậy của thành viên vào chất lượng dịch vụ của Metvuong.com và thực hiện theo các bước sau:</p>
+        <ul>
+            <li><strong>Bước 1:</strong>Thành viên người mua khiếu nại về BĐS của người bán qua email: <a data-cke-saved-href="mailto:info@metvuong.com" href="mailto:info@metvuong.com">info@metvuong.com</a></li>
+            <li><strong>Bước 2: </strong>Bộ phận Chăm Sóc Khách Hàng của Metvuong.com sẽ tiếp nhận các khiếu nại của thành viên người mua, tùy theo tính chất và mức độ của khiếu nại thì bên Metvuong.com có những biện pháp cụ thể hộ trợ người mua để giải quyết tranh chấp đó.</li>
+            <li><strong>Bước 3: </strong>Trong trường hợp nằm ngoài khả năng và thẩm quyền của Metvuong.com thì ban quản trị sẽ yêu cầu người mua đưa vụ việc này ra cơ quan nhà nước có thẩm quyền giải quyết theo pháp luật<strong><em></em></strong><strong><em></em></strong><strong><em></em></strong><strong><em></em></strong><strong><em></em></strong>
+                <br>
+            </li>
+        </ul>
+        <p><strong><em>Ngưởi mua gửi khiếu nại tại địa chỉ :</em></strong>
+            <br>
+        </p>
+        <p><strong>Công ty TNHH Đầu Tư TMDV </strong><strong>Mét Vuông</strong>
+            <br>
+        </p>
+        <p>Địa chỉ hỗ trợ khách hàng : Tầng 12 Tòa nhà Miss Áo Dài, Số 21 Nguyễn Trung Ngạn, P. Bến Nghé, Quận 1, Việt Nam
+            <br>
+        </p>
+        <p>Điện thoại: 08 3824 6810
+            <br>
+        </p>
+        <p>Email: <a data-cke-saved-href="mailto:info@metvuong.com" href="mailto:info@metvuong.com">info@metvuong.com</a></p>
+        <p>Metvuong.com tôn trọng và nghiêm túc thực hiện các quy định của pháp luật về bảo vệ quyền lợi của người mua (người tiêu dùng). Vì vậy, đề nghị các thành viên đăng tin rao vặt sản phẩm, BĐS trên sàn cung cấp đầy đủ, chính xác, trung thực và chi tiết các thông tin liên quan đến sản phẩm, BĐS. Mọi hành vi lừa đảo, gian lận trong kinh doanh đều bị lên án và phải chịu hoàn toàn trách nhiệm trước pháp luật.</p>
+        <p>Các bên bao gồm người bán, người mua sẽ phải có vai trò trách nhiệm trong việc tích cực giải quyết vấn đề. Đối với người bán hàng cần có trách nhiệm cung cấp văn bản giấy tờ chứng thực thông tin liên quan đến sự việc đang gây mâu thuẫn cho khách hàng. Đối với Metvuong.com sẽ có trách cung cấp những thông tin liên quan đến người mua và người bán nếu được người mua hoặc người bán (liên quan đến tranh chấp đó) yêu cầu.</p>
+        <p>Sau khi người bán, người mua đã giải quyết xong tranh chấp phải có trách nhiệm báo lại cho bán quản trị Metvuong.com. Trong trường hợp giao dịch phát sinh mâu thuẫn mà lỗi thuộc về người bán: Metvuong.com sẽ có biện pháp cảnh cáo, khóa tài khoản hoặc chuyển cho cơ quan pháp luật có thẩm quyền tùy theo mức độ của sai phạm. Metvuong.com sẽ chấm dứt và gỡ bỏ&nbsp;toàn bộ tin bài về sản phẩm, BĐS của người bán đó trên Metvuong.com đồng thời yêu cầu người bán bồi hoàn cho khách hàng thỏa đáng trên cơ sở thỏa thuận với khách hàng.</p>
+        <p>Nếu thông qua hình thức thỏa thuận mà vẫn không thể giải quyết được mâu thuẫn phát sinh từ giao dịch giữa 2 bên người mua, người bán, thì một trong 2 bên người mua và người bán sẽ có quyền nhờ đến cơ quan pháp luật có thẩm quyền can thiệp nhằm đảm bảo lợi ích hợp pháp của các bên nhất là cho khách hàng.</p>
+    </div>
+</div>
