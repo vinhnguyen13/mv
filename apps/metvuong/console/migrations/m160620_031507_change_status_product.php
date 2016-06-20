494315a9f41a28cb5ff6ea43cb789b8557ccdec5
Lệnh Quách <qtlenh@gmail.com>
2016-06-20 10:18:44 +0700

    chagne status field of ad_product table

--- apps/metvuong/console/migrations/m160620_031507_change_status_product.php
+++ apps/metvuong/console/migrations/m160620_031507_change_status_product.php
@@ -7,7 +7,7 @@ class m160620_031507_change_status_product extends Migration
 {
     public function up()
     {
-		$this->execute("ALTER TABLE `ad_product` CHANGE `status` `status` TINYINT(1) DEFAULT 1  NOT NULL  COMMENT '1: hiện tin, 0: ẩn tin, -1 pending';");
+		$this->execute("ALTER TABLE `ad_product` CHANGE `status` `status` TINYINT(1) DEFAULT -1  NOT NULL  COMMENT '1: hiện tin, 0: ẩn tin, -1 pending';");
     }
 
     public function down()
