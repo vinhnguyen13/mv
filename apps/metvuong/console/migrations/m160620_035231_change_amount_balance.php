24adfb318a71293755695a913894606f6dbc81d1
Lệnh Quách <qtlenh@gmail.com>
2016-06-20 10:55:17 +0700

    balance

--- apps/metvuong/console/migrations/m160620_035231_change_amount_balance.php
+++ apps/metvuong/console/migrations/m160620_035231_change_amount_balance.php
@@ -7,8 +7,7 @@ class m160620_035231_change_amount_balance extends Migration
 {
     public function up()
     {
-		$this->execute("ALTER TABLE `ec_balance`   
-						  CHANGE `user_id` `user_id` INT(11) NOT NULL,
+		$this->execute("ALTER TABLE `ec_balance`
 						  CHANGE `amount` `amount` INT(11) UNSIGNED NOT NULL,
 						  CHANGE `amount_promotion` `amount_promotion` INT(11) UNSIGNED NOT NULL;");
     }
