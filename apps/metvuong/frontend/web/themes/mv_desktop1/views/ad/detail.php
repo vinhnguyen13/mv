81b7a6cad8e7fc400ba0a1973bd0f41c7bb88144
Nhut Tran <nhuttranm@gmail.com>
2016-06-21 13:08:42 +0700

    optimize image and share in project, product detail

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/detail.php
@@ -98,7 +98,7 @@ Yii::$app->view->registerMetaTag([
     'content' => $product->urlDetail(true)
 ]);
 
-if(isset(Yii::$app->params['tracking']['all']) && (Yii::$app->params['tracking']['all'] == true) && ($product->user_id != $user->id)) {
+if(isset(Yii::$app->params['tracking']['all']) && (Yii::$app->params['tracking']['all'] == true) && !empty($user) && ($product->user_id != $user->id)) {
     Tracking::find()->productVisitor($user->id, $product->id, time());
 }
 
