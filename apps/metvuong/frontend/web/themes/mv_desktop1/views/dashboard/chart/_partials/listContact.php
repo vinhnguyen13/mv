9499fc52cdab160fd2e7b24bf3776d07d9e4baa1
Nhut Tran <nhuttranm@gmail.com>
2016-06-29 09:33:30 +0700

    Translate popup chart data, change title chart

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/dashboard/chart/_partials/listContact.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/dashboard/chart/_partials/listContact.php
@@ -1,10 +1,10 @@
 <?php
 use yii\helpers\Url;
 
-Yii::t('chart','Finders');
-Yii::t('chart','Visitors');
-Yii::t('chart','Saved');
-Yii::t('chart','Shares');
+Yii::t('chart','finders');
+Yii::t('chart','visitors');
+Yii::t('chart','saved');
+Yii::t('chart','shares');
 ?>
 <div class="clearfix mgB-15">
     <h3><?=Yii::t('chart', $view) ?></h3>
@@ -40,5 +40,5 @@ if(count($favourites) > 0) {
 ?>
 </ul>
 <p class="push-price">
-    Để tin đạt hiệu quả hơn vui lòng click vào đây <a href="#" data-toggle="modal" data-target="#upgrade-time" data-product="79110" class="btn-nang-cap mgL-10 btn-up">Boost</a>
+    <?=Yii::t('chart', 'Để tin đạt hiệu quả hơn vui lòng click vào đây')?> <a href="#" data-toggle="modal" data-target="#upgrade-time" data-product="79110" class="btn-nang-cap mgL-10 btn-up"><?= Yii::t('statistic', 'Up') ?></a>
 </p>
