5a59ab4ff78cc4ba42af5e1aa193d1aacfeed37f
dientruong <dien.truong@trungthuygroup.vn>
2016-06-29 14:18:32 +0700

    update style

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/coupon/_partials/coupon.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/coupon/_partials/coupon.php
@@ -19,8 +19,8 @@ if(!empty($event)) {
     ?>
     <div class="wrap-coupon">
         <p class="mgB-5">MetVuong hiện đang chạy Beta, hệ thống sẽ tặng <b><?=intval($code->amount);?></b> Keys</p>
-        <p class="mgB-5"><a data-toggle="tooltip" data-placement="bottom" title="Sao chép đường dẫn" data-title-success="Đã sao chép" class="btn-copy" data-clipboard-text="<?=$code->code;?>">Copy mã của bạn <b><?=$code->code;?></b></a></p>
-        <p class="mgB-5">Sau đó <a href="javascript:;" class="btn-coupon"><b>click vào đây</b></a>, dán mã của bạn vào để nhận Keys miễn phí từ MetVuong.</p>
+        <p class="mgB-5"><a href="#" data-toggle="tooltip" data-placement="bottom" title="Sao chép đường dẫn" data-title-success="Đã sao chép" class="btn-copy" data-clipboard-text="<?=$code->code;?>">Copy mã của bạn <b><?=$code->code;?></b></a></p>
+        <p class="mgB-5">Sau đó <a href="javascript:;" class="btn-coupon text-decor"><b>click vào đây</b></a>, dán mã của bạn vào để nhận Keys miễn phí từ MetVuong.</p>
     </div>
     <div id="coupon-dialog" class="modal fade popup-common" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog" role="document">
@@ -48,7 +48,7 @@ if(!empty($event)) {
                         </div>
                         <div class="bottom-popup">
                             <div class="text-right">
-                                <a href="#" class="btn btn-close close" data-dismiss="modal" aria-label="Close"><?=Yii::t('coupon', 'Cancel')?></a>
+                                <a href="#" class="btn btn-cancel btn-close close" data-dismiss="modal" aria-label="Close"><?=Yii::t('coupon', 'Cancel')?></a>
                                 <a href="#" class="btn coupon"><?=Yii::t('coupon', 'OK')?></a>
                             </div>
                         </div>
