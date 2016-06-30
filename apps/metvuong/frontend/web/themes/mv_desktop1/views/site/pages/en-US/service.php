93aeb81a3802346b042ed018a13ac60570f1b489
dientruong <dien.truong@trungthuygroup.vn>
2016-06-30 10:21:24 +0700

    update service page

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/site/pages/en-US/service.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/site/pages/en-US/service.php
@@ -2,72 +2,111 @@
 	<div class="wrap-content-page">
 		<p class="text-uper color-cd fs-15 font-600">MetVuong.com Scoring system</p>
 		<p>Every listing in Metvuong is assigned a score out of 100, and all of our search results will be based off of this score. It reflects the quality of the listing, as well as the recency of the listing, thus higher quality listings will be listed above poor listings, and newer listings will be listed higher than old ones. However, the original quality of the listing will be valued much more than when it was posted. As people post more listings, their reputation as an agent will also impact the listing scores.
-		    <br>
+		    
 		</p>
 		<p><strong>MetVuong ID:</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>Every listing also has a unique ID, denoted by MV#####, this ID can be used to quickly go to this listing.
-		    <br>Through the url.&nbsp;<span style="color:#00a769;"><u>www.metvuong.com/MV12345</u></span>
-		    <br>Through the search bar: simply type in 12345.
-		    <br>Any time you share a link, will also be with the quick link above.
-		    <br>This is to simplify navigating all of your potential listings, and to easily find the listing that is most suitable for you.
-		    <br>
+		    Through the url.&nbsp;<span style="color:#00a769;"><u>www.metvuong.com/MV12345</u></span>
+		    Through the search bar: simply type in 12345.
+		    Any time you share a link, will also be with the quick link above.
+		    This is to simplify navigating all of your potential listings, and to easily find the listing that is most suitable for you.
+		    
 		</p>
+		<p><strong>Chìa khóa</strong></p>
+		<p>
+			Trên metvuong.com, tất cả những chức năng trên Metvuong.com sẽ được thanh toán bằng chìa khóa, <strong>1 chìa = 1000 VND</strong>, và bạn có thể dể dàng nạp thên chìa qua nhiều cách khác nhau trên metvuong.com
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-1.jpg' ?>" alt="">
+		</p>
+		<p class="font-600 fs-20 text-decor">Chức năng cho người mua/thuê Bất Động Sản</p>
 		<p><strong>1. Search&nbsp;</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>Metvuong.com’s search is made to be easy, fast, and effective. &nbsp; In the quick search field on the home page, you can search a Street, District, City, or a specific Developmentand the result will come up with real estate properties for sell or rent. Searching for the Metvuong ID will also instantly take you to your desired listing<strong></strong>
-		    <br>
+		    
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-2.jpg' ?>" alt="">
 		</p>
 		<p><strong>2. Favorites</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>When searching for a house you go through hundreds of listings, it can be hard to keep track of the great ones, our favorite feature lets you save the listing, and revisit at a later time.
-		    <br>
+		    
 		</p>
 		<p><strong>3 Contacting an Agent.</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>If you see a listing you are interested in,Metvuong.com&nbsp;will contact them for you on your behalf, and they can get back to you with the required information
-		    <br>
+		    
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-3.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Email</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-4.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Chat</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-5.jpg' ?>" alt="">
 		</p>
 		<p><strong>4 Sharing a Listing.</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>Sometimes you want a second opinion, sometimes you need to show your agent which specific listing you're talking about, you can simply copy a link, and share this link with him, or you can share it through other platforms such as email social media, and chat.
-		    <br>
+		    
 		</p>
-		<p class="text-uper color-cd fs-15 font-600">COMING SOON 3 Comparison Feature</p>
-		<p>To make it easier for you to compare listings,metvuong.com&nbsp;will offer a comparison feature, allowing you to see detailed comparisons between price, area, and much much more.
-		    <br>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-6.jpg' ?>" alt="">
 		</p>
+		<p class="font-600">Chia sẻ qua Facebook</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-7.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Chia sẻ qua Email</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-8.jpg' ?>" alt="">
+		</p>
+		<p class="font-600 fs-20 text-decor">Chức năng cho người cho thuê/bán Bất Động Sản.</p>
 		<p><strong>1. Posting a Listing.&nbsp;</strong>
-		    <br>
+		    
 		</p>
 		<p style="margin-left: 40px;"><strong></strong><strong>a.</strong> Unlike most other websites and traditional way of posting your real estate property;&nbsp;Metvuong.com’s intricate posting method evaluates your posting information with hundred of thousands of data to make sure it is accurate.&nbsp; We also assign a numerical valuation to your listing so the more information you provide the higher your listing will appear on the search results.&nbsp; At&nbsp;Metvuong.com, we have data scientists diligently working on our data bases to assure that result of your searches will match to the most relevant buyers.&nbsp;
-		    <br>
+		    
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-9.jpg' ?>" alt="">
 		</p>
 		<p><strong>2. Boosting a Listing&nbsp;</strong>
-		    <br>
+		    
 		</p>
 		<p style="margin-left: 40px;"><strong></strong><strong>a.</strong> Over time, a listings score will naturally decay, so Boosting your listing will reset your score to it's original value, making it have a higher ranking for searches&nbsp;
-		    <br>
+		    
 		</p>
 		<p style="margin-left: 40px;"><strong>b.</strong> Your listing will also appear at the top whenever a relevant search occurs, over a certain period of time
-		    <br>
+		    
 		</p>
 		<p style="margin-left: 40px;"><strong>c.</strong> Your boosted listing will also have a red boarder, making it more attractive to other viewers.
-		    <br>
+		    
 		</p>
 		<p style="margin-left: 40px;"><strong>d.</strong> Your boosted listing will also appear on the Home Page under “HOT Properties”.
-		    <br>
+		    
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-10.jpg' ?>" alt="">
 		</p>
 		<p><strong>3. Dashboard&nbsp;</strong>
-		    <br>
+		    
 		</p>
 		<p><strong></strong>Dashboard provides you with analytics on who has viewed or interacted with your listing.&nbsp; We can provide you with the following data: &nbsp;a.) &nbsp;how many people searched for your listings, &nbsp;b.) &nbsp;how clicked on your listings, who saved it as their favorite, and who shared your listings with their peers.&nbsp; All these vital data can specifically lead out to potentially interested customers.
-		    <br>
+		    
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-11.jpg' ?>" alt="">
 		</p>
 	</div>
 </div>
--- apps/metvuong/frontend/web/themes/mv_desktop1/views/site/pages/vi-VN/service.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/site/pages/vi-VN/service.php
@@ -1,3 +1,6 @@
+<?php
+use yii\helpers\Url;
+?>
 <div class="container">
 	<div class="wrap-content-page">
 		<p class="text-uper color-cd fs-15 font-600">Hệ thống chấm điểm của MetVuong.com:</p>
@@ -7,24 +10,65 @@
 		<p>- Dán vào trình duyệt đường dẫn<span style="color:#00a769"><u> www.metvuong.com/MV12345</u></span></p>
 		<p>- Gõ vào thanh tìm kiếm: MV12345 hoặc 12345.</p>
 		<p>Điều này giúp việc tìm kiếm trở nên dễ dàng hơn và nhanh hơn cho bạn.</p>
+		<p><strong>Chìa khóa</strong></p>
+		<p>
+			Trên metvuong.com, tất cả những chức năng trên Metvuong.com sẽ được thanh toán bằng chìa khóa, <strong>1 chìa = 1000 VND</strong>, và bạn có thể dể dàng nạp thên chìa qua nhiều cách khác nhau trên metvuong.com
+		</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-1.jpg' ?>" alt="">
+		</p>
+		<p class="font-600 fs-20 text-decor">Chức năng cho người mua/thuê Bất Động Sản</p>
 		<p><strong>1. Tìm kiếm:</strong></p>
 		<p>Việc tìm kiếm các sản phẩm bất động sản trên Metvuong.com chưa bao giờ dễ dàng, nhanh chóng và hiệu quả đến vậy. Trong thanh Tìm kiếm nhanh trên trang chủ, bạn có thể gõ vào bất cứ tên đường, phường, quận, thành phố, hoặc một dự án bất kì, bạn sẽ có ngay kết quả của hàng trăm bất động sản rao bán hoặc cho thuê. Tìm kiếm bằng cách gõ mã số MV (dãy số) hoặc dãy số bạn biết cũng sẽ ngay lập tức đưa bạn đến sản phẩm mong muốn</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-2.jpg' ?>" alt="">
+		</p>
 		<p><strong>2. Yêu thích:</strong></p>
 		<p>Trong loạt danh sách tin đăng bạn tìm kiếm sẽ có rất nhiều sản phẩm phù hợp hoặc gần nhất với yêu cầu, để dễ dàng theo dõi sản phẩm bạn hài lòng, hãy dùng tính năng Yêu thích của chúng tôi để lưu các sản phẩm đó, và bạn có thể xem lại sau.</p>
 		<p><strong>3 Liên hệ với người đăng tin</strong></p>
 		<p>Nếu bạn tìm thấy một tin đăng mà bạn hài lòng, bạn có thể click vào thông tin của người đăng tin và liên hệ với họ bằng email hoặc chat</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-3.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Email</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-4.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Chat</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-5.jpg' ?>" alt="">
+		</p>
 		<p><strong>4 Chia sẻ:</strong></p>
 		<p>Đôi khi bạn cần có ý kiến từ người khác về bất động sản mà bạn muốn mua hoặc thuê, hay bạn muốn chỉ rõ cho người đăng tin biết bạn hài lòng về sản phẩm nào trong danh sách tin của họ, đơn giản bạn chỉ cần dùng tính năng Chia sẻ bằng Facebook, bằng Email hoặc sao chép đường dẫn tin hoặc cho mã số tin đăng cho người bạn muốn chia sẻ.</p>
-		<p class="text-uper color-cd fs-15 font-600">SẮP RA MẮT 3 Tính năng nổi bật:</p>
-		<p>Nhằm giúp so sánh các tin đăng mà bạn đang muốn tìm hiểu thêm và tìm ra được sản phẩm ưng ý nhất, MetVuong.com mang đến các tính năng hữu ích cho phép bạn so sánh về vị trí, giá cả và nhiều nhiều hơn nữa.</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-6.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Chia sẻ qua Facebook</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-7.jpg' ?>" alt="">
+		</p>
+		<p class="font-600">Chia sẻ qua Email</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-8.jpg' ?>" alt="">
+		</p>
+		<p class="font-600 fs-20 text-decor">Chức năng cho người cho thuê/bán Bất Động Sản.</p>
 		<p><strong>1. Đăng tin.</strong></p>
 		<p style="margin-left:40px"><span class="font-700">a.</span> Không giống như hầu hết các trang mạng đăng tin bất động sản truyền thống khác , Metvuong.com đã xây dựng cách đăng tin rõ ràng, chi tiết và hệ thống đánh giá tin đăng của bạn với hàng trăm ngàn dữ liệu để đảm bảo tin là chính xác. Chúng tôi cũng có thang điểm cho tin đăng nhằm xác định vị trí và tần suất xuất hiện của tin trên kết quả tìm kiếm. Tại MetVuong.com, chúng tôi có các nhà khoa học dữ liệu làm việc trên cơ sở so sánh rất nhiều kho dữ liệu của chúng tôi để đảm bảo rằng bạn tìm được sản phẩm phù hợp nhất.</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-9.jpg' ?>" alt="">
+		</p>
 		<p><strong>2. Boost tin đăng</strong></p>
 		<p style="margin-left:40px"><span class="font-700">a.</span> Theo thứ tự sắp xếp, tin đăng cũ sẽ bị trôi dần xuống dưới của danh sách và giảm dần điểm, để thiết lập lại vị trí dẫn đầu và đạt số điểm cao, bạn hãy boost tin đăng theo hướng dẫn để tin của bạn luôn có nhiều khách hàng quan tâm</p>
 		<p style="margin-left:40px"><span class="font-700">b.</span> Tin đăng boost sẽ được xuất hiện trên đầu danh sách và hiển thị trong nhiều kết quả tìm kiếm có liên quan của người dùng</p>
 		<p style="margin-left:40px"><span class="font-700">c.</span> Tin đăng boost sẽ được làm nổi bật trên danh sách, nhằm thu hút người tìm kiếm.</p>
 		<p style="margin-left:40px"><span class="font-700">d.</span> Tin đăng boost sẽ xuất hiện trên trang chủ dưới dạng "CÁC TIN ĐƯỢC TÌM KIẾM NHIỀU NHẤT".</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-10.jpg' ?>" alt="">
+		</p>
 		<p><strong>3. Dashboard</strong></p>
 		<p>Dashboard cung cấp cho bạn các số liệu khách hàng đã tương tác với tin đăng của bạn như: a) Có bao nhiêu người tìm kiếm danh sách của bạn b) Có bao nhiêu người yêu thích danh sách của bạn c) Có bao nhiêu người chia sẻ danh sách của bạn... Tất cả những dữ liệu quan trọng này bao gồm thông tin của khách hàng như email, điện thoại sẽ giúp bạn liên hệ với các khách hàng tiềm năng một cách nhanh chóng.</p>
+		<p>
+			<img class="w-70" src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/s-11.jpg' ?>" alt="">
+		</p>
 	</div>
 </div>
