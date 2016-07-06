19cda0ab4c3e06f388139df80f3d753ed9f7fed2
Lệnh Quách <qtlenh@gmail.com>
2016-07-06 11:24:08 +0700

    remove ward district from address

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/side-list.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/side-list.php
@@ -45,6 +45,7 @@
 					$address[] = $street['pre'] . ' ' . $street['name'];
 				}
 				
+				/*
 				if(isset($searchModel->wards[$product['ward_id']])) {
 					$ward = $searchModel->wards[$product['ward_id']];
 					
@@ -56,6 +57,7 @@
 					
 					$address[] = trim($district['pre'] . ' ' . $district['name']);
 				}
+				*/
 				
 				$address = implode(', ', array_filter($address));
 				
