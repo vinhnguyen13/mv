715d08122e3da8b23131d0b9c4d4ac133c8da04f
dientruong <dien.truong@trungthuygroup.vn>
2016-07-07 16:40:33 +0700

    update style

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/news/_partials/menu.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/news/_partials/menu.php
@@ -8,7 +8,7 @@ Yii::t('news', 'Business');
 Yii::t('news', 'Economy');
 ?>
 <div class="title-top clearfix">
-    <h2><?=Yii::t('news','NEWS')?></h2>
+    <div class="title-top"><?=Yii::t('news','NEWS')?></div>
 </div>
 <div class="list-menu-news hide">
     <div class="swiper-container">
