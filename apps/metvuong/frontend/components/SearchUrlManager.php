cb8feaad96ba921ee7150994ab9fa169359f8e19
Lệnh Quách <qtlenh@gmail.com>
2016-07-18 11:13:54 +0700

    map

--- apps/metvuong/frontend/components/SearchUrlManager.php
+++ apps/metvuong/frontend/components/SearchUrlManager.php
@@ -10,25 +10,21 @@ class SearchUrlManager extends UrlManager {
 	public $enableStrictParsing = false;
 	
 	public function createUrl($params) {
-		if(isset($params['params'])) {
-			$action = ($params[0] == '/ad/index1') ? \Yii::t('url', 'nha-dat-ban') : \Yii::t('url', 'nha-dat-cho-thue');
+		$action = ($params[0] == '/ad/index1') ? \Yii::t('url', 'nha-dat-ban') : \Yii::t('url', 'nha-dat-cho-thue');
 			
-			$segments = empty($params['params']) ? ['_'] : explode("/", $params['params']);
-			if(strpos($segments[0], '_') === FALSE) {
-				$action .= '/' . $segments[0];
-			}
-			
-			$covnertParams = [];
-			
-			foreach ($params as $k => $param) {
-				if($k && ($ck = array_search($k, MapSearch::$fieldsMapping)) !== FALSE) {
-					$covnertParams[] = $ck . '_' . $param;
-				}
-			}
-			
-			return '/' . $action . '/' . implode('/', $covnertParams);
-		} else {
-			return parent::createUrl($params);
+		$segments = empty($params['params']) ? ['_'] : explode("/", $params['params']);
+		if(strpos($segments[0], '_') === FALSE) {
+			$action .= '/' . $segments[0];
 		}
+		
+		$covnertParams = [];
+		
+		foreach ($params as $k => $param) {
+			if($k && ($ck = array_search($k, MapSearch::$fieldsMapping)) !== FALSE) {
+				$covnertParams[] = $ck . '_' . $param;
+			}
+		}
+		
+		return '/' . $action . '/' . implode('/', $covnertParams);
 	}
 }
