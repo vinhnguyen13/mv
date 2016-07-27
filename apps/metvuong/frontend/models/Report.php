d594b4fc4a0e3f6d5cf2dc110ba3391d3c2a3fd0
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-07-27 11:07:52 +0700

    report

---
+++ apps/metvuong/frontend/models/Report.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinhnguyen
+ * Date: 7/27/2016
+ * Time: 10:18 AM
+ */
+
+namespace frontend\models;
+use common\components\Util;
+use Yii;
+use yii\base\Component;
+use yii\db\Query;
+
+class Report extends Component
+{
+    public static function me()
+    {
+        return Yii::createObject(self::className());
+    }
+
+    public function chart($filter){
+        $arrFilter = [
+            'week' => -6,
+            '2week' => -13,
+            'month' => -30,
+            'quarter' => -90,
+        ];
+        $from = strtotime($arrFilter[$filter].' days');
+        $to = time();
+
+        $dateRange = Util::me()->dateRange( $from, $to,'+1 day', 'd/m/Y');
+        if(!empty($dateRange)){
+            $default = array_map(function($v){
+                return ['y'=>0, 'date'=>'#'];
+            },$dateRange);
+            $dataRegister = $dataLogin = $dataListing = $default;
+            /**
+             * user register
+             */
+            $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`created_at`), '%d/%m/%Y')");
+            $query = new Query();
+            $query->select(['count(*) total', $month_year." today"])->from('user')
+                ->where(['>', 'created_at', $from])
+                ->andWhere(['<', 'created_at', $to])
+                ->andWhere('updated_at > created_at')
+                ->groupBy('today')->orderBy('created_at DESC');
+            $stats_register = $query->all();
+            $totalRegister = 0;
+            foreach($stats_register as $item){
+                $totalRegister += $item['total'];
+                $kDate = array_search($item['today'], $dateRange);
+                $dataRegister[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
+            }
+            /**
+             * user login
+             */
+            $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`updated_at`), '%d/%m/%Y')");
+            $query = new Query();
+            $query->select(['count(*) total', $month_year." today"])->from('user')
+                ->where(['>', 'updated_at', $from])
+                ->andWhere(['<', 'updated_at', $to])
+                ->andWhere('updated_at > created_at')
+                ->groupBy('today')->orderBy('updated_at DESC');
+            $stats_login = $query->all();
+            $totalLogin = 0;
+            foreach($stats_login as $item){
+                $totalLogin += $item['total'];
+                $kDate = array_search($item['today'], $dateRange);
+                $dataLogin[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
+            }
+            /**
+             * listing
+             */
+            $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`created_at`), '%d/%m/%Y')");
+            $query = new Query();
+            $query->select(['count(*) total', $month_year." today"])->from('ad_product')
+                ->where(['>', 'created_at', $from])
+                ->andWhere(['<', 'created_at', $to])
+                ->groupBy('today')->orderBy('created_at DESC');
+            $stats_listing = $query->all();
+            $totalListing = 0;
+            foreach($stats_listing as $item){
+                $totalListing += $item['total'];
+                $kDate = array_search($item['today'], $dateRange);
+                $dataListing[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
+            }
+
+            /**
+             * load data to chart
+             */
+            $categories = $dateRange;
+            $dataChart[0] = [
+                'name' => 'Register',
+                'data' => $dataRegister
+            ];
+            $dataChart[1] = [
+                'name' => 'Login',
+                'data' => $dataLogin
+            ];
+            $dataChart[2] = [
+                'name' => 'Listing',
+                'data' => $dataListing
+            ];
+            return ['categories'=>$categories, 'dataChart'=>$dataChart];
+        }
+        return false;
+    }
+
+}
--- apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/index.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/index.php
@@ -1,96 +1,7 @@
 <?php
 use yii\web\View;
 use yii\helpers\Url;
-use yii\db\Query;
-use common\components\Util;
-
-$filter = Yii::$app->request->get("filter", 'week');
-
-$arrFilter = [
-    'week' => -6,
-    '2week' => -13,
-    'month' => -30,
-    'quarter' => -90,
-];
-$from = strtotime($arrFilter[$filter].' days');
-$to = time();
-
-$dateRange = Util::me()->dateRange( $from, $to,'+1 day', 'd/m/Y');
-if(!empty($dateRange)){
-    $default = array_map(function($v){
-        return ['y'=>0, 'date'=>'#'];
-    },$dateRange);
-    $dataRegister = $dataLogin = $dataListing = $default;
-    /**
-     * user register
-     */
-    $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`created_at`), '%d/%m/%Y')");
-    $query = new Query();
-    $query->select(['count(*) total', $month_year." today"])->from('user')
-        ->where(['>', 'created_at', $from])
-        ->andWhere(['<', 'created_at', $to])
-        ->andWhere('updated_at > created_at')
-        ->groupBy('today')->orderBy('created_at DESC');
-    $stats_register = $query->all();
-    $totalRegister = 0;
-    foreach($stats_register as $item){
-        $totalRegister += $item['total'];
-        $kDate = array_search($item['today'], $dateRange);
-        $dataRegister[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
-    }
-    /**
-     * user login
-     */
-    $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`updated_at`), '%d/%m/%Y')");
-    $query = new Query();
-    $query->select(['count(*) total', $month_year." today"])->from('user')
-        ->where(['>', 'updated_at', $from])
-        ->andWhere(['<', 'updated_at', $to])
-        ->andWhere('updated_at > created_at')
-        ->groupBy('today')->orderBy('updated_at DESC');
-    $stats_login = $query->all();
-    $totalLogin = 0;
-    foreach($stats_login as $item){
-        $totalLogin += $item['total'];
-        $kDate = array_search($item['today'], $dateRange);
-        $dataLogin[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
-    }
-    /**
-     * listing
-     */
-    $month_year = new \yii\db\Expression("DATE_FORMAT(FROM_UNIXTIME(`created_at`), '%d/%m/%Y')");
-    $query = new Query();
-    $query->select(['count(*) total', $month_year." today"])->from('ad_product')
-        ->where(['>', 'created_at', $from])
-        ->andWhere(['<', 'created_at', $to])
-        ->groupBy('today')->orderBy('created_at DESC');
-    $stats_listing = $query->all();
-    $totalListing = 0;
-    foreach($stats_listing as $item){
-        $totalListing += $item['total'];
-        $kDate = array_search($item['today'], $dateRange);
-        $dataListing[$kDate] = ['y'=>intval($item['total']), 'date' => $item['today']];
-    }
-
-    /**
-     * load data to chart
-     */
-    $categories = $dateRange;
-    $dataChart[0] = [
-        'name' => 'Register',
-        'data' => $dataRegister
-    ];
-    $dataChart[1] = [
-        'name' => 'Login',
-        'data' => $dataLogin
-    ];
-    $dataChart[2] = [
-        'name' => 'Listing',
-        'data' => $dataListing
-    ];
-}
 ?>
-
 <div class="title-fixed-wrap">
     <div class="container">
         <div class="statis">
@@ -116,31 +27,16 @@ if(!empty($dateRange)){
         			</div>
                     <ul class="option-view-stats clearfix">
                         <li>
-                            <a href="#" class="btn-visitor radio-ui active">
-                                <span class="icon-mv"><span class="icon-eye-copy"></span></span>
-                                <?=Yii::t('statistic','Register')?>
-                                <span><?=$totalRegister?></span>
-                                <input type="radio" name="toggle-chart" value="1000000">
-                                <span class="icon-mv toggle-ui-check"><span class="icon-checkbox"></span></span>
-                            </a>
+                            <input type="checkbox" name="toggle-chart" value="" id="register" checked><label for="register"><?=Yii::t('report','Register')?></label>
                         </li>
                         <li>
-                            <a href="#" class="btn-favourite radio-ui">
-                                <span class="icon-mv"><span class="icon-heart-icon-listing"></span></span>
-                                <?=Yii::t('statistic','Login')?>
-                                <span><?=$totalLogin ?></span>
-                                <input type="radio" name="toggle-chart" value="1000000">
-                                <span class="icon-mv toggle-ui-check"><span class="icon-checkbox"></span></span>
-                            </a>
+                            <input type="checkbox" name="toggle-chart" value="" id="login" checked><label for="login"><?=Yii::t('report','Login')?></label>
                         </li>
                         <li>
-                            <a href="#" class="btn-share radio-ui">
-                                <span class="icon-mv"><span class="icon-share-social"></span></span>
-                                <?=Yii::t('statistic','Listing')?>
-                                <span><?=$totalListing?></span>
-                                <input type="radio" name="toggle-chart" value="1000000">
-                                <span class="icon-mv toggle-ui-check"><span class="icon-checkbox"></span></span>
-                            </a>
+                            <input type="checkbox" name="toggle-chart" value="" id="listing" checked><label for="listing"><?=Yii::t('report','Listing')?></label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="toggle-chart" value="" id="transaction" checked><label for="transaction"><?=Yii::t('report','Transaction')?></label>
                         </li>
                     </ul>
         		</div>
@@ -152,17 +48,17 @@ if(!empty($dateRange)){
 <script>
     $(document).ready(function () {
         var chart = $('#chartAds').highcharts();
-        $('.option-view-stats .radio-ui').radio({
-            done: function (item) {
-                var index = item.parent().parent().index();
-                console.log(chart.series.length)
-                chart.series[index].show();
-                for ( var i = 0; i < chart.series.length; i++ ) {
-                    if ( i != index) {
-                        chart.series[i].hide();
-                    }
-                }
+        $(document).on('click', '.option-view-stats input', function (e) {
+            for ( var i = 0; i < chart.series.length; i++ ) {
+                chart.series[i].hide();
             }
+            $('.option-view-stats input[type=checkbox]').each(function () {
+                if (this.checked) {
+                    var index = $(this).parent().index();
+                    chart.series[index].show();
+                }
+            });
+
         });
     });
 </script>
