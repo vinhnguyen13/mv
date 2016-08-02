d1f2f7f533c0dcf6d54d798c38cd62e22a3ded2f
Lệnh Quách <qtlenh@gmail.com>
2016-08-02 16:41:10 +0700

    search

--- apps/metvuong/common/myvendor/vsoft/ad/controllers/TrackingSearchController.php
+++ apps/metvuong/common/myvendor/vsoft/ad/controllers/TrackingSearchController.php
@@ -20,8 +20,8 @@ class TrackingSearchController extends Controller {
 		\Yii::$app->db->createCommand("SET SESSION group_concat_max_len = 1000000;")->execute();
 		
 		$query = new Query();
-		$query->from("(SELECT `alias`, CONCAT_WS(' |*| ', CONCAT('ca : ', `created_at`), CONCAT('r : ', `referer`), CONCAT('f : ', `from`), CONCAT('l : ', `location`), CONCAT('t : ', `type`), CONCAT('ci : ', `category_id`), CONCAT('rn : ', `room_no`), CONCAT('tn : ', `toilet_no`), CONCAT('pi : ', `price_min`), CONCAT('pa : ', `price_max`), CONCAT('si : ', `size_min`), CONCAT('sa : ', `size_max`), CONCAT('o : ', `order_by`)) `content` FROM `tracking_search`) `tracking_search`");
-		$query->select(["`alias`", "GROUP_CONCAT(`content` SEPARATOR ' ||| ') `contents`"]);
+		$query->from("`tracking_search`");
+		$query->select(["`alias`"]);
 		$query->groupBy("alias");
 		
 		$dataProvider = new ActiveDataProvider([
