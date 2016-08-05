954f8546d27a7911697497112f00d43ef23b9dc4
dientruong <dien.truong@trungthuygroup.vn>
2016-08-05 16:56:01 +0700

    report >> transaction

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/list-transaction.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/list-transaction.php
@@ -11,25 +11,27 @@ $count_data = count($data) > 0 ? count($data) : 0;
 $last_key = count($data) - 1;
 if($count_data > 0) {
 ?>
-    <ul class="clearfix listTransaction">
-    <?php
-    foreach ($data as $key => $val) {
-        $transaction = Yii::$app->db->cache(function() use($val){
-            return \frontend\models\Transaction::findOne($val["id"]);
-        });
-        $amount = $transaction->amount;
-        ?>
-        <li class="">
-            <div class="wrap-tr-each swiper-slide">
-                <div class="w-10"><span><?=$transaction->user->profile->getDisplayName()?></span></div>
-                <div class="w-15"><span><?=date('d/m/Y, H:i', $transaction->created_at)?></span></div>
-                <div class="w-15"><span><?=$transaction->getObjectType($transaction->object_type)?></span></div>
-                <div class="w-15"><span class="color-cd"><?=$transaction->getTransactionStatus($transaction->status)?></span></div>
-                <div class="w-15"><span><?= abs($amount) > 1 ? $amount." Keys" : $amount." Key" ?></span></div>
-                <div class="w-30"><span><?=$transaction->getNote($transaction->object_type)?></span></div>
+    <div class="tbl-wrap">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php
+                foreach ($data as $key => $val) {
+                    $transaction = Yii::$app->db->cache(function() use($val){
+                        return \frontend\models\Transaction::findOne($val["id"]);
+                    });
+                    $amount = $transaction->amount;
+                    ?>
+                    <div class="wrap-tr-each">
+                        <div class="w-15"><span><?=$transaction->user->profile->getDisplayName()?></span></div>
+                        <div class="w-20"><span><?=date('d/m/Y, H:i', $transaction->created_at)?></span></div>
+                        <div class="w-15"><span><?=$transaction->getObjectType($transaction->object_type)?></span></div>
+                        <div class="w-15"><span class="color-cd"><?=$transaction->getTransactionStatus($transaction->status)?></span></div>
+                        <div class="w-15"><span><?= abs($amount) > 1 ? $amount." Keys" : $amount." Key" ?></span></div>
+                        <div class="w-20"><span><?=$transaction->getNote($transaction->object_type)?></span></div>
+                    </div>
+                    <?php
+                }?>
             </div>
-        </li>
-        <?php
-    }?>
-    </ul>
+        </div>
+    </div>
 <?php } ?>
