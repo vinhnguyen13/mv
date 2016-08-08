1387b39a557af23f375401f4744c5311a6b4ffeb
Nhut Tran <nhuttranm@gmail.com>
2016-08-08 10:56:36 +0700

    comment migrate db Crawl

--- apps/metvuong/console/migrations/crawl/m160521_094059_add_project_main_id_dbCraw.php
+++ apps/metvuong/console/migrations/crawl/m160521_094059_add_project_main_id_dbCraw.php
@@ -4,17 +4,17 @@ use yii\db\Migration;
 
 class m160521_094059_add_project_main_id_dbCraw extends Migration
 {
-    public function up()
-    {
-        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
-                            ADD COLUMN `project_main_id` INT NULL DEFAULT 0 COMMENT '' AFTER `ward_id`;");
-    }
-
-    public function down()
-    {
-        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
-                          DROP COLUMN `project_main_id`;");
-    }
+//    public function up()
+//    {
+//        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
+//                            ADD COLUMN `project_main_id` INT NULL DEFAULT 0 COMMENT '' AFTER `ward_id`;");
+//    }
+//
+//    public function down()
+//    {
+//        $this->execute("ALTER TABLE `db_mv_tool`.`ad_building_project`
+//                          DROP COLUMN `project_main_id`;");
+//    }
 
     /*
     // Use safeUp/safeDown to run migration code within a transaction
