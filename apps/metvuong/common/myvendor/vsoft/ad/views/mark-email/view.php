0d260d288604a19a1c5877286f612005ca6b4cb5
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-11 11:11:14 +0700

    mark-email

--- apps/metvuong/common/myvendor/vsoft/ad/views/mark-email/view.php
+++ apps/metvuong/common/myvendor/vsoft/ad/views/mark-email/view.php
@@ -15,7 +15,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->email], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->email], [
             'class' => 'btn btn-danger',
             'data' => [
