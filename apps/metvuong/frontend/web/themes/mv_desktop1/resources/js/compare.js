57c15475eedcc7ac2c2d1ea301ceef8bd7bed3c6
Lệnh Quách <qtlenh@gmail.com>
2016-08-11 09:41:19 +0700

    compare

--- apps/metvuong/frontend/web/themes/mv_desktop1/resources/js/compare.js
+++ apps/metvuong/frontend/web/themes/mv_desktop1/resources/js/compare.js
@@ -1,74 +1,35 @@
 $(document).ready(function(){
 	var compare = {
-		countCompare: 0,
-		numCheck: 3,
 		setNumItem: $('.tool-compare button'),
 		numGet: $('.tool-compare .num-show'),
 		saveGetItem: $('.getCompare'),
-		saveArr: [],
 		compareItemCookie: 'compareItems',
 		init: function () {
-			$(document).on('click', '.flag-compare-set', function (e) {
-				e.preventDefault();
-				compare.add($(this));
-			});
-			$(document).on('click', '.flag-compare-remove', function (e) {
-				e.preventDefault();
-				compare.remove($(this));
-			});
+			$(document).on('click', '.flag-compare-set', this.add);
+			$(document).on('click', '.flag-compare-remove', this.remove);
 			
-			var length = this.getCookieCompares().length;
-			if (length == 0 ) {
-				compare.numGet.text('');
-			} else {
-				compare.numGet.text('('+length+')');	
-			}
+			this.setCounter(this.getCookieCompares());
 		},
-		add: function (item) {
-			compare.countCompare += 1;
+		add: function () {
+			var item = $(this);
+			var id = item.data('value');
+			
 			item.removeClass('flag-compare-set').addClass('flag-compare-remove');
 			item.find('.txt-change').text(lajax.t('Đã thêm so sánh'));
 			item.find('.icon-balance-scale').attr('class','icon-close-icon');
-			compare.numGet.text('('+compare.countCompare+')');
-			compare.effectShow();
-			compare.checkVal(item, 1);
 			
-			this.pushCookie(item.data('value'));
+			if(!compare.isAdded(id)) {
+				compare.pushCookie(id);
+			}
 		},
-		remove: function (item) {
+		remove: function () {
+			var item = $(this);
+			
 			item.removeClass('flag-compare-remove').addClass('flag-compare-set');
 			item.find('.txt-change').text(lajax.t('So Sánh'));
 			item.find('.icon-close-icon').attr('class','icon-balance-scale');
-			compare.countCompare -= 1;
-			if ( compare.countCompare == 0 ) {
-				compare.numGet.text('');
-			}else {
-				compare.numGet.text('('+compare.countCompare+')');	
-			}
 			
-			compare.effectShow();
-			compare.checkVal(item, 0);
-			
-			this.removeCookie(item.data('value'));
-		},
-		checkVal: function (item, flag) {
-			var idItem = item.data('value');
-			if ( flag ) {
-				compare.saveArr.push(idItem);
-			}else {
-				for ( var i = 0; i < compare.saveArr.length; i++ ) {
-					if ( idItem == compare.saveArr[i] ) {
-						compare.saveArr.splice(i, 1);
-					}
-				}
-			}
-			
-			var valSet = '['+compare.saveArr.toString()+']';
-			compare.saveGetItem.val(valSet);
-		},
-		effectShow: function () {
-			compare.setNumItem.addClass('get-show-num');
-			setTimeout(function(){compare.setNumItem.removeClass('get-show-num')},300);
+			compare.removeCookie(item.data('value'));
 		},
 		pushCookie: function(id) {
 			var compareItems = this.getCookieCompares();
@@ -93,7 +54,7 @@ $(document).ready(function(){
 			
 			compareItems.push(id + ':' + '1');
 			
-			setCookie(this.compareItemCookie, compareItems.join(','));
+			this.updateCookie(compareItems);
 		},
 		removeCookie: function(id) {
 			var compareItems = this.getCookieCompares();
@@ -108,13 +69,38 @@ $(document).ready(function(){
 				}
 			}
 			
-			setCookie(this.compareItemCookie, compareItems.join(','));
+			this.updateCookie(compareItems);
 		},
 		getCookieCompares: function() {
 			var compareItems = getCookie(this.compareItemCookie);
 			compareItems = compareItems ? compareItems.split(',') : [];
 			
 			return compareItems;
+		},
+		updateCookie: function(compareItems) {
+			setCookie(this.compareItemCookie, compareItems.join(','));
+			this.setCounter(compareItems);
+		},
+		setCounter: function(compareItems) {
+			if(compareItems.length == 0) {
+				compare.numGet.text('');
+			} else {
+				compare.numGet.text('('+compareItems.length+')');	
+			}
+		},
+		isAdded: function(id) {
+			var compareItems = this.getCookieCompares();
+			
+			for(var i in compareItems) {
+				var item = compareItems[i].split(':');
+				var _id = item[0];
+				
+				if(_id == id) {
+					return true;
+				}
+			}
+			
+			return false;
 		}
 	};
 
