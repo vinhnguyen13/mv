184c7d6eab824427ab27264b9600966adb456a21
Lệnh Quách <qtlenh@gmail.com>
2016-08-11 13:39:14 +0700

    compare

---
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/compare.php
@@ -0,0 +1,32 @@
+<?php 
+	use vsoft\express\components\StringHelper;
+?>
+<div class="row list-compare">
+	<?php foreach ($products as $product): ?>
+	<div class="col-xs-12 col-sm-6 col-lg-4 item">
+		<div class="inner-box">
+			<a href="" class="pic-intro"><img src="<?= $product->representImage ?>" alt=""></a>
+			<div class="infor">
+				<p class="address-listing"><?= $product->address ?></p>
+				<ul>
+					<li class="price-item"><?= StringHelper::formatCurrency($product->price) ?> <span class="txt-unit"><?= Yii::t('ad', 'VND') ?></span>
+						<a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-down"></span></span></a>
+					</li>
+					<li>
+						<span class="icon-mv"><span class="icon-page-1-copy"></span></span><?= $product->area ?>m<sup>2</sup>
+						<a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
+					</li>
+					<li>
+						<span class="icon-mv"><span class="icon-bed-search"></span></span><?= $product->adProductAdditionInfo->room_no ?> <?= Yii::t('ad', 'Beds') ?>
+						<a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
+					</li>
+					<li>
+						<span class="icon-mv"><span class="icon-icon-bathroom"></span></span><?= $product->adProductAdditionInfo->toilet_no ?> <?= Yii::t('ad', 'Baths') ?>
+						<a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<?php endforeach; ?>
+</div>
--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/compare.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/compare.php
@@ -2,7 +2,7 @@
 	use yii\helpers\Url;
 	use vsoft\ad\models\AdProduct;
 	use yii\db\Query;
-use yii\db\Expression;
+	use yii\db\Expression;
 	
 	if(isset($_COOKIE['compareItems'])) {
 		$temp = explode(',', $_COOKIE['compareItems']);
@@ -22,9 +22,17 @@ use yii\db\Expression;
         <div class="menuUser">
             <div class="listing-compare">
                 <div class="title">Select Listing</div>
-                <?php if(isset($products)) : ?>
+                <?php
+                	if(isset($products)) :
+                		$selectProducts = []
+                ?>
                 <ul class="clearfix">
-                	<?php foreach ($products as $product) : ?>
+                	<?php
+                		foreach ($products as $product) :
+                			if($compares[$product->id]) {
+                				$selectProducts[] = $product;
+                			}
+                	?>
                     <li>
                         <label for="" class="checkbox-ui"><input type="checkbox" <?= $compares[$product->id] ? 'checked="checked"' : '' ?>><span class="icon-mv"><span class="icon-checkbox"></span></span><?= $product->address ?></label>
                         <a class="remove-compare" href=""><span class="icon-mv"><span class="icon-close-icon"></span></span></a>
@@ -49,88 +57,7 @@ use yii\db\Expression;
     <div class="title-fixed-wrap container">
         <div class="u-allduan">
             <div class="title-top">Compare Listing</div>
-            <div class="compare-block">
-                <div class="row list-compare">
-                    <div class="col-xs-12 col-sm-6 col-lg-4 item">
-                        <div class="inner-box">
-                            <a href="" class="pic-intro"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg' ?>" alt=""></a>
-                            <div class="infor">
-                                <p class="address-listing">Đường Mai Chí Thọ, Phường An Phú, Quận 2, Hồ Chí Minh</p>
-                                <ul>
-                                    <li class="price-item">
-                                        1,6191 <span class="txt-unit">tỷ</span> <span class="txt-unit">VNĐ</span>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-down"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-page-1-copy"></span></span>82m<sup>2</sup>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-bed-search"></span></span>2 phòng ngủ
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-icon-bathroom"></span></span>2 phòng tắm
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-lg-4 item">
-                        <div class="inner-box">
-                            <a href="" class="pic-intro"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg' ?>" alt=""></a>
-                            <div class="infor">
-                                <p class="address-listing">Đường Mai Chí Thọ, Phường An Phú, Quận 2, Hồ Chí Minh</p>
-                                <ul>
-                                    <li class="price-item">
-                                        1,6191 <span class="txt-unit">tỷ</span> <span class="txt-unit">VNĐ</span>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-down"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-page-1-copy"></span></span>82m<sup>2</sup>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-bed-search"></span></span>2 phòng ngủ
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-icon-bathroom"></span></span>2 phòng tắm
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xs-12 col-sm-6 col-lg-4 item">
-                        <div class="inner-box">
-                            <a href="" class="pic-intro"><img src="<?= Yii::$app->view->theme->baseUrl . '/resources/images/Government_-South_Australia_Police_Headquarters_Built_Environs_main.jpg' ?>" alt=""></a>
-                            <div class="infor">
-                                <p class="address-listing">Đường Mai Chí Thọ, Phường An Phú, Quận 2, Hồ Chí Minh</p>
-                                <ul>
-                                    <li class="price-item">
-                                        1,6191 <span class="txt-unit">tỷ</span> <span class="txt-unit">VNĐ</span>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-down"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-page-1-copy"></span></span>82m<sup>2</sup>
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-bed-search"></span></span>2 phòng ngủ
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                    <li>
-                                        <span class="icon-mv"><span class="icon-icon-bathroom"></span></span>2 phòng tắm
-                                        <a href="" class="pull-right arrow-updown"><span class="icon-mv"><span class="icon-long-arrow-up"></span></span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div class="compare-block"><?= $this->render('_partials/compare.php', ['products' => $selectProducts]) ?></div>
         </div>
     </div>
     <script>
