c7091e4e0cb092c32a2d41a46e2452f1594136eb
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-11 14:05:10 +0700

    mark-email

---
+++ apps/metvuong/common/myvendor/vsoft/ad/views/mark-email/view.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model vsoft\ad\models\MarkEmail */
+
+$this->title = $model->email;
+$this->params['breadcrumbs'][] = ['label' => 'Mark Emails', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="mark-email-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Delete', ['delete', 'id' => $model->email], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'email:email',
+            'type',
+            'count',
+            'status',
+            'send_time:datetime',
+        ],
+    ]) ?>
+
+</div>
