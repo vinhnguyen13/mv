a3743c632f6df9134d76696959b0e70f57435342
dientruong <dien.truong@trungthuygroup.vn>
2016-08-12 17:10:48 +0700

    compare

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/compare.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/ad/_partials/compare.php
@@ -6,9 +6,9 @@
 	$directionList = AdProductAdditionInfo::directionList();
 	$facilities = AdFacility::find()->indexBy('id')->all();
 ?>
-<div class="row list-compare" id="compare-box">
+<div class="list-compare" id="compare-box">
 	<?php foreach ($products as $product): ?>
-	<div class="col-xs-4 item">
+	<!-- <div class="col-xs-4 item">
 		<div class="inner-box">
 			<a href="" class="pic-intro"><img src="<?= $product->representImage ?>" alt=""></a>
 			<div class="infor">
@@ -50,6 +50,75 @@
 				</ul>
 			</div>
 		</div>
-	</div>
+	</div> -->
 	<?php endforeach; ?>
+	<div class="tbl-wrap">
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16"><span></span></div>
+					<div class="w-28">
+						<span>
+							<a href="" class="img-compare clearfix">
+								<div class="pull-left pic-intro"><img src="http://file4.batdongsan.com.vn/resize/745x510/2016/05/10/20160510081518-4d13.jpg" alt=""></div>
+								<div class="overflow-all">Đường Trần Khánh Dư, Phường Tân Định, Quận 1, Hồ Chí Minh</div>
+							</a>
+						</span>
+					</div>
+					<div class="w-28">
+						<span>
+							<a href="" class="img-compare clearfix">
+								<div class="pull-left pic-intro"><img src="http://file4.batdongsan.com.vn/resize/745x510/2016/05/10/20160510081518-4d13.jpg" alt=""></div>
+								<div class="overflow-all">Đường Trần Khánh Dư, Phường Tân Định, Quận 1, Hồ Chí Minh</div>
+							</a>
+						</span>
+					</div>
+					<div class="w-28">
+						<span>
+							<a href="" class="img-compare clearfix">
+								<div class="pull-left pic-intro"><img src="http://file4.batdongsan.com.vn/resize/745x510/2016/05/10/20160510081518-4d13.jpg" alt=""></div>
+								<div class="overflow-all">Đường Trần Khánh Dư, Phường Tân Định, Quận 1, Hồ Chí Minh</div>
+							</a>
+						</span>
+					</div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span><span class="icon-mv mgR-10 fs-18"><span class="icon-pricing"></span></span>Giá</span></div>
+					<div class="w-28 text-center bg-2"><span>1 tỷ</span></div>
+					<div class="w-28 price-big text-center"><span>2 tỷ</span></div>
+					<div class="w-28 text-center bg-2"><span>1.5 tỷ</span></div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span><span class="icon-mv fs-18 mgR-10"><span class="icon-page-1-copy"></span></span>Diện tích</span></div>
+					<div class="w-28 text-center bg-2"><span>103m2</span></div>
+					<div class="w-28 text-center"><span>103m2</span></div>
+					<div class="w-28 text-center bg-2"><span>103m2</span></div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span><span class="icon-mv fs-18 mgR-10"><span class="icon-bed-search"></span></span>Phòng ngủ</span></div>
+					<div class="w-28 text-center bg-2"><span>8 phòng ngủ</span></div>
+					<div class="w-28 text-center"><span>8 phòng ngủ</span></div>
+					<div class="w-28 text-center bg-2"><span>8 phòng ngủ</span></div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span><span class="icon-mv fs-18 mgR-10"><span class="icon-icon-bathroom"></span></span>Phòng tắm</span></div>
+					<div class="w-28 text-center bg-2"><span>8 phòng tắm</span></div>
+					<div class="w-28 text-center"><span>8 phòng tắm</span></div>
+					<div class="w-28 text-center bg-2"><span>8 phòng tắm</span></div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span>Số tầng</span></div>
+					<div class="w-28 text-center bg-2"><span>5</span></div>
+					<div class="w-28 text-center"><span>không xác định</span></div>
+					<div class="w-28 text-center bg-2"><span>không xác định</span></div>
+				</div>
+				<div class="wrap-tr-each swiper-slide">
+					<div class="w-16 font-600"><span>Mặt tiền</span></div>
+					<div class="w-28 text-center bg-2"><span>5</span></div>
+					<div class="w-28 text-center"><span>không xác định</span></div>
+					<div class="w-28 text-center bg-2"><span>không xác định</span></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
