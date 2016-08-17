fbc50cafc04b502353d7abaa9c0b3cc53c3d97ac
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-17 16:35:37 +0700

    menu report

--- apps/metvuong/common/components/Acl.php
+++ apps/metvuong/common/components/Acl.php
@@ -7,11 +7,31 @@
  */
 
 namespace common\components;
+use Yii;
+use yii\base\Component;
+use yii\web\ForbiddenHttpException;
 
 
-class Acl
+class Acl extends Component
 {
     const ACL_ADMIN = 'Admin';
     const ACL_REPORT = 'Report';
+
+    public static function me()
+    {
+        return Yii::createObject(self::className());
+    }
+
+    public function checkACL($permissionName)
+    {
+        $parseUrl = Yii::$app->urlManager->parseRequest(Yii::$app->request);
+        $urlBase = !empty($parseUrl[0]) ? $parseUrl[0] : '';
+        if(!Yii::$app->user->isGuest && !in_array($urlBase, ['site/logout'])) {
+            if (Yii::$app->user->can($permissionName)) {
+                return true;
+            }
+            throw new ForbiddenHttpException(Yii::t('yii', 'You are not allowed to perform this action.'));
+        }
+    }
 
 }
