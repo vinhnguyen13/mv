b6de028e23b5fb022e515a09b6f83643d85500fd
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-18 15:17:21 +0700

    report daily

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/list-favorite.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/list-favorite.php
@@ -24,16 +24,16 @@ if($count_data > 0) {
         $email = empty($user->profile->public_email) ? $user->email : $user->profile->public_email;
         $avatar = $user->profile->getAvatarUrl();
         $address = $product->getAddress();
+
+        $message = Yii::t('activity', '{owner} favorite {product}',
+            [
+                'owner'=> \yii\helpers\Html::a($user->profile->getDisplayName(), \yii\helpers\Url::to(['member/profile','username' => $username], true)),
+                'product'=> \yii\helpers\Html::a($address, $product->urlDetail())
+            ]
+        );
         ?>
         <li class="">
-            <?=Yii::t('activity', '{owner} favorite {product}');?>
-            <a href="<?=\yii\helpers\Url::to(['member/profile','username' => $username], true)?>" title="<?=$username?>">
-                <?=$user->profile->getDisplayName()?>
-            </a>
-
-            <a href="<?=$product->urlDetail()?>" title="<?=$address?>">
-                <?=$address?>
-            </a>
+            <?=$message;?>
         </li>
         <?php
     }?>
