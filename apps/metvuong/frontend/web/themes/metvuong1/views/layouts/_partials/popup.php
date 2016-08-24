e11bfef7fa2ce94967cdbd9279a038b76be198d1
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-24 14:09:29 +0700

    remove themes

--- apps/metvuong/frontend/web/themes/metvuong1/views/layouts/_partials/popup.php
+++
@@ -1,3 +0,0 @@
-<?php $this->beginContent('@app/views/member/_partials/signup.php'); ?><?php $this->endContent();?>
-<?php $this->beginContent('@app/views/member/_partials/login.php'); ?><?php $this->endContent();?>
-<?php $this->beginContent('@app/views/member/_partials/forgot.php'); ?><?php $this->endContent();?>
