3f6fd672b0601c2c6ee9b029ffb24e5f2124e938
vinhnguyen13 <quangvinh.nguyen@trungthuygroup.vn>
2016-08-26 11:45:25 +0700

    report statistic

--- apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/statistic.php
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/report/default/_partials/statistic.php
@@ -6,15 +6,15 @@
         <th>Name</th>
         <th>
             Last Month (30 days before)<br/>
-            <i style="font-size: 11px;">From <?=date("d-m-Y 00:00:00", strtotime('-30 days'))?> To <?=date("d-m-Y 00:00:00", time())?></i>
+            <i style="font-size: 11px;"><?=date("d-m 00:00:00", strtotime('-30 days'))?> -> <?=date("d-m 00:00:00", time())?></i>
         </th>
         <th>
             Yesterday (1 day before)<br/>
-            <i style="font-size: 11px;">From <?=date("d-m-Y 00:00:00", strtotime('-1 days'))?> To <?=date("d-m-Y 23:59:59", strtotime('-1 days'))?></i>
+            <i style="font-size: 11px;"><?=date("d-m 00:00:00", strtotime('-1 days'))?> -> <?=date("d-m 23:59:59", strtotime('-1 days'))?></i>
         </th>
         <th>
             Total (today)<br/>
-            <i style="font-size: 11px;">From <?=date("d-m-Y 00:00:00", time())?> To <?=date("d-m-Y 23:59:59", time())?></i>
+            <i style="font-size: 11px;"><?=date("d-m 00:00:00", time())?> -> <?=date("d-m 23:59:59", time())?></i>
         </th>
     </tr>
     </thead>
