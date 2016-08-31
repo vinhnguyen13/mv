985f9656cc562046196dec95cb189004fa5bdcf3
Lệnh Quách <qtlenh@gmail.com>
2016-08-31 17:44:46 +0700

    dashboard

---
+++ apps/metvuong/frontend/web/themes/mv_desktop1/views/dashboard/statistics/notify-free.php
@@ -0,0 +1,16 @@
+<?php
+	use vsoft\ec\models\EcStatisticView;
+	use yii\helpers\Url;
+?>
+<div class="title-fixed-wrap">
+    <div class="container">
+        <div class="statis">
+        	<div class="title-top">
+                <?=Yii::t('statistic','Statistic')?>
+            </div>
+        	<section class="clearfix mgB-40">
+				
+        	</section>
+        </div>
+    </div>
+</div>
